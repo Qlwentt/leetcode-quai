cbe68fa617462421ef826748f9d62d85d620ceff
Quai Wentt <quai.wentt@gmail.com>
2022-07-06 13:19:40 -0400

    Time: 46 ms (62.28%), Space: 13.9 MB (9.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,5 +1,10 @@
 class Solution:
+    def __init__(self):
+        self.memo = {} # n -> f(n)
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+        if n not in self.memo:
+            self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        print(self.memo)
+        return self.memo[n]
