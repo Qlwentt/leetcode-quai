bbc167a2e58eb0edb904707e7bce9ca4f9e8278a
Quai Wentt <quai.wentt@gmail.com>
2022-07-09 17:52:10 -0400

    Time: 57 ms (87.36%), Space: 18.1 MB (44.57%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -5,17 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        treeArray = []
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         treeArray = []
         
-        def inorder(root):
-            if not root:
-                return treeArray
-            if root.left:
-                inorder(root.left)
-            treeArray.append(root.val)
-            if root.right:
-                inorder(root.right)
-            return treeArray
-        treeArray = inorder(root)
-        return treeArray[k-1]
+#         def inorder(root):
+#             if not root:
+#                 return treeArray
+#             if root.left:
+#                 inorder(root.left)
+#             treeArray.append(root.val)
+#             if root.right:
+#                 inorder(root.right)
+#             return treeArray
+#         treeArray = inorder(root)
+#         return treeArray[k-1]
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = [(root,False)]
+        i = 1
+        # LVR
+        while stack:
+            node, visit = stack.pop()
+            if not node:
+                continue
+            if visit:
+                if k == i:
+                    return node.val
+                i += 1
+                continue
+            stack.extend([(node.right,False),(node,True),(node.left,False)])
