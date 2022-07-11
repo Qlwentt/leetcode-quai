6e6ee585c8f8d99bdb795586f9671d640d4a7c45
Quai Wentt <quai.wentt@gmail.com>
2022-07-11 13:44:00 -0400

    Time: 42 ms (97.28%), Space: 17 MB (14.02%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,17 +7,18 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         prev = float('-inf')
+        
         def inorder(root):
             nonlocal prev
             if not root:
                 return True
             if not inorder(root.left):
                 return False
-            if not root.val > prev:
+            if not prev < root.val:
                 return False
             prev = root.val
             if not inorder(root.right):
                 return False
             return True
-        return inorder(root)    
+        return inorder(root)
         
