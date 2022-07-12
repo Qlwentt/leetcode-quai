6789784e7d14c97704fc9a24e3f05ea6f4e392dd
Quai Wentt <quai.wentt@gmail.com>
2022-07-12 16:45:39 -0400

    Time: 247 ms (73.53%), Space: 20.1 MB (91.75%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -16,21 +16,19 @@ class Codec:
         """
         if not root:
             return ""
-        q = deque([(root,1)])
+        
+        q = deque([root])
         res = []
-        i = 1
         while q:
-            while q and q[0][1] == i:
-                node, j = q.popleft()
-                if node:
-                    res.append(str(node.val))
-                    q.append((node.left, j + 1))
-                    q.append((node.right, j + 1))
-                else:
-                    res.append("N")
-            i += 1
-        print(res)
-        return ",".join(res)
+            node = q.popleft()
+            if not node:
+                res.append("N")
+                continue
+            res.append(str(node.val))
+            q.append(node.left)
+            q.append(node.right)
+        res = ",".join(res)
+        return res
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -41,25 +39,20 @@ class Codec:
         if not data:
             return None
         data = data.split(",")
-        i = 1
         root = TreeNode(int(data[0]))
         q = deque([root])
-        
-        while q and i < len(data):
+        i = 1
+        while q:
             node = q.popleft()
-            if data[i] != 'N':
-                left = TreeNode(int(data[i]))
-                node.left = left
-                q.append(left)
+            if data[i] != "N":
+                node.left = TreeNode(int(data[i]))
+                q.append(node.left)
             i += 1
-            if data[i] != 'N':
-                right = TreeNode(int(data[i]))
-                node.right = right
-                q.append(right)
-            i+= 1
+            if data[i] != "N":
+                node.right = TreeNode(int(data[i]))
+                q.append(node.right)
+            i += 1
         return root
-            
-                    
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
