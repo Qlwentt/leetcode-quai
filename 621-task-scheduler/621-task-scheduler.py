603e05d34c203f1faa4bc8a795eb3cdbded17cb0
Quai Wentt <quai.wentt@gmail.com>
2022-07-17 13:11:24 -0400

    Time: 1359 ms (8.05%), Space: 14.4 MB (39.73%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.py
@@ -0,0 +1,41 @@
+from collections import deque
+class MaxHeap:
+    def __init__(self, nums):
+        self.heap = [ -x for x in nums]
+        heapq.heapify(self.heap)
+        
+    def __str__(self):
+        return ",".join(str(self.heap))
+    
+    def push(self, val):
+        heapq.heappush(self.heap, val * -1)
+        
+    def pop(self):
+        return heapq.heappop(self.heap) * -1
+    
+    def empty(self):
+        return len(self.heap) == 0
+    
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        taskCount = Counter(tasks)
+        
+        maxHeap = MaxHeap(taskCount.values())
+        
+        q = deque([])
+        time = 0
+        while not maxHeap.empty() or q:
+            time += 1
+            
+            if not maxHeap.empty():
+                task = maxHeap.pop()
+                task -= 1
+                if task:
+                    cooldown = time + n
+                    q.append((task, cooldown))
+
+            if q and q[0][1] == time:
+                task, cool = q.popleft()
+                maxHeap.push(task)
+        return time
