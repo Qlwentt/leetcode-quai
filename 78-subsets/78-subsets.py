6425e14d1ffa1456c1cb2afeed8a0c13508aba5d
Quai Wentt <quai.wentt@gmail.com>
2022-07-19 13:32:18 -0400

    Time: 49 ms (59.73%), Space: 14.2 MB (33.99%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,18 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
         subset = []
-        def dfs(i):
+        result = []
+        def backtrack(i):
             if i >= len(nums):
-                result.append(subset.copy())
-                return          
+                result.append(subset[:])
+                return
             
-            # decision 1: add nums[i] to subset
+            # add to subset
             subset.append(nums[i])
-            dfs(i+1)
+            backtrack(i+1)
             
-            # decision 2: don't add nums[i] to subset
+            # didn't add to subset
             subset.pop()
-            dfs(i+1)
-        dfs(0)
+            backtrack(i+1)
+        backtrack(0)
         return result
