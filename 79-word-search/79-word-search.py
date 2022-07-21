efe6af1a6cc1239188501d642b4101e82a6d5f9f
Quai Wentt <quai.wentt@gmail.com>
2022-07-21 14:27:12 -0400

    Time: 8590 ms (28.38%), Space: 13.9 MB (50.90%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,30 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rowLen = len(board)
-        colLen = len(board[0])
+        numRows = len(board)
+        numCols = len(board[0])
         path = set()
-        
         def dfs(i, row, col):
             if i == len(word):
                 return True
-            if ( row < 0 or col < 0 or
-                 row >= rowLen or col >= colLen
-                 or word[i] != board[row][col]
-                 or (row, col) in path
+            if (row < 0 or col < 0 or
+                row >= numRows or col >= numCols or
+                word[i] != board[row][col] or
+                (row, col) in path
                ):
                 return False
             path.add((row,col))
             res = (dfs(i+1, row + 1, col) or
-                   dfs(i+1, row - 1, col) or
-                   dfs(i+1, row, col + 1) or
-                   dfs(i+1, row, col - 1)
-                  )
+                    dfs(i+1, row - 1, col) or 
+                    dfs(i+1, row, col + 1) or
+                    dfs(i+1, row, col -1))
             path.remove((row,col))
             return res
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if dfs(0, r, c):
-                    return True
-                
+                   
+        for r in range(numRows):
+            for c in range(numCols):
+                   if dfs(0, r, c):
+                        return True
+                   
         return False
-    
+        
