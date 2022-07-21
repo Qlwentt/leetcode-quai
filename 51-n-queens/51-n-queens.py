ad4a7f6f16d56137800004ff6b372d422bcf9540
Quai Wentt <quai.wentt@gmail.com>
2022-07-21 18:59:51 -0400

    Time: 92 ms (55.97%), Space: 14.4 MB (44.03%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -7,18 +7,43 @@ class Solution:
         validBoards = []
         board = []
         
+        rowTally = [False] * n
+        colTally = [False] * n
+        xDiagTally = [False] * (((n-1) * 2) +1)
+        yDiagTally = [False] * (((n-1) * 2 )+1)
+        
         def backtrack(possibleChoices):
             if not possibleChoices  and self.isSolution(board):
                 validBoards.append(board[:])
                 return
             
             for i, queenPosition in enumerate(possibleChoices):
+                r = queenPosition
+                c = len(board)
+                
+                if (rowTally[r] or
+                    colTally[c] or 
+                    xDiagTally[r-c + n - 1] or
+                    yDiagTally[r+c]):
+                    continue
+                
                 row = ["."] * n
                 row[queenPosition] = "Q"
                 row = "".join(row)
                 board.append(row)
+                
+                rowTally[r] = True
+                colTally[c] = True
+                xDiagTally[r-c + n - 1] = True
+                yDiagTally[r+c] = True
+                
                 backtrack(possibleChoices[:i]+possibleChoices[i+1:])
+                
                 board.pop()
+                rowTally[r] = False
+                colTally[c] = False
+                xDiagTally[r-c + n - 1] = False
+                yDiagTally[r+c] = False
             
         backtrack([i for i in range(n)])
         return validBoards
