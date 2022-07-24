87da7ec188af2ba893167da96ab3396449f20955
Quai Wentt <quai.wentt@gmail.com>
2022-07-24 12:39:40 -0400

    Time: 73 ms (20.05%), Space: 14.6 MB (26.20%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+    def __str__(self):
+        return str(self.val)
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        
+        copy = Node(node.val)
+        q = deque([(node, copy)])
+        nodesAndCopies = {}
+        visited = set()
+        
+        while q:
+            current, currentCopy = q.popleft()
+            if current in visited:
+                continue
+            for neighbor in current.neighbors:
+                if neighbor.val != current.val:
+                    if neighbor in nodesAndCopies:
+                        neighborCopy = nodesAndCopies[neighbor]
+                    else:
+                        neighborCopy = Node(neighbor.val)
+                        nodesAndCopies[neighbor] = neighborCopy
+                    currentCopy.neighbors.append(neighborCopy)
+                    q.append((neighbor, neighborCopy))
+                else:
+                    currentCopy.neighbors.append(currentCopy)
+            nodesAndCopies[current] = currentCopy
+            visited.add(current)
+    
+        return copy
