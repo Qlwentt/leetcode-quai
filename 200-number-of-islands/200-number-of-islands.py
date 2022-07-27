7bb50e8df2b2a2328f4dff686177bd8b77226fb8
Quai Wentt <quai.wentt@gmail.com>
2022-07-27 12:32:51 -0400

    Time: 356 ms (81.78%), Space: 25.6 MB (9.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,32 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        
         numRows = len(grid)
         numCols = len(grid[0])
         
-        numIslands = 0
+        visited = set() # (r,c)
         
-        def dfs(r,c):
-            if ( r < 0 or c < 0 or
-                 r >= numRows or c >= numCols or
-                 grid[r][c] == '0' or
+        def isNewIsland(r,c):
+            if (r < 0 or c < 0 or
+                r >= numRows or
+                c >= numCols or
+                grid[r][c] == "0" or
                 (r,c) in visited
             ):
                 return False
+            
             visited.add((r,c))
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+            
+            isNewIsland(r + 1,c)
+            isNewIsland(r - 1 ,c)
+            isNewIsland(r,c + 1)
+            isNewIsland(r,c - 1)
+            
             return True
-            
-        for row in range(numRows):
-            for col in range(numCols):
-                if grid[row][col] == "1":
-                    if dfs(row,col):
-                        numIslands += 1
         
+        numIslands = 0
+        
+        for r in range(numRows):
+            for c in range(numCols):
+                if grid[r][c] == "1":
+                    if isNewIsland(r,c):
+                        numIslands += 1
         return numIslands
-            
             
