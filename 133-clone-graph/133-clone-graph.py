a7740611478c7db5a54f8ddb66949a27d4cec153
Quai Wentt <quai.wentt@gmail.com>
2022-07-28 14:22:03 -0400

    Time: 55 ms (58.46%), Space: 14.6 MB (26.20%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -1,36 +1,33 @@
 from collections import deque
-
 # Definition for a Node.
 # class Node:
 #     def __init__(self, val = 0, neighbors = None):
 #         self.val = val
 #         self.neighbors = neighbors if neighbors is not None else []
 
-
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
-            return node
+            return None
         
         copy = Node(node.val)
         q = deque([(node, copy)])
-        nodesAndCopies = {}
         visited = set()
+        nodeAndCopy = {} #key : node, value: copy
         
         while q:
-            current, currentCopy = q.popleft()
-            if current in visited:
+            currentNode, currentCopy = q.popleft()
+            if currentNode in visited:
                 continue
-            for neighbor in current.neighbors:
-                if neighbor in nodesAndCopies:
-                    neighborCopy = nodesAndCopies[neighbor]
-                else:
-                    neighborCopy = Node(neighbor.val)
-                    nodesAndCopies[neighbor] = neighborCopy
-                currentCopy.neighbors.append(neighborCopy)
-                q.append((neighbor, neighborCopy))
-               
-            nodesAndCopies[current] = currentCopy
-            visited.add(current)
-    
+            for neigh in currentNode.neighbors:
+                if neigh not in nodeAndCopy:
+                    copiedNeigh = Node(neigh.val)
+                    nodeAndCopy[neigh] = copiedNeigh
+                copiedNeigh = nodeAndCopy[neigh]
+                currentCopy.neighbors.append(copiedNeigh)
+                q.append((neigh, copiedNeigh))
+            nodeAndCopy[currentNode] = currentCopy
+            visited.add(currentNode)
+        
         return copy
+            
