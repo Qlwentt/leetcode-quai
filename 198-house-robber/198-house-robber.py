df749a3516dd9c3c8d488be5e7b18bc6947d55f8
Quai Wentt <quai.wentt@gmail.com>
2022-08-02 14:26:57 -0400

    Time: 42 ms (71.64%), Space: 13.8 MB (97.29%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,12 +1,20 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
+    # def __init__(self):
+    #     self.memo = {}
     def rob(self, nums: List[int]) -> int:
-        def robHelper(i):
-            if i >= len(nums):
-                return 0
-            if i not in self.memo:            
-                self.memo[i] = max(robHelper(i+2) + nums[i], robHelper(i+1))
-            return self.memo[i]
-        return robHelper(0)
+        # def robHelper(i):
+        #     if i >= len(nums):
+        #         return 0
+        #     if i not in self.memo:            
+        #         self.memo[i] = max(robHelper(i+2) + nums[i], robHelper(i+1))
+        #     return self.memo[i]
+        # return robHelper(0)
+        n = len(nums)
+        dp = [-1] * (n + 1)
         
+        dp[0] = 0
+        dp[1] = nums[0]
+        
+        for i in range(1,n):
+            dp[i+1] = max(dp[i], dp[i-1] + nums[i])
+        return dp[n]        
