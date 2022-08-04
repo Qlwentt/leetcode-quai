ca86347f8c493da3f791c0913dfdf3a1c23e779a
Quai Wentt <quai.wentt@gmail.com>
2022-08-04 12:39:13 -0400

    Time: 49 ms (56.11%), Space: 14.1 MB (20.05%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {len(s): 1}
+        
+        def getNumDecodings(i):
+            if i in memo:
+                return memo[i]
+            if s[i] == "0":
+                return 0
+            res = getNumDecodings(i+1)
+            
+            if i + 1 < len(s) and (
+                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
+            ):
+                res += getNumDecodings(i+2)
+            
+            memo[i] = res
+            return res
+        return getNumDecodings(0)
