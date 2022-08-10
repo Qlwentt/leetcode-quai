003b96d38c41766d623edfc017fb6556c7cc747a
Quai Wentt <quai.wentt@gmail.com>
2022-08-10 12:49:33 -0400

    Time: 142 ms (6.91%), Space: 16.8 MB (76.07%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]),
+                               max(newInterval[1], intervals[i][1])]
+        result.append(newInterval)
+        return result
