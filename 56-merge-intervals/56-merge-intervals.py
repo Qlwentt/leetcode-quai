4bece2a091baba9da93601fa32be6fdbe8703739
Quai Wentt <quai.wentt@gmail.com>
2022-08-10 13:38:34 -0400

    Time: 200 ms (37.98%), Space: 19.5 MB (20.92%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda i:i[0])
+        output = [intervals[0]]
+        
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            
+            if start <= lastEnd:
+                output[-1][1] = max(end, lastEnd)
+            else:
+                output.append([start,end])
+        return output
+        
