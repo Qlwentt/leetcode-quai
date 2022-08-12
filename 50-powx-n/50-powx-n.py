976bcc188a7c8d58f298bd2dde6a4c3da3598884
Quai Wentt <quai.wentt@gmail.com>
2022-08-12 14:55:29 -0400

    Time: 36 ms (19.70%), Space: 13.4 MB (68.65%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        def powPositive(x,n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+            if x == 0:
+                return 0
+            half = self.myPow(x,n//2)
+            if n % 2 == 0:
+                return half * half
+            else:
+                return x * half * half
+
+        if n > 0:
+            return powPositive(x,n)
+        else:
+            result = powPositive(x,-n)
+            return 1/result
