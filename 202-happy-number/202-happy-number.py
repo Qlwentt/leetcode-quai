fb2bee2b006313c3aeea7337285b33db42c35ec8
Quai Wentt <quai.wentt@gmail.com>
2022-08-14 12:34:03 -0400

    Time: 39 ms (87.16%), Space: 13.9 MB (14.36%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
+class Solution:
+    def isHappy(self, n: int) -> bool:
         seen = set()
         while n != 1:
             seen.add(n)
             n = sum([int(digit)**2 for digit in str(n)])
             if n in seen:
                 return False         
-        return True
+        return True 
