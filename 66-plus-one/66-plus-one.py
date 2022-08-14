8a2f33074d620679a13bd0b512eb0fae56acc395
Quai Wentt <quai.wentt@gmail.com>
2022-08-14 12:35:08 -0400

    Time: 58 ms (32.78%), Space: 14 MB (11.85%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
         strDigits = [str(digit) for digit in digits]
         number = int("".join(strDigits))
         number += 1
         
         number = str(number)
         return [int(digit) for digit in number]
-        
