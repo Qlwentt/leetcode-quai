9e10c8030a620d45bd01e2a992dc0a0643900cc3
Quai Wentt <quai.wentt@gmail.com>
2022-08-18 18:10:20 -0400

    Time: 2090 ms (5.02%), Space: 27.7 MB (81.01%) - LeetHub

---
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumHealth(self, damage: List[int], armor: int) -> int:
+        power = damage
+        maxPower = max(power)
+        def canWin(health, armor):
+            for p in power:
+                if p == maxPower:
+                    health -= (p - armor) if p > armor else 0
+                    armor = 0
+                else:
+                    health -= p
+                if health <= 0:
+                    return False
+            return True
+        low = 0
+        high = sum(power)
+        while low <= high:
+            mid = (low + high) // 2
+            if canWin(mid,armor):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
