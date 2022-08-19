a3bb956cc020d7b056c346a76b9f42c580b39b56
Quai Wentt <quai.wentt@gmail.com>
2022-08-19 11:58:03 -0400

    Time: 119 ms (16.34%), Space: 14.2 MB (45.45%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.py
@@ -0,0 +1,18 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        durations = { k: 0 for k in keysPressed}
+        durations[keysPressed[0]] = releaseTimes[0]
+        for i in range(1,len(releaseTimes)):
+            duration = releaseTimes[i] - releaseTimes[i-1]
+            durForKey = durations[keysPressed[i]]
+            
+            durations[keysPressed[i]] = max(durForKey, duration)
+        print(durations)
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        alphaIndices = {char: -(i+1) for i, char in enumerate(alphabet)}
+        maxHeap = [(-duration, alphaIndices[char]) for char, duration in durations.items()]
+        heapq.heapify(maxHeap)
+        
+        return alphabet[-maxHeap[0][1] -1]
+
+            
