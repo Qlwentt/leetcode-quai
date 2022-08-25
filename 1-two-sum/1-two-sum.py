23a370774320e78a7fa4470b988f1fc942eb95bd
Quai Wentt <quai.wentt@gmail.com>
2022-08-25 16:00:09 -0400

    Time: 151 ms (36.73%), Space: 15.2 MB (50.22%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsDict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            foundIndex = numsDict.get(complement, None)
+            if foundIndex is not None:
+                return [foundIndex, i]
+            else:
+                numsDict[num] = i
