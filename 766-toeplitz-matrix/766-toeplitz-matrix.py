719d41c8c85c72579d1c8d86ccf95ea358c82af8
Quai Wentt <quai.wentt@gmail.com>
2022-09-01 16:28:54 -0400

    Time: 105 ms (77.35%), Space: 14 MB (37.61%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,21 +1,6 @@
 from collections import defaultdict
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        #   0 1 2 3
-        #0 [1,2,3,4],
-        #1 [5,1,2,3],
-        #2 [9,5,1,2]
-        
-        
-      # 0,0 1,1 2,2
-      # 0,1 1,2 2,3
-      # 0,2 1,3
-      # 0,3
-      # 1,0 2,1
-      # 2,0
-        
-      # 0-0 = 0 1-1 = 0 2-2=0
-      # 0-1 = -1 1-2 = -1 2-3 = -1
         numRows = len(matrix)
         numCols = len(matrix[0])
         diagNums = defaultdict(set)
