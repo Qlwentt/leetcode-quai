973b7d1425e6fffdff8e220f9818d8495de987d1
Quai Wentt <quai.wentt@gmail.com>
2023-08-24 13:49:38 -0400

    Time: 119 ms (72.63%), Space: 18.3 MB (41.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,41 +1,42 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # min(i-1,i+1) - i
-        # 0 0,3 = 0 - 0 = 0 = 0
-        # 1 1,3 = 1 - 1 = 0
-        # 2 1,3 = 1 - 0 = 1
-        # 3 2,3 = 2 - 2 = 0
-        # 4 2,3 = 2 - 1 = 1
-        # 5 2,3 = 2 - 0 = 2
-        # 6 2,3 = 2 - 1 = 1
-        # 7 3,3 = 3 - 3 = 0
-        # 8 2,3 = 2 - 2 = 0
-        # 9 2,3 = 2 - 1 = 1
-        # 0 2,3 = 2 - 2 = 0
-        # 1 1,3 = 1 - 1 = 0
+#         # min(L,R) - h[i]
         
-        # [0,1,0,2,1,0,1,3,2,1,2,1]
-        # [3,3,3,3,3,3,3,3,2,2,2,1]
-        # [0,1,1,2,2,2,2,3,3,3,3,3]
-        #  0 1 2 3 4 5 6 7 8 9 0 1
+#         Lmaxes = [0] * len(height)
+#         Rmaxes = [0] * len(height)
         
-        Lmaxes = [0] * len(height)
-        Rmaxes = [0] * len(height)
+#         Lmax = 0
+#         for i in range(len(height)):
+#             Lmax = max(Lmax, height[i])
+#             Lmaxes[i] = Lmax
         
-        Lmax = 0
-        for i in range(len(height)):
-            Lmax = max(Lmax, height[i])
-            Lmaxes[i] = Lmax
-        
-        Rmax = 0
-        for i in range(len(height)-1, -1 , -1):
-            Rmax = max(Rmax,height[i])
-            Rmaxes[i] = Rmax
+#         Rmax = 0
+#         for i in range(len(height)-1, -1 , -1):
+#             Rmax = max(Rmax,height[i])
+#             Rmaxes[i] = Rmax
             
+#         water = 0
+#         for i, elevation in enumerate(height):
+#             water += min(Lmaxes[i], Rmaxes[i]) - elevation
+            
+#         return water
+        L = 0
+        R = len(height) - 1
+        
         water = 0
-        for i, elevation in enumerate(height):
-            water += min(Lmaxes[i], Rmaxes[i]) - elevation
-            
+        Lmax = 0
+        Rmax = 0
+        while L < R:
+            if height[L] < height[R]:
+                Lmax = max(Lmax, height[L])
+                water += Lmax - height[L]
+                L +=1
+            else:
+                Rmax = max(Rmax, height[R])
+                water += Rmax - height[R]
+                R -= 1
         return water
+                
+            
             
         
