493114d6f1e579c4c2566de06af04a0e716a7579
Quai Wentt <quai.wentt@gmail.com>
2023-08-25 17:00:31 -0400

    Time: 40 ms (77.45%), Space: 16.4 MB (33.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,22 +5,14 @@ class Solution:
         #      R
         # [2,3,3,3]
 
-        # [1,3,0,4,2,2,2,2] 2 
-        #          L
-        #                R
-        L = 0
-        R = 0
-        k = 0 # 1 2 3 4
-        while R < len(nums):
-            if nums[L] == val:
-                while nums[R] == val:
-                    R +=1
-                    if R >= len(nums):
-                        return k
-                nums[L], nums[R] = nums[R], nums[L]
-            else:
-                R += 1
-            k += 1
-            L += 1
+        # [0,1,3,0,4,2,2,2] 2 
+        #            k        
+        #                  i
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
         return k
+  
         
