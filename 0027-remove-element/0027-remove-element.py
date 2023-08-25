8e28fa7800606dfb740a8a3b45cc3fa0d65d96e9
Quai Wentt <quai.wentt@gmail.com>
2023-08-25 16:55:13 -0400

    Time: 43 ms (66.34%), Space: 16.4 MB (33.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,12 +1,26 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] == val:
-                nums[i] = float("inf")
-                count += 1
-        
-        nums.sort()
-        return len(nums) - count
-                                
+        # [3,3,2,3] 3
+        #  L
+        #      R
+        # [2,3,3,3]
+
+        # [1,3,0,4,2,2,2,2] 2 
+        #          L
+        #                R
+        L = 0
+        R = 0
+        k = 0 # 1 2 3 4
+        while R < len(nums):
+            if nums[L] == val:
+                while nums[R] == val:
+                    R +=1
+                    if R >= len(nums):
+                        return k
+                nums[L], nums[R] = nums[R], nums[L]
+            else:
+                R += 1
+            k += 1
+            L += 1
+        return k
         
