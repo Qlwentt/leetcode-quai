9560d6446090ad887a90c29eb2902d1baf40a525
Quai Wentt <quai.wentt@gmail.com>
2023-08-26 14:50:37 -0400

    Time: 46 ms (47.74%), Space: 16.4 MB (38.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,19 +1,40 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 1:
-            return [[1]]
-        elif numRows == 2:
-            return [[1],[1,1]]
-        else:
-            answer = self.generate(numRows-1)
-            previous = answer[-1]
-            new = []
-            new.append(1)
-            for i in range(len(previous)-1):
-                new.append(previous[i] + previous[i+1])
-            new.append(1)
-            answer.append(new)
+#         if numRows == 1:
+#             return [[1]]
+#         elif numRows == 2:
+#             return [[1],[1,1]]
+#         else:
+#             answer = self.generate(numRows-1)
+#             previous = answer[-1]
+#             new = []
+#             new.append(1)
+#             for i in range(len(previous)-1):
+#                 new.append(previous[i] + previous[i+1])
+#             new.append(1)
+#             answer.append(new)
             
-            return answer
+#             return answer
+        answer = []
+        def helper(rows):
+            if rows == 1:
+                answer.append([1])
+                return answer
+            elif rows == 2:
+                previous = helper(rows-1)[-1]
+                answer.append([1,1])
+                return answer
+            else:
+                previous = helper(rows-1)[-1]
+                new = []
+                new.append(1)
+                for i in range(len(previous)-1):
+                    new.append(previous[i] + previous[i+1])
+                new.append(1)
+                answer.append(new)
+
+                return answer
+        return helper(numRows)
+                
        
         
