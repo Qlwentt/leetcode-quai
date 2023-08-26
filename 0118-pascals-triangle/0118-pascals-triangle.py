ecb8b9e2a2b99ad4106f9f2777a3bc509a096d8c
Quai Wentt <quai.wentt@gmail.com>
2023-08-26 14:58:19 -0400

    Time: 53 ms (17.81%), Space: 16.3 MB (72.81%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -15,26 +15,45 @@ class Solution:
 #             answer.append(new)
             
 #             return answer
-        answer = []
-        def helper(rows):
-            if rows == 1:
-                answer.append([1])
-                return answer
-            elif rows == 2:
-                previous = helper(rows-1)[-1]
-                answer.append([1,1])
-                return answer
-            else:
-                previous = helper(rows-1)[-1]
-                new = []
-                new.append(1)
-                for i in range(len(previous)-1):
-                    new.append(previous[i] + previous[i+1])
-                new.append(1)
-                answer.append(new)
 
-                return answer
-        return helper(numRows)
+#         answer = []
+#         def helper(rows):
+#             if rows == 1:
+#                 answer.append([1])
+#                 return answer
+#             elif rows == 2:
+#                 previous = helper(rows-1)[-1]
+#                 answer.append([1,1])
+#                 return answer
+#             else:
+#                 previous = helper(rows-1)[-1]
+#                 new = []
+#                 new.append(1)
+#                 for i in range(len(previous)-1):
+#                     new.append(previous[i] + previous[i+1])
+#                 new.append(1)
+#                 answer.append(new)
+
+#                 return answer
+#         return helper(numRows)
+        if numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1], [1,1]]
+        
+        answer = [[1],[1,1]]
+        
+        for _ in range(numRows-2):
+            previous = answer[-1]
+            new = []
+            new.append(1)
+            for i in range(len(previous)-1):
+                new.append(previous[i] + previous[i+1])
+            new.append(1)
+            answer.append(new)
+        return answer
+            
+        
                 
        
         
