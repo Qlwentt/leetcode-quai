8dd896d992782808d1eee06f7e2696bfb433e9a6
Quai Wentt <quai.wentt@gmail.com>
2023-08-29 17:48:59 -0400

    Time: 206 ms (50.02%), Space: 20.8 MB (53.86%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,12 +3,14 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        p1 = 0
-        p2 = len(s) - 1
+        L = 0
+        R = len(s) - 1
         
-        while p1 < p2:
-            s[p1], s[p2] = s[p2], s[p1]
-            p1 += 1
-            p2 -= 1
+        while L < R:
+            s[L], s[R] = s[R], s[L]
+            L += 1
+            R -= 1
+            
+        
         
         
