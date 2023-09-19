be29a2027b6d44a4031e1189233057e49c954d43
Quai Wentt <quai.wentt@gmail.com>
2023-09-18 21:09:29 -0400

    Time: 43 ms (32.28%), Space: 16.3 MB (47.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,18 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         parens = {
-            "(" : ")",
+            "{" : "}",
             "[" : "]",
-            "{" : "}"
+            "(" : ")"
         }
-
+        
         for char in s:
-            top = stack[-1] if stack else ""
             if char in parens:
                 stack.append(char)
-            elif char == parens.get(top, None):
+            elif stack and parens[stack[-1]] == char:
                 stack.pop()
             else:
                 return False
         return len(stack) == 0
-        
