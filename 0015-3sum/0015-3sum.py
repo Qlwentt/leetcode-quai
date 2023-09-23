c143d14b709ab9a63738f205d6c35b8d8c6a7df1
Quai Wentt <quai.wentt@gmail.com>
2023-09-23 13:08:17 -0400

    Time: 2179 ms (23.50%), Space: 20.9 MB (22.32%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # answer = set()
-        # for i in range(len(nums)):
-        #     target = 0 - nums[i]
-        #     restOfArray = nums[:i]+nums[i+1:]
-        #     complements = set()
-        #     for num in restOfArray:
-        #         if target - num in complements:            
-        #             ele = [nums[i], num, target - num]
-        #             ele.sort()
-        #             answer.add(tuple(ele))
-        #         complements.add(num)
-        # return answer
+        result = set()
+        
         nums.sort()
-        answer = set()
         for i in range(len(nums)):
-            lo = i + 1
-            hi = len(nums) - 1
-            while lo < hi:
-                currentSum = nums[lo] + nums[hi] + nums[i]
-                if currentSum > 0:
-                    hi -= 1
-                elif currentSum < 0:
-                    lo += 1
-                else: # currentSum == 0
-                    answer.add((nums[lo], nums[hi], nums[i]))
-                    lo += 1
-                    hi -= 1
-        return answer
+            low = i + 1
+            high = len(nums) - 1
+            while low < high:
+                if nums[low] + nums[high] + nums[i] > 0:
+                    high -= 1
+                elif nums[low] + nums[high] + nums[i] < 0:
+                    low += 1
+                else:
+                    tup = (nums[low], nums[high], nums[i])
+                    result.add(tup)
+                    low += 1
+                    high -= 1
+                    
+        return result
 
 
             
