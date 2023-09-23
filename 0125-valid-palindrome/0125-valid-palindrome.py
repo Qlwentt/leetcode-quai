1a21c757f1829326240c5dec90953bc8bcf5fbbe
Quai Wentt <quai.wentt@gmail.com>
2023-09-23 12:58:07 -0400

    Time: 53 ms (46.16%), Space: 17.1 MB (60.10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,13 +6,14 @@ class Solution:
         while L < R:
             if not s[L].isalnum():
                 L += 1
-                continue
-            if not s[R].isalnum():
+            elif not s[R].isalnum():
                 R -= 1
-                continue
-            if s[L].lower() != s[R].lower():
+            elif s[L].lower() != s[R].lower():
                 return False
-            L += 1
-            R -= 1
+            else:
+                L += 1
+                R -= 1
+                
         return True
+            
             
