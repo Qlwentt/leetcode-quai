7e1f633f70f1c96a8ab8eece9fd4193ab2132aa5
Quai Wentt <quai.wentt@gmail.com>
2023-09-29 22:17:16 -0400

    Time: 891 ms (29.42%), Space: 64.8 MB (25.68%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        combs = []
+        
+        def backtrack(i, curComb):
+            if len(curComb) == k:
+                combs.append(curComb.copy())
+                return
+            if i > n:
+                return
+             
+            for j in range(i, n+1):
+                curComb.append(j)
+                backtrack(j+1, curComb)
+                curComb.pop()
+
+        backtrack(1, [])
+        return combs
