a60393c4a573441ade0f9239702968a3ecf95863
Quai Wentt <quai.wentt@gmail.com>
2023-10-01 14:16:55 -0400

    Time: 69 ms (29.10%), Space: 16.4 MB (74.71%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,24 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
         combs = []
+        nums = candidates
 
         def backtrack(i, curComb, curSum):
             if curSum == target:
                 combs.append(curComb.copy())
                 return
-            if i >= len(candidates) or curSum > target:
+            
+            if i >= len(nums) or curSum > target:
                 return
             
-            # include number between 1 and unlimited times
-            curComb.append(candidates[i])
-            curSum += candidates[i]
-            backtrack(i, curComb, curSum)
-               
+            # choose num between 1 and unlimited times
+            curComb.append(nums[i])
+            backtrack(i, curComb, curSum + nums[i])
+            
+            # skip num
             curComb.pop()
-            curSum -= candidates[i]
-            # don't include number
             backtrack(i+1, curComb, curSum)
-                
+        
         backtrack(0, [], 0)
         return combs
