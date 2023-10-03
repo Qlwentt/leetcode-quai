11378e78d365cfd9fddc90cc759d9fba5e2d6c3e
Quai Wentt <quai.wentt@gmail.com>
2023-10-03 16:31:12 -0400

    Time: 59 ms (58.42%), Space: 16.7 MB (89.72%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        rows = defaultdict(lambda: False)
+        cols = defaultdict(lambda: False)
+        xdiag = defaultdict(lambda: False)
+        ydiag = defaultdict(lambda: False)
+        answer = []
+        def backtrack(row, col, curBoard):
+            if len(curBoard) == n:
+                answer.append(curBoard.copy())
+                return
+
+            line = ["."] * n
+            for c in range(n):
+                if rows[row] or cols[c] or xdiag[c-row] or ydiag[c+row]:
+                    continue
+                rows[row] = True
+                cols[c] = True
+                xdiag[c-row] = True
+                ydiag[c+row] = True
+                line[c] = "Q"
+                
+                curBoard.append("".join(line))
+                backtrack(row+1, c, curBoard) 
+                curBoard.pop()
+                
+                line[c] = "."
+                cols[c] = False
+                xdiag[c-row] = False
+                ydiag[c+row] = False
+                rows[row] = False
+        backtrack(0,0,[])
+        return answer
+            
+        
