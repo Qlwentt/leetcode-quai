7041b4dbb451246659e4ee49c38b29219b7e115d
Quai Wentt <quai.wentt@gmail.com>
2023-10-04 21:35:19 -0400

    Time: 790 ms (8.95%), Space: 18 MB (28.84%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,28 +5,26 @@ class Solution:
         COLS = len(grid[0])
         
         visited = set([(0,0)])
-        q = deque([(0,0)])
-        level = 1
+        q = deque([(0,0,1)])
         while q:
-            for _ in range(len(q)):
-                r,c = q.popleft()
-                if grid[r][c] == 1:
-                    continue
-                if r == ROWS - 1 and c == COLS - 1:
-                    return level
-                # up, down, left, right, up+left, up+right, down+left, down+right
-                directions = [[-1, 0], [1,0], [0,-1], [0,1], [-1,-1], [1,1], [-1,1], [1,-1]]
-                for dr, dc in directions:
-                    newRow = dr + r
-                    newCol = dc + c
-                    if (newRow in range(ROWS) and 
-                        newCol in range(COLS) and
-                        (newRow,newCol) not in visited and
-                        grid[newRow][newCol] == 0
-                       ):
-                        q.append((newRow, newCol))
-                        visited.add((newRow, newCol))
+            r,c,level = q.popleft()
+            if grid[r][c] == 1:
+                continue
+            if r == ROWS - 1 and c == COLS - 1:
+                return level
+            # up, down, left, right, up+left, up+right, down+left, down+right
+            directions = [[-1, 0], [1,0], [0,-1], [0,1], [-1,-1], [1,1], [-1,1], [1,-1]]
+            for dr, dc in directions:
+                newRow = dr + r
+                newCol = dc + c
+                if (newRow in range(ROWS) and 
+                    newCol in range(COLS) and
+                    (newRow,newCol) not in visited and
+                    grid[newRow][newCol] == 0
+                   ):
+                    q.append((newRow, newCol, level+1))
+                    visited.add((newRow, newCol))
                     
-            level += 1
+            
         return -1
             
