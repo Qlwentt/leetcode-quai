3b7179c22b6be80aa1cc2c2f6545acd6479a5bf7
Quai Wentt <quai.wentt@gmail.com>
2023-10-06 18:17:22 -0400

    Time: 101 ms (52.26%), Space: 19.4 MB (46.46%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjList = { course: [] for course in range(numCourses)}
+        
+        for course, pre in prerequisites:
+            adjList[course].append(pre)
+            
+        order = []
+        visited = set()
+        visiting = set()
+        
+        def dfs(course):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+            
+            visited.add(course)
+            visiting.add(course)
+            
+            for neigh in adjList[course]:
+                if not dfs(neigh):
+                    return False
+            
+            order.append(course)
+            visiting.remove(course)
+            return True
+        
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return []
+        return order
