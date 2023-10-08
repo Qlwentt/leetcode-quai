b21ac14319fbf979e521582f9f5953e76444209e
Quai Wentt <quai.wentt@gmail.com>
2023-10-08 12:15:00 -0400

    Time: 138 ms (56.89%), Space: 18.4 MB (41.47%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROWS = len(board)
+        COLS = len(board[0])
+        
+        dontFlip = set()
+        
+        # mark all ones you shouldn't flip
+        def dfs(row,col):
+            if (row < 0 or col < 0 or 
+                row >= ROWS or col >= COLS or
+                board[row][col] == "X" or
+                (row,col) in dontFlip
+               ):
+                return
+            
+            dontFlip.add((row,col))
+        
+            dfs(row-1, col)
+            dfs(row+1, col)
+            dfs(row, col-1)
+            dfs(row, col+1)
+        
+        # run dfs to find all the ones you shouldn't flip
+        for c in range(COLS):
+            dfs(0,c)
+            dfs(ROWS-1,c)
+        
+        for r in range(ROWS):
+            dfs(r,0)
+            dfs(r, COLS-1)
+        print(dontFlip)
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == "O" and (row,col) not in dontFlip:
+                    board[row][col] = "X"
+        
+                
+        
