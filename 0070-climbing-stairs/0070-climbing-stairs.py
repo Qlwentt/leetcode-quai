3d096c51ef2de9899263e57e7b481f97d90ef604
Quai Wentt <quai.wentt@gmail.com>
2023-10-12 17:38:47 -0400

    Time: 42 ms (23.65%), Space: 16.1 MB (79.20%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @functools.lru_cache
+        cache = {}
         def fib(n):
             if n <= 1:
                 return n
-            return fib(n-1) + fib(n-2)
+            if n in cache:
+                return cache[n]
+            cache[n] = fib(n-1) + fib(n-2)
+            return cache[n]
         return fib(n+1)
         
