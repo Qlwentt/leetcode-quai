a13b3a7a21816db86109f1a9771e3df2cf155f43
Quai Wentt <quai.wentt@gmail.com>
2023-10-14 17:38:28 -0400

    Time: 34 ms (78.21%), Space: 16.3 MB (47.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
         def fib(n):
             if n <= 1:
                 return n
-
-            nMinus1 = 1
-            nMinus2 = 0
             
-            while n > 1 :
-                fibN = nMinus1 + nMinus2
-                nMinus1, nMinus2 = fibN, nMinus1
-                n -= 1
+            dp = [0] * (n+1)
+            dp[1] = 1
             
-            return fibN
-                
-        
-        
+            i = 2
+            while i <= n:
+                dp[i] = dp[i-1] + dp[i-2]
+                i += 1         
+            return dp[n]
         return fib(n+1)
