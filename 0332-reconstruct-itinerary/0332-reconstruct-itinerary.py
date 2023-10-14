70d6e9c43a78f134f9b2061e4deff3adb091e1f7
Quai Wentt <quai.wentt@gmail.com>
2023-10-14 17:41:36 -0400

    Time: 73 ms (97.40%), Space: 17.1 MB (20.60%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,15 +1,13 @@
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj, weights = {}, {}
+        adj = {}
         for src, dst, in tickets:
             if src not in adj:
                 adj[src] = []
-                weights[src] = self.calc_lex(src)
             if dst not in adj:
                 adj[dst] = []
-                weights[dst] = self.calc_lex(dst)
-            heapq.heappush(adj[src], (weights[dst], dst))
+            heapq.heappush(adj[src], (dst))
 
         topo = []
 
@@ -18,7 +16,7 @@ class Solution:
                 return
             
             while adj[root]:
-                cost, nei = heapq.heappop(adj[root])
+                nei = heapq.heappop(adj[root])
                 topsort(nei, adj)
 
             topo.append(root)
@@ -27,6 +25,4 @@ class Solution:
         topsort('JFK', adj)
         topo.reverse()
         return topo
-    # weighted lexicographic cost (first char has most weight, second has second-most, etc.)
-    def calc_lex(self, s):
-        return sum([(ord(c) * ((3 - i)**((3 - i) + (3 - i)))) for i, c in enumerate(s)])
+  
