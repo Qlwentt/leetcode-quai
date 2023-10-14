8cd8f9438b2b8125a573f286c5b73d548229f61b
Quai Wentt <quai.wentt@gmail.com>
2023-10-14 17:30:19 -0400

    Time: 64 ms (36.96%), Space: 16.4 MB (46.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,42 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-# recursive
-#         def climb(i, curCost):
-#             if i >= len(cost) - 2:
-#                 nextCost = cost[i+1] if i+1 in range(len(cost)) else 0
-#                 curCost += min(cost[i], nextCost)
-#                 return curCost
-            
-#             return min(climb(i+1, curCost+cost[i]), climb(i+2,curCost + cost[i+1]))
-#         return climb(0, 0)
-
-# memoization
-#         memo = {}
-#         def climb(i):
-#             if i >= len(cost) - 2:
-#                 nextCost = cost[i+1] if i+1 in range(len(cost)) else 0
-#                 minCost = min(cost[i], nextCost)
-#                 return minCost
-#             if i in memo:
-#                 return memo[i]
-#             memo[i] = min(cost[i] + climb(i+1), cost[i+1] + climb(i+2))
-#             return memo[i]
+        n = len(cost)
+        dp = [0] * (n+2)
+        dp[n-1] = cost[n-1]
         
-#         return climb(0)
-
-# # dp
-        def climb():
-            n = len(cost)
-
-            dp = [-1]*n
-            dp[n-2] = min(cost[n-2], cost[n-1]) 
-            dp[n-1] = min(cost[n-1], 0)
-            
-            i = n-3
-            while i >= 0:
-                dp[i] = min(cost[i] + dp[i+1], cost[i+1] + dp[i+2])
-                i -= 1
-            return dp[0]
-        return climb()
-        
-        
+        i = n - 2
+        while i >= 0:
+            dp[i] = min(cost[i] + dp[i+1], cost[i] + dp[i+2])
+            i -= 1
+        print(dp)
+        return min(dp[0], dp[1])
