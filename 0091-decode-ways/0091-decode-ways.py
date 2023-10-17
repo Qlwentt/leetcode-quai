64c7b3f3cfac60f790cf99ebcf0e86fa2a57e3bc
Quai Wentt <quai.wentt@gmail.com>
2023-10-17 19:51:47 -0400

    Time: 47 ms (14.58%), Space: 16.5 MB (17.49%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,29 +1,43 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         numMap = {str(num+1): letter for num, letter in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
-        memo = {}
-        def backtrack(i):
-            if i > len(s):
-                return 0
-            if i == len(s):
-                return 1
-            if i in memo:
-                return memo[i]
+#         memo = {}
+#         def backtrack(i):
+#             if i > len(s):
+#                 return 0
+#             if i == len(s):
+#                 return 1
+#             if i in memo:
+#                 return memo[i]
             
-            res = 0
-            # take only one num
-            nextNum = s[i]
-            if nextNum in numMap:
-                res += backtrack(i+1)
+#             res = 0
+#             # take only one num
+#             nextNum = s[i]
+#             if nextNum in numMap:
+#                 res += backtrack(i+1)
             
-            # take two nums
-            nextNum = s[i:i+2]
-            if nextNum in numMap:
-                res += backtrack(i+2)
+#             # take two nums
+#             nextNum = s[i:i+2]
+#             if nextNum in numMap:
+#                 res += backtrack(i+2)
 
-            memo[i] = res    
-            return res
+#             memo[i] = res    
+#             return res
                 
-        return backtrack(0)
+#         return backtrack(0)
+        def decodingDP():
+            n = len(s)
+            dp = [0]*(n+1)
+            dp[n-1], dp[n] = 1 if s[-1] in numMap else 0 ,1
+            
+            i = n-2
+            while i >= 0:
+                print(s[i:i+2])
+                dp[i] = (dp[i+1] if s[i] in numMap else 0) + (dp[i+2] if s[i:i+2] in numMap else 0)
+                i -= 1
+            print(dp)
+            return dp[0]
+        
+        return decodingDP()
             
         
