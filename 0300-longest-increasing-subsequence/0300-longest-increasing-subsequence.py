a6ba339745581a0fe6874b94da60d80fb63dd0fd
Quai Wentt <quai.wentt@gmail.com>
2023-10-21 11:26:46 -0400

    Time: 3669 ms (5.00%), Space: 20.7 MB (5.13%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,52 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # not optimal memoization - TLE
+#         memo = {i: {} for i in range(len(nums))}
+#         def backtrack(i, prev):
+#             if i >= len(nums):
+#                 return 0
+            
+#             if prev in memo[i]:
+#                 return memo[i][prev]
+
+#             take = 0
+#             num = nums[i]
+#             if num > prev:
+#                 # take number
+#                 take = 1 + backtrack(i+1, num)
+#             # skip number
+#             skip = backtrack(i+1, prev)
+
+#             memo[i][prev] = max(take,skip)
+#             return memo[i][prev]
+        
+#         return backtrack(0, float('-inf'))
+
+# memoization
+        memo = {}
+        def backtrack(i):
+            if i >= len(nums):
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            
+            maxTake = 1
+            for j in range(i+1, len(nums)):
+                take = 1
+                if nums[i] < nums[j]:
+                    take += backtrack(j)
+                    maxTake = max(take,maxTake)
+                                
+            memo[i] = maxTake
+            return memo[i]
+        
+        answer = 1
+        for i in range(len(nums)):
+            answer = max(answer, backtrack(i))
+        return answer
+                
+        
+        
+
+        
