4590cf21f34e7efa1b99921eeacd1663bf0ec4dd
Quai Wentt <quai.wentt@gmail.com>
2023-10-22 14:36:28 -0400

    Time: 42 ms (32.68%), Space: 16.2 MB (65.50%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,32 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+    # memoization
+#         memo = {}
+#         def backtrack(row, col):
+#             if row >= m or col >= n:
+#                 return 0
+#             if (row,col) == ((m-1), (n-1)):
+#                 return 1
+#             if (row,col) in memo:
+#                 return memo[(row,col)]
+
+#             left = backtrack(row, col+1)
+#             down = backtrack(row+1, col)
+
+#             memo[(row,col)] = left + down
+#             return memo[(row,col)]
+
+#         return backtrack(0,0)
+    
+    # dp
+        dp = [[0]*(n+1) for _ in range(m+1) ]
+        dp[m-1][n-1] = 1
+        
+        for row in range(m-1, -1,-1):
+            for col in range(n-1, -1,-1):
+                if (row, col) == (m-1, n-1):
+                    continue
+                dp[row][col] = dp[row][col+1] + dp[row+1][col]
+                
+        return dp[0][0]
+        
