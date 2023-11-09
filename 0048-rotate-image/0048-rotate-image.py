32511214830c757ea5df93aa599314e258e31c89
Quai Wentt <quai.wentt@gmail.com>
2023-11-08 18:56:28 -0600

    Time: 47 ms (22.61%), Space: 16.3 MB (21.60%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,15 +6,45 @@ class Solution:
         # row becomes column, col becomes row (transpose)
         # reverse
         
-        N = len(matrix)
+#         N = len(matrix)
         
-        offset = 0
-        for row in range(N):
-            for col in range(offset, N):
-                matrix[row][col] , matrix[col][row] = matrix[col][row], matrix[row][col]
-            offset += 1
-        for row in matrix:
-            row.reverse()
+#         offset = 0
+#         for row in range(N):
+#             for col in range(offset, N):
+#                 matrix[row][col] , matrix[col][row] = matrix[col][row], matrix[row][col]
+#             offset += 1
+#         for row in matrix:
+#             row.reverse()
+
+        # rotate one by one
+        N = len(matrix)
+        TOP = 0
+        BOT = N - 1
+        while TOP < BOT:
+            L = TOP
+            R = BOT
+            for offset in range(R-L):
+                topLeft = matrix[TOP][L+offset]
+                topRight = matrix[TOP+offset][R]
+                botLeft = matrix[BOT-offset][L]
+                botRight = matrix[BOT][R-offset]
+                # rotate
+                # top left goes to top right
+                # top right goes to bottom right
+                # bottom right goes to bottom left
+                # bottom left goes to top left
+
+                matrix[TOP+offset][R] = topLeft
+                matrix[BOT][R-offset] = topRight
+                matrix[BOT-offset][L] = botRight
+                matrix[TOP][L+offset] = botLeft
+                offset += 1
+            TOP += 1
+            BOT -= 1
+        
+                
+                
+        
             
             
         
