25fbc9f63a775f09138ee8dbf649ed8f8b54657d
Quai Wentt <quai.wentt@gmail.com>
2023-11-11 12:50:00 -0600

    Time: 43 ms (38.54%), Space: 16.2 MB (61.97%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        power = 31
+        answer = 0
+        while n:
+            if n & 1:
+                answer += 2 ** power
+            power -= 1
+            n = n >> 1
+        return answer
