b4f4952503f23fafe2ee91ef3c25762db9e5043b
Quai Wentt <quai.wentt@gmail.com>
2023-11-19 16:01:46 -0600

    Time: 87 ms (15.51%), Space: 16.4 MB (77.83%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -0,0 +1,22 @@
+import re
+from collections import defaultdict
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        stack = []
+        exTime = [0] * n
+        for log in logs:
+            i, command, time = re.match("(\d+):(start|end):(\d+)", log).groups()
+            i = int(i)
+            time = int(time)
+            
+            if command == "start":
+                stack.append((i, time))
+            else:
+                startI, start = stack.pop()
+                end = time
+                xtime = end - start + 1
+                exTime[startI] += xtime
+                if stack:
+                    j, start = stack[-1]
+                    exTime[j] -= xtime
+        return exTime
