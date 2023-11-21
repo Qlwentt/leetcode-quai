7ea08a1c678882b09974707e5c600985a46472fa
Quai Wentt <quai.wentt@gmail.com>
2023-11-21 14:38:59 -0600

    Time: 170 ms (84.30%), Space: 21.5 MB (49.33%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,25 @@
+from random import randint
+from bisect import bisect_left
+class Solution:
+
+    def __init__(self, w: List[int]):    
+        self.choices = [0]*len(w)
+        self.sumWeights = sum(w)
+        runSum = 0
+        for i, weight in enumerate(w):
+            runSum += weight
+            self.choices[i] = runSum
+        print(self.choices)
+        
+            
+
+    def pickIndex(self) -> int:
+        
+        key = randint(1, self.sumWeights)
+        return bisect_left(self.choices, key)
+    
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
