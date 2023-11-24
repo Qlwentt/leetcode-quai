588b00cde72f44fce148ed877ae3bc8f4fd48296
Quai Wentt <quai.wentt@gmail.com>
2023-11-24 16:01:21 -0600

    Time: 86 ms (57.67%), Space: 16.3 MB (63.49%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,14 +1,15 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        diag = {}
         ROWS = len(matrix)
         COLS = len(matrix[0])
         
         for row in range(ROWS):
             for col in range(COLS):
                 item = matrix[row][col]
-                existing = diag.get(row-col, None)
-                if existing != None and existing != item:
-                    return False
-                diag[row-col] = item
+                top = row - 1
+                left = col - 1
+                if top in range(ROWS) and left in range(COLS):
+                    if matrix[top][left] != item:
+                        return False
+                
         return True
