d9d051ec344de039d1b78c5a2e584002396d3aa6
Quai Wentt <quai.wentt@gmail.com>
2024-01-10 20:35:58 -0500

    Time: 537 ms (77.24%), Space: 19.4 MB (5.73%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,10 +1,9 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        prevMax = -1
-        prev = -1
-        for i in range(len(arr)-1, -1, -1):
-            maxRight = max(prev, prevMax)
-            prev = arr[i]
-            prevMax = maxRight
-            arr[i] = prevMax
-        return arr
+        answer = [-1] * len(arr)
+        
+        maxRight = -1
+        for i in range(len(answer)-2,-1,-1):
+            maxRight = max(maxRight, arr[i+1])
+            answer[i] = maxRight
+        return answer
