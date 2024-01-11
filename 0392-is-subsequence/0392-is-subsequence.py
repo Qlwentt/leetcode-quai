2ccc19945af6cefdcd50b72027978b08e7efcbad
Quai Wentt <quai.wentt@gmail.com>
2024-01-11 17:44:32 -0500

    Time: 27 ms (98.22%), Space: 17.8 MB (15.16%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,38 @@
+from collections import defaultdict
+import bisect
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pS = 0
-        pT = 0
+#         pS = 0
+#         pT = 0
         
-        while pS < len(s) and pT < len(t):
-            if s[pS] == t[pT]:
-                pS += 1
-                pT += 1
+#         while pS < len(s) and pT < len(t):
+#             if s[pS] == t[pT]:
+#                 pS += 1
+#                 pT += 1
+#             else:
+#                 pT += 1
+        
+#         return pS == len(s)
+
+        indexes = defaultdict(list)
+        for i, char in enumerate(t):
+            indexes[char].append(i)
+        
+        currMatchI = -1
+        for char in s:
+            if char not in indexes:
+                return False
+        
+            matchI = bisect.bisect(indexes[char], currMatchI)
+            if matchI != len(indexes[char]):
+                currMatchI = indexes[char][matchI]
             else:
-                pT += 1
+                return False
         
-        return pS == len(s)
+        return True
+            
+            
+    
         
         
         
