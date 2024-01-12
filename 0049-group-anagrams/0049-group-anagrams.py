e68a659ede4eb59dcf0b7b80b33457580012bc0d
Quai Wentt <quai.wentt@gmail.com>
2024-01-12 13:21:47 -0500

    Time: 118 ms (20.32%), Space: 21.1 MB (38.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
+        words = defaultdict(list)
         for s in strs:
-            hashedWord = { char: 0 for char in "abcdefghijklmnopqrstuvwxyz"}
+            hashedWord = {char: 0 for char in "abcdefghijklmnopqrstuvwxyz"}
             for char in s:
                 hashedWord[char] += 1
-            anagrams[hash(tuple(hashedWord.items()))].append(s)
-        return anagrams.values()
+            words[hash(tuple(hashedWord.items()))].append(s)
+            
+        return words.values()
