0b71c910d4e7bc7422b45fcbb0a8c9b48d95e671
Quai Wentt <quai.wentt@gmail.com>
2024-01-13 10:16:34 -0500

    Time: 218 ms (61.05%), Space: 24 MB (22.45%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -5,27 +5,32 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         q = deque([])
-        visited = set()
         ROWS = len(rooms)
         COLS = len(rooms[0])
+        visited = set()
+        # initialize q with gates
         
-        for row in range(ROWS):
-            for col in range(COLS):
-                if rooms[row][col] == 0:
-                    q.append((row,col, 0))
-                    visited.add((row,col))
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append((r,c, 0))
+                    visited.add((r,c))
                     
-        directions = [[0,-1],[0,1],[-1,0],[1,0]]           
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
         while q:
             r, c, distance = q.popleft()
             rooms[r][c] = distance
             
             for dr, dc in directions:
-                row,col = r + dr, c + dc
-                if (row in range(ROWS) and col in range(COLS) and
-                    (row,col) not in visited and
-                    rooms[row][col] != -1
-                ):
-                    q.append((row, col, distance + 1))
-                    visited.add((row,col))
+                newR = dr + r
+                newC = dc + c
+                if (newR in range(ROWS) and 
+                    newC in range(COLS) and
+                    (newR, newC) not in visited and
+                   rooms[newR][newC] != -1):
+                    q.append((newR, newC, distance + 1))
+                    visited.add((newR, newC))
+                    
         
+                    
+            
