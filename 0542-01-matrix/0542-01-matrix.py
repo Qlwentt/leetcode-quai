545dbfcf86d7b8c684ac3f9bc03f7ae82a5709fb
Quai Wentt <quai.wentt@gmail.com>
2024-01-13 11:17:56 -0500

    Time: 605 ms (35.69%), Space: 20.6 MB (35.89%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,30 @@
+from collections import deque
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        ROWS = len(mat)
+        COLS = len(mat[0])
+        visited = set()
+        q = deque([])
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if mat[r][c] == 0:
+                    q.append((r,c, 0))
+                    visited.add((r,c))
+                    
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        while q:
+            r,c,distance = q.popleft()
+            mat[r][c] = distance
+            
+            for dr, dc in directions:
+                newR = dr + r
+                newC = dc + c
+                if (newR in range(ROWS) and
+                    newC in range(COLS) and
+                    (newR, newC) not in visited
+                   ):
+                    q.append((newR,newC, distance + 1))
+                    visited.add((newR,newC))
+        return mat
+        
