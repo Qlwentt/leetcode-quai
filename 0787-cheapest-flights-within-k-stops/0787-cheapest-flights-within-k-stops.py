b5cf93825a2625003d69b0bff578e6bf3cd5130e
Quai Wentt <quai.wentt@gmail.com>
2024-01-14 13:02:59 -0500

    Time: 76 ms (99.96%), Space: 18.8 MB (23.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,26 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        minHeap = [(0,src,0)] # price , node, stops
         adjList = {i: [] for i in range(n)}
+        minHeap = [(0,src, 0)] # price, node, stops
         
         for from_, to, price in flights:
             adjList[from_].append((to, price))
-        visited = {}
+        cheapestPath = {i: (float(inf), float(inf)) for i in range(n)  } # price, stops 
         while minHeap:
             curPrice, node, stops = heapq.heappop(minHeap)
             
-            
-            
-            if node in visited and visited[node] <= stops:
+            if curPrice >= cheapestPath[node][0] and stops >= cheapestPath[node][1]:
                 continue
-            visited[node] = stops
-            
+            cheapestPath[node] = (curPrice, stops)
+                        
             if node == dst and stops-1 <= k:
                 return curPrice
-                          
-            for neigh, price in adjList[node]:
-                heapq.heappush(minHeap, (price+curPrice, neigh, stops+1))
-        
+            
+            for neigh, neighPrice in adjList[node]:
+                heapq.heappush(minHeap, (curPrice+neighPrice, neigh, stops+1))
         return -1
-        
         
