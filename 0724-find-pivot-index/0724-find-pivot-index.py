357253c0c4a1cc7d09983c026da316c61712fffc
Quai Wentt <quai.wentt@gmail.com>
2024-01-16 17:12:13 -0500

    Time: 116 ms (96.23%), Space: 18.6 MB (27.51%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,25 +1,23 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # [1 ,7 , 3 , 6 ,5 ,6 ]
-        # [0 , 1, 8 , 11,17,22]
-        # [27,20, 17, 11, 6, 0]
-        
-        prefixSums = [0] * len(nums)
-        postfixSums = [0] * len(nums)
-        
-        prefixSum = 0
+    #   [1, 7, 3, 6, 5, 6 ]
+# right [27,20,17,11,6, 0 ]
+# left  [0, 1, 8, 11,17,22]
+
+#       [2,1,-1]
+# right [0 -1 0]
+# left  [0, 1 0]
+
+        rightSums = [0]*len(nums)
+        for i in range(len(nums)-2,-1,-1):
+            rightSums[i] = nums[i+1] + rightSums[i+1]
+
+        leftSums = [0]*len(nums)
         for i in range(1, len(nums)):
-            prefixSum += nums[i-1]
-            prefixSums[i] = prefixSum
-        
-        postfixSum = 0
-        for i in range(len(nums)-2, -1, -1):
-            postfixSum += nums[i+1]
-            postfixSums[i] = postfixSum
-            
+            leftSums[i] = nums[i-1] + leftSums[i-1]
+
         for i in range(len(nums)):
-            if postfixSums[i] == prefixSums[i]:
+            if leftSums[i] == rightSums[i]:
                 return i
+
         return -1
-        
-        
