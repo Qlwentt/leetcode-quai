68ce39ea86cc4a5fb828133a1e3023fd2ff39e8d
Quai Wentt <quai.wentt@gmail.com>
2024-01-16 13:25:57 -0500

    Time: 42 ms (75.08%), Space: 17.5 MB (30.90%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,17 +1,21 @@
-from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-     #   {a: e, g:d}
-     #   {p:t, a:i, e:l r: e}
-        charMap = {}
-        
         if len(s) != len(t):
-            return false
+            return False
         
-        for i,char in enumerate(s):
-            existing = charMap.get(char, t[i])
-            if existing != t[i]:
-                return False
-            charMap[char] = t[i]
-                
-        return len(charMap.values()) == len(set(charMap.values()))
+        s_to_t = {}
+        t_to_s = {}
+        
+        for i, char in enumerate(s):
+            if char in s_to_t:
+                if s_to_t[char] != t[i]:
+                    return False
+            s_to_t[char] = t[i]
+        
+        for i, char in enumerate(t):
+            if char in t_to_s:
+                if t_to_s[char] != s[i]:
+                    return False
+            t_to_s[char] = s[i]
+        return True
+        
