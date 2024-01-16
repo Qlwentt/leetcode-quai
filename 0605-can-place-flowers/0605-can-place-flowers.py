4a47ccf538bb822c344825dbddadbb63a01eba3e
Quai Wentt <quai.wentt@gmail.com>
2024-01-16 14:39:45 -0500

    Time: 169 ms (14.52%), Space: 17.7 MB (32.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,39 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # [0,0,0,0,1,0,0,0,0] 2 2
-        # [1,0,0,0,1]  length 3, capacity 2 or 1
-        # 0[1,0,1,0,1,0]1 length 6, capaicty 3 or 2
-        # [0,0,0,0,0] length 5 , capacity 3 or 2
-        # [0,0,0,0] length 4, capacity 2 or 1
-        #  0 1 2 3 4
-        # [1,0,0,0,1]
-      #      L
-      #            R
-        #[1,0,0,0,1] 3//2
- #       [0,0,1,0,1]
- #       L
-#            R
-
-   #     [1,0,0,0,0,1]
-    #      L
-    #              R
-       #[1,0,0,0,1,0,0]
-        L = 0
-        R = 0
-        capacity = 0
-        allZeros = True
-        while R < len(flowerbed)+1:
-            prev = flowerbed[L-1] if L > 0 else 0
-            next_ = flowerbed[R] if R < len(flowerbed) else 0
-            if next_ == 1:
-                allZeros = False
-            if next_ == 1 or R == len(flowerbed):
-                capacity+= ((R - L) // 2)
-                capacity -= prev and next_ and (R-L) % 2 == 0
-                L = R + 1
-            R += 1
-        
-        if allZeros:
-            capacity = (len(flowerbed) // 2 ) + len(flowerbed) % 2
-                
-        return capacity >= n
+        placed = 0
+        for i in range(len(flowerbed)):
+            left = flowerbed[i-1] if i-1 in range(len(flowerbed)) else 0
+            right = flowerbed[i+1] if i+1 in range(len(flowerbed)) else 0
+            if flowerbed[i] == 0 and left == 0 and right == 0:
+                placed += 1
+                flowerbed[i] = 1
+        return placed >= n
+            
+            
