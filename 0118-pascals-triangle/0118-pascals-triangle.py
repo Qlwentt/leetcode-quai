df8e3ba7d52d4c4e70949e84ae4167b0a04bb252
Quai Wentt <quai.wentt@gmail.com>
2024-01-16 10:39:36 -0500

    Time: 35 ms (83.80%), Space: 17.3 MB (37.23%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,59 +1,20 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-#         if numRows == 1:
-#             return [[1]]
-#         elif numRows == 2:
-#             return [[1],[1,1]]
-#         else:
-#             answer = self.generate(numRows-1)
-#             previous = answer[-1]
-#             new = []
-#             new.append(1)
-#             for i in range(len(previous)-1):
-#                 new.append(previous[i] + previous[i+1])
-#             new.append(1)
-#             answer.append(new)
-            
-#             return answer
-
-#         answer = []
-#         def helper(rows):
-#             if rows == 1:
-#                 answer.append([1])
-#                 return answer
-#             elif rows == 2:
-#                 previous = helper(rows-1)[-1]
-#                 answer.append([1,1])
-#                 return answer
-#             else:
-#                 previous = helper(rows-1)[-1]
-#                 new = []
-#                 new.append(1)
-#                 for i in range(len(previous)-1):
-#                     new.append(previous[i] + previous[i+1])
-#                 new.append(1)
-#                 answer.append(new)
-
-#                 return answer
-#         return helper(numRows)
         if numRows == 1:
             return [[1]]
-        elif numRows == 2:
+        if numRows == 2:
             return [[1], [1,1]]
         
-        answer = [[1],[1,1]]
+        prev = self.generate(numRows-1)
+        lastPrev = prev[-1]
+        new = []
+        new.append(1)
         
-        for _ in range(numRows-2):
-            previous = answer[-1]
-            new = []
-            new.append(1)
-            for i in range(len(previous)-1):
-                new.append(previous[i] + previous[i+1])
-            new.append(1)
-            answer.append(new)
-        return answer
-            
+        for i in range(len(lastPrev)):
+            if i+1 in range(len(lastPrev)):
+                new.append(lastPrev[i]+lastPrev[i+1])
         
-                
-       
+        new.append(1)
         
+        prev.append(new)
+        return prev
