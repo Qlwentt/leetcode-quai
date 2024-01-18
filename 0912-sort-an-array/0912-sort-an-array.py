931b25c0510f06b9f3e4e82b90aadc4def2d9fb5
Quai Wentt <quai.wentt@gmail.com>
2024-01-18 15:48:03 -0500

    Time: 1518 ms (43.78%), Space: 26.8 MB (22.43%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,23 +1,19 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(nums1, nums2):
+        def merge(nums1,nums2):
             p1 = 0
             p2 = 0
-            p3 = 0
-            
-            answer = [None] * (len(nums1) + len(nums2))
-            
-            while p1 in range(len(nums1)) or p2 in range(len(nums2)):
-                item1 = nums1[p1] if p1 in range(len(nums1)) else float('inf')
-                item2 = nums2[p2] if p2 in range(len(nums2)) else float('inf')
+            answer = []
+            while p1 < len(nums1) or p2 < len(nums2):
+                item1 = nums1[p1] if p1 < len(nums1) else float('inf')
+                item2 = nums2[p2] if p2 < len(nums2) else float('inf')
                 
                 if item1 <= item2:
-                    answer[p3] = item1
+                    answer.append(item1)
                     p1 += 1
                 else:
-                    answer[p3] = item2
+                    answer.append(item2)
                     p2 += 1
-                p3 += 1
             return answer
         
         def mergeSort(nums):
@@ -25,13 +21,10 @@ class Solution:
                 return nums
             
             M = len(nums) // 2
-            left = mergeSort(nums[:M])
-            right = mergeSort(nums[M:])
+            left = nums[:M]
+            right = nums[M:]
             
-            return merge(left,right)
-            
-            
+            return merge(mergeSort(left), mergeSort(right))
         
         return mergeSort(nums)
-            
         
