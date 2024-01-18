34d371e16eb71c3496e926375bebdf806bf468a7
Quai Wentt <quai.wentt@gmail.com>
2024-01-18 11:50:37 -0500

    Time: 37 ms (74.18%), Space: 17.4 MB (17.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,16 +1,21 @@
-from collections import Counter
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = [0]*3
-        for num in nums:
-            count[num] += 1   
+     #  [2,0,2,1,1,0]
+     
+        bucket = [0]*3
         
-        i = 0
-        for key in range((len(count))):
-            for _ in range(count[key]):
-                nums[i] = key
-                i += 1
-        return nums
+        for num in nums:
+            bucket[num]+= 1
+        #  [2,2,2]
+        p = 0
+       # [0,0,1,1,2,2]
+        for i,amount in enumerate(bucket):
+            for _ in range(amount):
+                nums[p] = i
+                p += 1
+        
+        
+        
