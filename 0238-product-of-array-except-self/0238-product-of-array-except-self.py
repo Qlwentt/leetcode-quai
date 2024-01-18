ac8c3acae1872d98059415ea56697fcf1f4370e3
Quai Wentt <quai.wentt@gmail.com>
2024-01-18 10:22:36 -0500

    Time: 175 ms (69.09%), Space: 25.7 MB (15.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,18 +3,21 @@ class Solution:
         rightProd = [1] * len(nums)
         leftProd = [1] * len(nums)
         
-        [1 ,2 ,3 ,4 ]
-        [1 , 1, 2, 6]
-        [24, 12, 4, 1]
-        [24, 12, 8, 6]
+#         [1,2,3,4]
         
+#         [1, 1, 2,6]
+#         [24,12,4,1]
         
         for i in range(1,len(nums)):
-            leftProd[i] = leftProd[i-1] * nums[i-1]
-    
-        for i in range(len(nums) -2, -1, -1):
-            rightProd[i] = rightProd[i+1] * nums[i+1]
-        answer = []  
+            leftProd[i] = nums[i-1] * leftProd[i-1]
+                        
+        for i in range(len(nums)-2,-1,-1):
+            rightProd[i] = nums[i+1] * rightProd[i+1]
+        
+        # [24,12,8,6]
+        answer = [1] * len(nums)
         for i in range(len(nums)):
-            answer.append(leftProd[i]* rightProd[i])
+            answer[i] = leftProd[i] * rightProd[i]
+        
         return answer
+        
