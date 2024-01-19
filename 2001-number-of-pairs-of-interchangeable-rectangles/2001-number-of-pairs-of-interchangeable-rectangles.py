dc680cb878776c17e3879cbddc4ffc682d13f8f5
Quai Wentt <quai.wentt@gmail.com>
2024-01-19 13:53:14 -0500

    Time: 1296 ms (95.64%), Space: 72.9 MB (65.59%) - LeetHub

---
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        ratios = {}
+        for width, height in rectangles:
+            ratio = width/height
+            ratios[ratio] = 1 + ratios.get(ratio,0)
+            
+       # {0.5: 4} # number of combinations
+        answer = 0
+        for n in ratios.values():
+            if n > 1:
+                answer += comb(n,2)
+            
+        return answer
