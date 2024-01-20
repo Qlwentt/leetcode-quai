4d92516d61fdfd080992f7dbd87fba4f87e25083
Quai Wentt <quai.wentt@gmail.com>
2024-01-19 19:02:56 -0500

    Time: 79 ms (90.07%), Space: 17.7 MB (62.81%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,24 +2,34 @@ from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         # sliding window - fixed size
+        "cbaebabacd"
+       #  L
+       #   R
         
         L = 0
         R = 0
+        
+        sCount = {char:0 for char in "abcdefghijklmnopqrstuvwxyz"}
         pCount = {char:0 for char in "abcdefghijklmnopqrstuvwxyz"}
+        
         for char in p:
             pCount[char] += 1
-            
-        sCount = {char:0 for char in "abcdefghijklmnopqrstuvwxyz"}
+        
         answer = []
         while R < len(s):
-            sCount[s[R]]+=1
-            if pCount == sCount:
+            sCount[s[R]] += 1
+            
+            if sCount == pCount:
                 answer.append(L)
+            
             if R-L+1 >= len(p):
-                sCount[s[L]]-=1
-                L+=1
-            R+=1
+                sCount[s[L]] -= 1
+                L += 1
+            
+            R += 1
         return answer
+         
+        
     
     # O(n) * O(26) (26 letters of alphabet) time
     # O(26) space
