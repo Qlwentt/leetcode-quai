d7c13027c229b6d761781e88ed41bdf25b9a0dee
Quai Wentt <quai.wentt@gmail.com>
2024-01-22 16:30:42 -0500

    Time: 60 ms (51.29%), Space: 20.9 MB (38.96%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,19 +10,35 @@ class Node:
 from collections import defaultdict
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        parents = set()
         
-        def traverseUp(node):
+        def distance(node):
             if not node:
-                return node
+                return 0
             
-            if node in parents:
-                return node
-            parents.add(node)
-            return traverseUp(node.parent)
+            return 1 + distance(node.parent)
             
+        pDistance = distance(p)
+        qDistance = distance(q)
+        
+        # further away needs to swim up until it's equal
+        # only one of these will run or non if they are already equal
+        while pDistance > qDistance:
+            p = p.parent
+            pDistance -= 1
+        
+        while qDistance > pDistance:
+            q = q.parent
+            qDistance -= 1
+        
+        # swim up until both meet
+        while p != q:
+            p = p.parent
+            q = q.parent
+        
+        return p
             
-            
+# time: O(N)
+# space: O(1)
             
             
             
