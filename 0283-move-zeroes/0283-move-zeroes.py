e6acc608fa21a9b907f7074211ae8f606394e350
Quai Wentt <quai.wentt@gmail.com>
2024-01-23 14:31:07 -0500

    Time: 120 ms (94.44%), Space: 18.1 MB (56.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        p = 0
-        for i, num in enumerate(nums):
+        [1,3,12,0,0]
+       #     P
+       #         Q
+        P = 0
+        
+        for Q, num in enumerate(nums):
             if num != 0:
-                nums[i], nums[p] = nums[p], nums[i]
-                p += 1
+                nums[P], nums[Q] = nums[Q], nums[P]
+                P += 1
         
         
         
