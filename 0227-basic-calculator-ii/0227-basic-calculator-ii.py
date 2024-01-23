81e0af88e50e830d4d2538d59d348acb2e5d63a1
Quai Wentt <quai.wentt@gmail.com>
2024-01-23 17:03:47 -0500

    Time: 107 ms (65.75%), Space: 23.7 MB (7.87%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -3,7 +3,6 @@ class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
         s = re.split('(\-|\+|\*|\/)', s)
-        stack = []
         operations = {
             "-" : lambda a: a * -1,
             "*" : lambda a,b: a * b,
@@ -11,32 +10,29 @@ class Solution:
         }
         
         answer = 0
+        a = 0
         i = 0
         while i < len(s):
             char = s[i]
             if char == "*":
-                a = stack.pop()
                 b = int(s[i+1])
-                stack.append(operations[char](a,b))
+                a = operations[char](a,b)
                 i += 2
             elif char == "/":
-                a = stack.pop()
                 b = int(s[i+1])
-                stack.append(operations[char](a,b))
+                a = operations[char](a,b)
                 i += 2            
             elif char == "-":
-                a = stack.pop()
                 b = int(s[i+1])
-                stack.append(operations[char](b))
                 answer += a
+                a = operations[char](b)
                 i += 2
             elif char == "+":
-                a = stack.pop()
                 b = int(s[i+1])
-                stack.append(b)
                 answer += a
+                a = b
                 i += 2
             else:
-                stack.append(int(char))
+                a = int(char)
                 i += 1
-        return answer + stack[-1] if stack else answer
+        return answer + a
