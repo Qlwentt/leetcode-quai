eb80d212fb937f84aad128e124b140b660fcb5fa
Quai Wentt <quai.wentt@gmail.com>
2024-01-24 13:56:28 -0500

    Time: 32 ms (91.43%), Space: 16.8 MB (57.19%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,9 +1,7 @@
-import re
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # path = re.split("/+", path)
-        # path = list(filter(None, path))
         stack = []
+        
         for directory in path.split("/"):
             if directory == "..":
                 if stack:
