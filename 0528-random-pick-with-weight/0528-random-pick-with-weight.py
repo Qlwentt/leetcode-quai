1e205f88ca0186ae3803afb590e0c18cdd72660a
Quai Wentt <quai.wentt@gmail.com>
2024-01-24 11:13:40 -0500

    Time: 173 ms (61.93%), Space: 21.1 MB (90.92%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -13,7 +13,19 @@ class Solution:
 
     def pickIndex(self) -> int:
         target = randint(1,self.sum)
-        return bisect_left(self.choices, target)
+    
+        def lowerBound(arr, target):
+            lo = 0
+            hi = len(arr) - 1
+            
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if arr[mid] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            return lo
+        return lowerBound(self.choices, target)
 
 # Time __init__: O(N), pickIndex: O(log(N))
 # Space __init__: O(N), pickIndex: O(1)
