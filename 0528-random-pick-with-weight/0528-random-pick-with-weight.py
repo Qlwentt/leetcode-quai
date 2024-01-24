84032b69c742cf999f21f01176e91fb747883ee6
Quai Wentt <quai.wentt@gmail.com>
2024-01-24 11:06:47 -0500

    Time: 139 ms (93.25%), Space: 21.1 MB (95.40%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -2,33 +2,18 @@ from random import randint
 from bisect import bisect_left
 class Solution:
 
-    def __init__(self, w: List[int]):    
-        self.choices = [0]*len(w)
-        self.sumWeights = sum(w)
-        runSum = 0
-        for i, weight in enumerate(w):
-            runSum += weight
-            self.choices[i] = runSum
+    def __init__(self, w: List[int]):
+        self.choices = [0]* len(w)
+        self.sum = 0
         
-            
+        for i, weight in enumerate(w):
+            self.sum += weight
+            self.choices[i] = self.sum
+        
 
     def pickIndex(self) -> int:
-        
-        target = randint(1, self.sumWeights)
+        target = randint(1,self.sum)
         return bisect_left(self.choices, target)
-        
-#         lo = 0
-#         hi = len(self.choices) - 1
-        
-#         while lo <= hi:
-#             mid = (lo+hi) // 2
-            
-#             if target > self.choices[mid]:
-#                 lo = mid + 1
-#             else:
-#                 hi = mid -1
-#         return lo
-    
 
 
 # Your Solution object will be instantiated and called as such:
