b71a1ce2b15fecba3dcaf312d492eea06c3c3705
Quai Wentt <quai.wentt@gmail.com>
2024-01-25 12:57:13 -0500

    Time: 58 ms (79.06%), Space: 19.8 MB (63.81%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -3,18 +3,22 @@ class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self.arr = deque([])
+        self.q = deque([])
         self.sum = 0
+        [1,10,3,5]
 
     def next(self, val: int) -> float:
-        self.arr.appendleft(val)
+        self.q.append(val)
         self.sum += val
-        if len(self.arr) > self.size:
-            outgoing = self.arr.pop()
-            self.sum -= outgoing
-        return self.sum / len(self.arr)
+        while len(self.q) > self.size:
+            leaving = self.q.popleft()
+            self.sum -= leaving
+        return self.sum / len(self.q)
 
 
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
+
+# Time: O(1) for each next to pop and then calculate average, O(N) where n is number of calls to next
+# Space: O(k) where k is size of window
