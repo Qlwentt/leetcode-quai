c058687c28e503ce0ccce3931cd8bccf36c09dbc
Quai Wentt <quai.wentt@gmail.com>
2024-01-25 13:20:33 -0500

    Time: 38 ms (55.07%), Space: 16.5 MB (66.12%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,15 +1,18 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        
-        add = 0 
-        bal = 0 
+        bal = 0
+        add = 0
         
         for char in s:
             if char == "(":
                 bal += 1
-            elif char == ")":
+            else: # char == )
                 bal -= 1
             if bal < 0:
                 add += 1
                 bal = 0
-        return bal + add 
+        
+        return add + bal
+    
+# Time: O(N) iterate through string once
+# Space O(1) only use int vars
