30bd724a5acc0b26abd5d721df16dcdbe7f7a9b3
Quai Wentt <quai.wentt@gmail.com>
2024-01-25 13:12:54 -0500

    Time: 31 ms (88.76%), Space: 16.7 MB (57.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,35 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        # 2^10
+        # 2*2*2*2*2 *2*2*2*2*2
+        # 2^5 * 2^5
+        # 2*2*2*2*2
+        # 2^2 * 2^2 *2
+        # 2^2
+        # 2^1 * 2^1
+        # 2^0 * 2
+        # 2^0 = 1
+        
         def power(x,n):
-            if n == 1:
-                return x
             if n == 0:
                 return 1
+            if n == 1:
+                return x
             
-            halfpower = power(x, n // 2)
-            if n % 2:
-                return x * halfpower * halfpower
-            else:
-                return halfpower * halfpower
-            
+            halfPower = power(x, n // 2)
+            if n % 2: # odd
+                return halfPower * halfPower * x
+            else: # even
+                return halfPower * halfPower
+        
         if n < 0:
-            return 1/power(x, -n)
+            return 1/power(x,-n)
         else:
             return power(x,n)
+        
+# Time: O(log(N)) (because halfing every time)
+# Space: O(log(N))
+        
+        
+        
+       
