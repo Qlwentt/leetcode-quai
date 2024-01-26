e111902bf16d16c4f13589182fbdfac878585c31
Quai Wentt <quai.wentt@gmail.com>
2024-01-26 13:18:59 -0500

    Time: 51 ms (15.23%), Space: 17.4 MB (58.37%) - LeetHub

--- 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.py
+++ 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.py
@@ -8,29 +8,35 @@ class Node:
 
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        # Case 1: head is None
+        # Case 2: Insert into middle of linked list
+        #   a: insert val is in between two nodes
+        # Case 3: Insert at the end 
+            # a: insert val is greater that number at the end
+            # b: insert val is less than number at the beginning
+        # Case 4: All numbers are the same (also insert at the end - can insert anywhere)
+        
+        
         if not head:
             current = Node(insertVal)
             current.next = current
             return current
-    
-    # 1,3,4 5
-        # if self.val < self.next.val:
-            # if insertVal >= self.val and insertVal <= self.next.val
-            # insert it next
-        # else:
-            # if insertVal >= self.val:
-            
+        
         current = head
+        #[3,3,5] 5
+        #  H
+        # stop when you get to the end of the list (next node is head)
         while current.next != head:
-            if current.val <= current.next.val:
-                if insertVal >= current.val and insertVal <= current.next.val:
-                    current.next = next_ = Node(insertVal, current.next)
-                    return head
-            else:
-                if  insertVal >= current.val or insertVal <= current.next.val:
-                    current.next = next_ = Node(insertVal, current.next)
-                    return head
+            if insertVal >= current.val and insertVal <= current.next.val:
+                break
+            # if current is the end of the list
+            elif current.next.val < current.val:
+                # and insert val is greater than number at end or insert val is less than number at the beg
+                if insertVal > current.val or insertVal < current.next.val:
+                    break
             current = current.next
             
+        # all numbers are the same
         current.next = Node(insertVal, current.next)
         return head
+            
