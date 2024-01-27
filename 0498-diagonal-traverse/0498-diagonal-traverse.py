e418c1e955473f71bc5e377c143c67ee7b78264f
Quai Wentt <quai.wentt@gmail.com>
2024-01-27 13:15:47 -0500

    Time: 187 ms (18.77%), Space: 19.9 MB (90.82%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,41 +1,41 @@
-from collections import deque
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        
         ROWS = len(mat)
         COLS = len(mat[0])
-        
         answer = []
-        directions = {
-            True: lambda row, col, fn: fn(row-1,col+1, True),
-            False: lambda row, col, fn: fn(row+1,col-1, False),
-        }
         
-        def dfs(row, col, forward):
-            if (row not in range(ROWS) or 
-                col not in range(COLS)):
-                prevRow = row + 1 if forward else row -1
-                prevCol = col - 1 if forward else col + 1
-                return prevRow, prevCol
-                
-            answer.append(mat[row][col])
-            r, c = directions[forward](row, col, dfs)
-            return r,c
-        
-        row = 0
-        col = 0
-        
-        while row in range(ROWS) and col in range(COLS):
-            row, col = dfs(row,col, True)
-            if col + 1 in range(COLS):
-                col += 1
-            else:
-                row += 1
-            row, col = dfs(row,col,False)
-            if row +1 in range(ROWS):
-                row += 1
-            else:
-                col += 1
+        # returns last valid position of r and c
+        def dfs(r,c, isForward):
+            if (r not in range(ROWS) or
+                c not in range(COLS)
+               ):
+                prevR = r + 1 if isForward else r - 1
+                prevC = c - 1 if isForward else c + 1
+                return (prevR, prevC)
             
-                
+            answer.append(mat[r][c])
+            
+            if isForward:
+                # up and to the right
+                prevR, prevC = dfs(r-1, c+1, True)
+            else:
+                # down and to the left
+                prevR, prevC = dfs(r+1, c-1, False)
+            
+            return prevR, prevC
+        
+        r = 0
+        c = 0
+        
+        while r in range(ROWS) and c in range(COLS):
+            r, c = dfs(r,c,True) # go diagnally up until you can't anymore
+            if c + 1 in range(COLS): # move right if you can otherwise move down
+                c += 1
+            else:
+                r += 1
+            r,c = dfs(r,c,False) # go diagonally down until you can't anymore
+            if r + 1 in range(ROWS): # move down if you can, otherwise move right
+                r += 1
+            else:
+                c += 1
         return answer
