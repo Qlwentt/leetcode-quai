c4ff50b947455d4fa02f23245aa9db02c741d2a7
Quai Wentt <quai.wentt@gmail.com>
2024-01-30 12:14:49 -0500

    Time: 46 ms (56.13%), Space: 16.7 MB (54.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,27 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         def reverse(start, end):
             while start < end:
-                nums[start] , nums[end] = nums[end] , nums[start]
+                nums[start], nums[end] = nums[end], nums[start]
                 start += 1
                 end -= 1
         
+        # find first decreasing from end to get pivot
+        # [3,2,1,2,3]
         pivot = None
-        
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i] < nums[i+1]:
-                pivot = i
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i-1] < nums[i]:
+                pivot = i - 1
                 break
+        # if found pivot, swap with number just larger than pivot
         if pivot is not None:
-          # find number just larger than pivot
-            nextGreater = None
-            for j in range(len(nums)-1, pivot, -1):
-                if nums[j] > nums[pivot]:
-                    nums[j], nums[pivot] = nums[pivot] , nums[j]
+            for i in range(len(nums)-1, pivot, -1):
+                if nums[i] > nums[pivot]:
+                    nums[i], nums[pivot] = nums[pivot], nums[i]
                     break
+            # reverse numbers from pivot + 1 to end
             reverse(pivot+1,len(nums)-1)
+        # else reverse all nums
         else:
             nums.reverse()
-        
-        
