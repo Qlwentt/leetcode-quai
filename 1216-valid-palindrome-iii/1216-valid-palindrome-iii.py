d7a3ba7578ef5f89bba42fe01eba4a0470d74970
Quai Wentt <quai.wentt@gmail.com>
2024-01-30 16:33:45 -0500

    Time: 1271 ms (17.82%), Space: 256.4 MB (25.84%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        memo = {}
+        
+        def kPal(L,R,k):
+            if (L,R,k) in memo:
+                return memo[(L,R,k)]
+            while L < R:
+                if s[L] != s[R]:
+                    if k > 0:
+                        answer = kPal(L+1,R, k-1) or kPal(L, R-1, k-1)
+                        memo[(L,R,k)] = answer
+                        return answer
+                    else:
+                        memo[(L,R,k)] = False
+                        return False
+                L += 1
+                R -= 1
+            return True
+        return kPal(0,len(s)-1,k)
