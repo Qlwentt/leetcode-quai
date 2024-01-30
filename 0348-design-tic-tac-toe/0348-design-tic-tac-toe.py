bce9f115d41063cd1bf262c793e4b7115a9cec0f
Quai Wentt <quai.wentt@gmail.com>
2024-01-30 13:02:41 -0500

    Time: 81 ms (50.51%), Space: 18.9 MB (95.42%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -8,27 +8,26 @@ class TicTacToe:
             "negDiag": 0
         }
         self.n = n
-        
 
     def move(self, row: int, col: int, player: int) -> int:
         p = 1 if player == 1 else -1
         
-        # mark move
-        self.board["rows"][row] += p
-        self.board["cols"][col] += p
-        if row == col:
-            self.board["posDiag"] += p
-        if row + col == self.n -1:
-            self.board["negDiag"] += p
+        self.board['rows'][row] += p
+        self.board['cols'][col] += p
         
-        if (abs(self.board["rows"][row]) == self.n or
-            abs(self.board["cols"][col]) == self.n or
-            abs(self.board["posDiag"]) == self.n or
-            abs(self.board["negDiag"]) == self.n
+        if row == col:
+            self.board['posDiag'] += p
+        if row + col == self.n - 1:
+            self.board['negDiag'] += p
+        
+        if (abs(self.board['rows'][row]) == self.n or
+            abs(self.board['cols'][col]) == self.n or
+            abs(self.board['posDiag']) == self.n or
+            abs(self.board['negDiag']) == self.n
            ):
             return player
         return 0
-                
+
 
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
