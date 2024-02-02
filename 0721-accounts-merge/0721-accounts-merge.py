47c51adf6cbb4d7d8807010169e4ad951ef3b894
Quai Wentt <quai.wentt@gmail.com>
2024-02-02 11:37:40 -0500

    Time: 179 ms (70.87%), Space: 20 MB (99.67%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
 class UnionFind:
-    def __init__(self, n):
+    def __init__(self,n):
         self.parents = {}
         self.ranks = {}
         
         for i in range(n):
             self.parents[i] = i
             self.ranks[i] = 1
+    
     def find(self, node):
         parent = self.parents[node]
         
@@ -15,39 +16,40 @@ class UnionFind:
             parent = self.parents[parent]
         return parent
     
-    def union(self, x,y):
-        rootX, rootY = self.find(x), self.find(y)
-        
-        if rootX == rootY:
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+        if parent1 == parent2:
             return False
         
-        if self.ranks[rootX] < self.ranks[rootY]:
-            self.parents[rootY] = rootX
-        elif self.ranks[rootY] < self.ranks[rootX]:
-            self.parents[rootX] = rootY
-        else:
-            self.parents[rootX] = rootY
-            self.ranks[rootY] += 1
-
-
+        if self.ranks[parent1] < self.ranks[parent2]:
+            self.parents[parent2] = parent1
+        elif self.ranks[parent2] < self.ranks[parent1]:
+            self.parents[parent1] = parent2
+        else: # ranks are the same
+            self.parents[parent1] = parent2
+            self.ranks[parent2] += 1
+        return True
+    
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        emailsToAccount = {}
+        emailToAccountId = {}
         u = UnionFind(len(accounts))
-        
-        for i, account in enumerate(accounts):
-            for email in account[1:]:
-                if email in emailsToAccount:
-                    u.union(i, emailsToAccount[email])
+        for i, accountList in enumerate(accounts):
+            for email in accountList[1:]:
+                if email in emailToAccountId:
+                    u.union(i, emailToAccountId[email])
                 else:
-                    emailsToAccount[email] = i
+                    emailToAccountId[email] = i
+        
         emailGroups = defaultdict(list)
-        for email, node in emailsToAccount.items():
-            parent = u.find(node)
-            emailGroups[parent].append(email)
+        for email, accountId in emailToAccountId.items():
+            parentAccount = u.find(accountId)
+            emailGroups[parentAccount].append(email)
         
         answer = []
-        for i , emails in emailGroups.items():
-            answer.append([accounts[i][0]]+sorted(emails))
-            
+        for accountId, emails in emailGroups.items():
+            answer.append([accounts[accountId][0]] + sorted(emails))
         return answer
+    
+# Time: O (NKlog(NK))
+# Space O (NK)
