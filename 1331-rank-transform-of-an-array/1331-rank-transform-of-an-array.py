026e448344fe55054f49cc578bc1530f6f8044f3
Quai Wentt <quai.wentt@gmail.com>
2024-02-03 11:46:38 -0500

    Time: 292 ms (27.34%), Space: 43.3 MB (5.01%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        numToIndices = defaultdict(list)
+        
+        for i, num in enumerate(arr):
+            numToIndices[num].append(i)
+            
+        numKeys = sorted(numToIndices.keys())
+        answer = [0]* len(arr)
+        rank = 1
+        for num in numKeys:
+            for index in numToIndices[num]:
+                answer[index] = rank
+            rank += 1
+        return answer
