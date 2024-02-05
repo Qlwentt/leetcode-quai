93272187509c54cfdfefaffa47bd8643441f08b1
Quai Wentt <quai.wentt@gmail.com>
2024-02-05 14:41:06 -0500

    Time: 516 ms (10.06%), Space: 17.3 MB (79.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,40 +1,30 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len (s):
+        if len(t) > len(s):
             return ""
         
-        counterT = {}
+        counterS = defaultdict(int)
+        counterT = defaultdict(int)
+        
         for char in t:
-            counterT[char] = counterT.get(char,0) + 1
-        
-        counterS = {char:0 for char in counterT.keys()}
-        
-        minLen = float("inf")
-        pos = (None, None)
+            counterT[char] += 1
         
         L = 0
-        R = 0
-        
-        while R < len(s):
-            curChar = s[R]
-            if curChar in counterS:
-                counterS[curChar] += 1
-                
-            while all(counterS[key] >= counterT[key] for key in counterS.keys()):    
-                if R - L + 1 < minLen: # 6 < inf, 10 < 6, 8 < 6, 7 < 6, 6 < 6, 7 < 6, 6 < 6, 5 < 6 4 < 5
-                    pos = (L,R) # (0, 5),(8,12), (9, 12)
-                    minLen = (R - L + 1) # 6 5 4
-                prevChar = s[L]
-                if prevChar in counterS:
-                    counterS[prevChar] -= 1
-                L += 1
-            R += 1   
-        return s[pos[0]:pos[1]+1] if minLen < float("inf") else ""
-   # counterS = {A:0 1 0 1, B:0 1 2 1, C:0 1 0 1} counterT = {A:1,B:1,C:1}    
+        answerL = None
+        answerR = None
+        minLen = float('inf')
+        for R in range(len(s)):
+            char = s[R]
+            counterS[char] += 1
             
-        
-        "A D O B E C O D E B A N C"
-    #                        L
-    #                               R
+            while all(counterS[key] >= counterT[key] for key in counterT.keys()):
+                minLen = min(R-L+1, minLen)
+                if R-L+1 == minLen:
+                    answerL = L
+                    answerR = R
+                removingChar = s[L]
+                counterS[removingChar] -= 1
+                L += 1
+        print (answerL)
+        return s[answerL:answerR+1] if answerL is not None else ""
         
