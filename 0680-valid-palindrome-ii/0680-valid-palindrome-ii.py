56a9337e5bbccb8cbfa755bbab983f65b743d849
Quai Wentt <quai.wentt@gmail.com>
2024-02-09 13:35:27 -0500

    Time: 111 ms (43.45%), Space: 16.9 MB (94.84%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        
         def isPal(L,R):
             while L < R:
                 if s[L] != s[R]:
@@ -11,10 +12,10 @@ class Solution:
         L = 0
         R = len(s) - 1
         
-        while L < R:            
+        while L < R:
             if s[L] != s[R]:
-                return isPal(L+1,R) or isPal(L,R-1)
-            
+                return isPal(L+1,R) or isPal(L, R-1)
             L += 1
             R -= 1
         return True
+        
