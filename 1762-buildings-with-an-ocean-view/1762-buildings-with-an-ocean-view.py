fe7553b8e8d5eb8e8d4a6decccd686284c55ee37
Quai Wentt <quai.wentt@gmail.com>
2024-02-09 13:02:44 -0500

    Time: 550 ms (31.43%), Space: 34.2 MB (48.03%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,15 +1,12 @@
 from collections import deque
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        maxHeight = float('-inf')
         answer = deque([])
-        for i in range(len(heights)-1,-1,-1):
-            cur = heights[i]
-            if cur > maxHeight:
+        maxHeight = float('-inf')
+        
+        for i, height in reversed(list(enumerate(heights))):
+            if height > maxHeight:
                 answer.appendleft(i)
-            maxHeight = max(cur, maxHeight)
-            
+            maxHeight = max(maxHeight, height)
+        
         return answer
-    
-    # Time: O(N)
-    # Space: O(N) if you count answer, O(1) if you don't count answer
