1095456ffa77c051ce22b9f9b812b14534a57bac
Quai Wentt <quai.wentt@gmail.com>
2024-02-10 12:10:49 -0500

    Time: 128 ms (37.31%), Space: 25.8 MB (5.07%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,45 +1,38 @@
-import re
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
-        s = re.split('(\-|\+|\*|\/)', s)
-        operations = {
-            "-" : lambda a: a * -1,
-            "*" : lambda a,b: a * b,
-            "/" : lambda a,b: int(a/b),
-        }
-        
+        s = re.split(r'(\+|\-|\*|\/)', s)
+        s = [item for item in s if item != ""]
+
+        i = 0
         stack = []
         answer = 0
-        i = 0
         while i < len(s):
             char = s[i]
-            if char == "*":
+            if char.isnumeric():
+                stack.append(int(char))
+                i += 1
+            elif char == "+":
+                answer += stack.pop()
+                i += 1
+            elif char == "-":
+                answer += stack.pop()
+                stack.append(int(s[i+1]) * -1)
+                i += 2
+            elif char == "*":
                 a = stack.pop()
                 b = int(s[i+1])
-                stack.append(operations[char](a,b))
+                stack.append(a*b)
                 i += 2
             elif char == "/":
                 a = stack.pop()
                 b = int(s[i+1])
-                stack.append(operations[char](a,b))
-                i += 2            
-            elif char == "-":
-                a = stack.pop()
-                b = int(s[i+1])
-                stack.append(operations[char](b))
-                answer += a
-                i += 2
-            elif char == "+":
-                a = stack.pop()
-                b = int(s[i+1])
-                stack.append(b)
-                answer += a
+                stack.append(int(a/b))
                 i += 2
             else:
-                stack.append(int(char))
                 i += 1
-        return answer + stack[-1] if stack else answer
-    
-    # Time O(N)
-    # Space O(1) because there is only ever up to one thing in the stack
+        if stack:
+            answer += stack.pop()
+            
+        return answer
+                
