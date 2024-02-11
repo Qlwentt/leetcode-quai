93cfb5ba12bf4bc8e9332a267b373637855cead9
Quai Wentt <quai.wentt@gmail.com>
2024-02-11 14:53:38 -0500

    Time: 111 ms (52.81%), Space: 25.8 MB (5.02%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -3,35 +3,34 @@ class Solution:
         s = s.replace(" ", "")
         s = re.split(r'(\+|\-|\*|\/)', s)
         s = [item for item in s if item != ""]
-
+        
         i = 0
-        stack = []
         answer = 0
+        stack = []
         while i < len(s):
-            char = s[i]
-            if char.isnumeric():
-                stack.append(int(char))
-                i += 1
-            elif char == "+":
+            if s[i] == "+":
                 answer += stack.pop()
-                i += 1
-            elif char == "-":
+                stack.append(int(s[i+1]))
+                i += 2
+            elif s[i] == "-":
                 answer += stack.pop()
                 stack.append(int(s[i+1]) * -1)
                 i += 2
-            elif char == "*":
+            elif s[i] == "*":
                 a = stack.pop()
                 b = int(s[i+1])
                 stack.append(a*b)
                 i += 2
-            elif char == "/":
+            elif s[i] == "/":
                 a = stack.pop()
                 b = int(s[i+1])
                 stack.append(int(a/b))
                 i += 2
-            
+            else:
+                stack.append(int(s[i]))
+                i += 1
+                
         if stack:
             answer += stack.pop()
             
         return answer
-                
