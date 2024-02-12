caa36aa0a7e02f649b800ac473ef70d5ad332cfe
Quai Wentt <quai.wentt@gmail.com>
2024-02-12 10:04:14 -0500

    Time: 891 ms (49.62%), Space: 16.9 MB (85.33%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.py
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.py
@@ -1,35 +1,35 @@
 from collections import Counter
 class Solution:
     def minStickers(self, stickers: List[str], target: str) -> int:
-        stickerCounters = []
-        for sticker in stickers:
-            stickerCounters.append(Counter(sticker))
-        memo  = {} 
+        availStickers = []
+        for s in stickers:
+            availStickers.append(Counter(s))
+        
+        memo = {}
+        
         def dfs(curSticker, t):
             if t in memo:
                 return memo[t]
-            res = 1 if curSticker else 0
             
-            remainder = ""
+            answer = 1 if curSticker else 0
+            
+            leftover = ""
             for char in t:
                 if char in curSticker and curSticker[char] > 0:
                     curSticker[char] -= 1
                 else:
-                    remainder += char
+                    leftover += char
             
-            if remainder:
+            
+            if leftover:
                 used = float('inf')
-                for newSticker in stickerCounters:
-                    if remainder[0] in newSticker:
-                        used = min(used,dfs(newSticker.copy(), remainder))
-                memo[remainder] = used 
-                res += used
-           
-                 
-            return res
-                        
+                for newSticker in availStickers:
+                    if leftover[0] in newSticker:
+                        used = min(used, dfs(newSticker.copy(), leftover))
+                memo[leftover] = used
+                answer += used
             
-        answer = dfs({}, target)
+            return answer
         
-        
-        return answer if answer != float('inf') else -1
+        a = dfs({}, target) 
+        return a if a != float('inf') else -1
