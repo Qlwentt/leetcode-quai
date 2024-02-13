7a4e558b44f801672a80b267d8aca3372f6e0b61
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 14:38:30 -0500

    Time: 614 ms (20.08%), Space: 18.1 MB (49.20%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,34 +1,28 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        visited = set()
         q = deque([])
-        
-        if grid[0][0] == 0:
+        if grid[0][0] != 1:
             q.append((0,0,1))
-            visited.add((0,0))
+        visited = set([(0,0)])
+        n = len(grid)
         
-        directions = [[0,1],[1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1, -1]]
+        directions = [[0,1],[1,0],[-1,0],[0,-1],[1,1], [-1,-1], [-1,1],[1,-1]]
+        answer = -1
         while q:
             r, c, level = q.popleft()
             
-            if (r,c) == (ROWS-1, COLS-1):
+            if (r,c) == (n-1,n-1):
                 return level
             
             for dr, dc in directions:
-                newR = r + dr
-                newC = c + dc
-                
-                if (newR in range(ROWS) and
-                    newC in range(COLS) and
-                    (newR, newC) not in visited and
+                newR, newC = dr + r, dc + c
+                if (newR in range(n) and
+                    newC in range(n) and
+                    (newR,newC) not in visited and
                     grid[newR][newC] != 1
                    ):
-                    q.append((newR, newC, level +1))
-                    visited.add((newR, newC))        
-        
-        
-        return -1
-        
+                    q.append((newR,newC, level+1))
+                    visited.add((newR, newC))
+        return answer
+                    
