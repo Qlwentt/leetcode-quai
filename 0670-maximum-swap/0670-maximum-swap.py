5017266db26bdd7ab217492f67b885e58b252f37
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 13:26:32 -0500

    Time: 37 ms (50.21%), Space: 16.5 MB (88.31%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,19 +1,16 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        s = list(str(num))
-        
-        last = {num: i for i, num in enumerate(s)}
-        
-        for i, digit in enumerate(s):
-            for laterDigit in range(9, int(digit), -1):
-                laterDigit = str(laterDigit)
-                if laterDigit in last and last[laterDigit] > i:
-                    j = last[laterDigit]
-                    s[i], s[j] = s[j], s[i]
-                    return int("".join(s))
+        numDict = {}
+        answer = list(str(num))
+        for i, n in enumerate(str(num)):
+            numDict[n] = i
+            
+        for i, digit in enumerate(str(num)):
+            for higherNum in range(9, int(digit), -1):
+                s = str(higherNum)
+                if s in numDict and numDict[s] > i:
+                    answer[i], answer[numDict[s]] = answer[numDict[s]], answer[i]
+                    return int("".join(answer))
                 
-        return num
-    
-# Time: O(N) because inner loop only goes from at worst case 9 to 0
-# Space: O(N) for hashmap and s
+        return int("".join(answer))
         
