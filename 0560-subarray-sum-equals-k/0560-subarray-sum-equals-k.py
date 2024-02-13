b05d81e424b22538dfe3414b9cf0d00d4ac524d8
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 13:42:41 -0500

    Time: 249 ms (23.39%), Space: 19.5 MB (82.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -11,5 +11,5 @@ class Solution:
             if curSum - k in prefixDict:
                 answer += prefixDict[curSum-k]
             prefixDict[curSum] += 1
-        
+            
         return answer
