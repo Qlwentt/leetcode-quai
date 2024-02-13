6f98750365c80a3406a8d992772d0f6d6bd77c19
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 16:40:17 -0500

    Time: 34 ms (93.57%), Space: 17.2 MB (49.09%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,19 +1,16 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        carry = 0
         num1 = list(num1)
         num2 = list(num2)
+        remainder = 0
         answer = []
-        while carry or num1 or num2:
+        while num1 or num2 or remainder:
             digit1 = int(num1.pop()) if num1 else 0
             digit2 = int(num2.pop()) if num2 else 0
             
-            sum_ = digit1 + digit2 + carry
-            answer.append(str(sum_ % 10))
-            carry = sum_ // 10
-        
+            sum_ = digit1 + digit2 + remainder
+            answer.append(str(sum_% 10))
+            remainder = sum_ // 10
+            
         answer.reverse()
         return "".join(answer)
-    
-# O(max(N,M)) # worst case, iterate the longer of num1 and num2
-# O(max(N,M)) # length of new string is max length of max string + 1
