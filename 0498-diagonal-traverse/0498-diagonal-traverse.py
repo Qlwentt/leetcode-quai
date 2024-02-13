ce20a855f020faf94a75e930f40081a39de25706
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 16:19:29 -0500

    Time: 166 ms (25.76%), Space: 19.8 MB (66.54%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -3,39 +3,37 @@ class Solution:
         ROWS = len(mat)
         COLS = len(mat[0])
         answer = []
-        
-        # returns last valid position of r and c
-        def dfs(r,c, isForward):
+        def dfs(r,c,isForward):
             if (r not in range(ROWS) or
                 c not in range(COLS)
-               ):
-                prevR = r + 1 if isForward else r - 1
-                prevC = c - 1 if isForward else c + 1
-                return (prevR, prevC)
+            ):
+                prevRow = r + 1 if isForward else r - 1
+                prevCol = c - 1 if isForward else c + 1
+                return (prevRow, prevCol)
             
             answer.append(mat[r][c])
             
             if isForward:
-                # up and to the right
-                prevR, prevC = dfs(r-1, c+1, True)
+                return dfs(r-1,c+1, True)
             else:
-                # down and to the left
-                prevR, prevC = dfs(r+1, c-1, False)
+                return dfs(r+1,c-1, False)
             
-            return prevR, prevC
         
         r = 0
         c = 0
         
         while r in range(ROWS) and c in range(COLS):
-            r, c = dfs(r,c,True) # go diagnally up until you can't anymore
-            if c + 1 in range(COLS): # move right if you can otherwise move down
+            r, c = dfs(r,c, True)
+            if c + 1 in range(COLS): # if can go right, do it, otherwise go down
                 c += 1
             else:
                 r += 1
-            r,c = dfs(r,c,False) # go diagonally down until you can't anymore
-            if r + 1 in range(ROWS): # move down if you can, otherwise move right
+            r, c = dfs(r,c, False)
+            if r + 1 in range(ROWS): # if can go down do it, otherwise go right
                 r += 1
             else:
                 c += 1
-        return answer
+        
+        
+        return answer        
+                
