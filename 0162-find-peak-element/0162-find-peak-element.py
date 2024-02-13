840a120784e3053e23510515355eed358c0f75b3
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 14:19:53 -0500

    Time: 61 ms (8.83%), Space: 16.8 MB (64.96%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,17 +1,19 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         L = 0
-        R = len(nums) -1
+        R = len(nums) - 1
         
         while L <= R:
-            M = (L + R) // 2
-            [3,2,1]
-            # if left neighbor is greater search on the left
-            if M > 0 and nums[M-1] > nums[M]:
-                R = M - 1
-            # if right neighbor is greater search on the right
-            elif M < len(nums) -1 and nums[M+1] > nums[M]:
-                L = M + 1
-            else: # it is greater than left and right - found peak element
-                return M
-                
+            mid = (L + R) // 2
+            1,2,3,1
+            if mid + 1 < len(nums) and nums[mid+1] > nums[mid]:
+                # search to the right
+                L = mid + 1
+            elif mid - 1 >= 0 and nums[mid-1] > nums[mid]:
+                # search to the left
+                R = mid - 1
+            else:
+                return mid
+        
+        
+        
