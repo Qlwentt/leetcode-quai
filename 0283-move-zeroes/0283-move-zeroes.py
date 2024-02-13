f5982d7997988b81ef46974c699cce39a166ca39
Quai Wentt <quai.wentt@gmail.com>
2024-02-13 18:19:17 -0500

    Time: 127 ms (66.49%), Space: 18.3 MB (52.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-     #   [0,1,0,3,12]
-      #  P
-      #  Q
-        
         P = 0
         for Q in range(len(nums)):
             if nums[Q] != 0:
                 nums[P], nums[Q] = nums[Q], nums[P]
                 P += 1
         
-# Time: O(N)
-# Space: O(1)
