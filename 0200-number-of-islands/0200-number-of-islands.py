c8f17f51cbc3e630c1f8a3acafd0b829913df9e8
Quai Wentt <quai.wentt@gmail.com>
2024-02-14 14:38:50 -0500

    Time: 325 ms (16.93%), Space: 28 MB (8.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,11 +4,12 @@ class Solution:
         COLS = len(grid[0])
         visited = set()
         def dfs(r,c):
-            if (r not in range(ROWS) or
+            if (
+                r not in range(ROWS) or 
                 c not in range(COLS) or
                 (r,c) in visited or
                 grid[r][c] == "0"
-               ):
+            ):
                 return 0
             
             visited.add((r,c))
@@ -17,15 +18,11 @@ class Solution:
             dfs(r+1, c)
             dfs(r, c-1)
             dfs(r, c+1)
-            
+
             return 1
         islands = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1":
-                    islands += dfs(r,c)
-        
+                islands += dfs(r,c)
+                
         return islands
-    
-# Time: O(N*M)
-# Space: O(N*M)
