ffbd524ca877e77d0bf01ed75dad0b936cb6352b
Quai Wentt <quai.wentt@gmail.com>
2024-02-15 13:38:01 -0500

    Time: 74 ms (69.06%), Space: 18.9 MB (97.37%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -1,30 +1,28 @@
+from collections import defaultdict
 class TicTacToe:
 
     def __init__(self, n: int):
-        self.board = {
-            "rows": [0] * n,
-            "cols": [0] * n,
-            "posDiag": 0,
-            "negDiag": 0
-        }
+        self.rows = defaultdict(int) # key = row # value = tally of x vs 0
+        self.cols = defaultdict(int)
+        self.posDiag = 0
+        self.negDiag = 0
         self.n = n
+        
 
     def move(self, row: int, col: int, player: int) -> int:
-        p = 1 if player == 1 else -1
+        mark = 1 if player == 1 else -1
         
-        self.board['rows'][row] += p
-        self.board['cols'][col] += p
-        
+        self.rows[row] += mark
+        self.cols[col] += mark
         if row == col:
-            self.board['posDiag'] += p
+            self.posDiag += mark
         if row + col == self.n - 1:
-            self.board['negDiag'] += p
-        
-        if (abs(self.board['rows'][row]) == self.n or
-            abs(self.board['cols'][col]) == self.n or
-            abs(self.board['posDiag']) == self.n or
-            abs(self.board['negDiag']) == self.n
-           ):
+            self.negDiag += mark
+            
+        if (abs(self.rows[row]) == self.n or
+            abs(self.cols[col]) == self.n or
+            abs(self.posDiag) == self.n or
+            abs(self.negDiag) == self.n):
             return player
         return 0
 
