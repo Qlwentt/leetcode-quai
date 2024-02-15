a815712729a9bffe8835fa0b52ec66f0b7c2c839
Quai Wentt <quai.wentt@gmail.com>
2024-02-15 10:46:51 -0500

    Time: 35 ms (77.96%), Space: 16.7 MB (96.33%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -4,41 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from collections import deque, defaultdict
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        # left and down = (row+1, col-1)
-        # right and down = (row+1, col+1)
         if not root:
-            return root
-        minCol = float('inf')
-        maxCol = float('-inf')
-        colMap = defaultdict(list)
-        q = deque([(0,0, root)])
+            return []
         
+        q = deque([(root, 0,0)])
+        colGroups = defaultdict(list)
+        minCol = 0
+        maxCol = 0
         while q:
-            row, col, node = q.popleft()
-            
-            minCol = min(col, minCol)
-            maxCol = max(col, maxCol)
-            
-            colMap[col].append((row, node.val))
-            
+            node, r, c = q.popleft()
+            colGroups[c].append((r, node.val))
+            minCol = min(minCol, c)
+            maxCol = max(maxCol, c)
             if node.left:
-                q.append((row+1, col-1, node.left))
+                q.append((node.left, r+1, c-1))
+            
             if node.right:
-                q.append((row+1,col+1, node.right))
-        answer = [] 
+                q.append((node.right, r+1, c+1))
+        answer = []
         for i in range(minCol, maxCol+1):
-            answer.append([val for row, val in sorted(colMap[i])])
+            answer.append([val for row, val in sorted(colGroups[i])])
             
         return answer
         
-    # Time: N log N
-    # Space: N
-            
-        
-        
-        
-            
