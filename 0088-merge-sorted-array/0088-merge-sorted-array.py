7e28c2069845b305cb90ac88d6dc7281241326db
Quai Wentt <quai.wentt@gmail.com>
2024-02-15 11:22:58 -0500

    Time: 33 ms (93.25%), Space: 16.6 MB (61.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-      #  [1,2,2,3,5,6] [2,5,6]
-      #     P1        P2
-      #             R
+        [1,2,3,0,0,0]
+      #      I     P
+        [2,5,6]
+      #      J
+    
+        P = len(nums1) - 1
+        i = m - 1
+        j = n - 1
         
-        
-        P1 = m - 1
-        P2 = n - 1
-        
-        for R in range(len(nums1)-1,-1,-1):
-            num1 = nums1[P1] if P1 >= 0 else float('-inf')
-            num2 = nums2[P2] if P2 >= 0 else float('-inf')
+        while P >= 0:
+            num1 = nums1[i] if i in range(len(nums1)) else float('-inf')
+            num2 = nums2[j] if j in range(len(nums2)) else float('-inf')
             if num1 > num2:
-                nums1[R] = num1
-                P1 -= 1
+                nums1[P], nums1[i] = nums1[i], nums1[P]
+                i -= 1
             else:
-                nums1[R] = num2
-                P2 -= 1
-        
+                nums1[P], nums2[j] = nums2[j], nums1[P]
+                j -= 1
+            P -= 1
         
