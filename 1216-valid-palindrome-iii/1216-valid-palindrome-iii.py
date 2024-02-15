b1ce94b1eebc76aa435146609e69037f9ab73fae
Quai Wentt <quai.wentt@gmail.com>
2024-02-15 15:10:52 -0500

    Time: 1169 ms (20.93%), Space: 256.5 MB (24.65%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -7,14 +7,17 @@ class Solution:
                 return memo[(L,R,k)]
             while L < R:
                 if s[L] != s[R]:
-                    if k > 0:
-                        answer = kPal(L+1,R, k-1) or kPal(L, R-1, k-1)
+                    if k:
+                        answer = kPal(L+1,R,k-1) or kPal(L,R-1,k-1)
                         memo[(L,R,k)] = answer
                         return answer
                     else:
                         memo[(L,R,k)] = False
                         return False
+                    
                 L += 1
                 R -= 1
+                    
             return True
+        
         return kPal(0,len(s)-1,k)
