5a7641611e8c6fce5780e1eae358e2720bacb066
Quai Wentt <quai.wentt@gmail.com>
2024-02-16 10:58:48 -0500

    Time: 44 ms (57.56%), Space: 16.7 MB (50.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,27 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
         def reverse(L,R):
             while L < R:
                 nums[L], nums[R] = nums[R], nums[L]
                 L += 1
                 R -= 1
             
-        
         pivot = None
-#        [2,3,1]
-        for i in range(len(nums)-1,0,-1):
+        
+        for i in range(len(nums)-1, 0, -1):
             if nums[i-1] < nums[i]:
                 pivot = i - 1
                 break
-             
+                
         if pivot != None:
             for i in range(len(nums)-1, pivot, -1):
                 if nums[i] > nums[pivot]:
                     nums[i], nums[pivot] = nums[pivot], nums[i]
                     break
             reverse(pivot+1, len(nums)-1)
-            
         else:
             nums.reverse()
+        
+        return nums
