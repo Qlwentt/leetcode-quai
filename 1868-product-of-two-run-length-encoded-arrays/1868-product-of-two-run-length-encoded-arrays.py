b88ff8a1fd5580e9878ad229bb6cfcc38e770f46
Quai Wentt <quai.wentt@gmail.com>
2024-02-17 10:46:39 -0500

    Time: 2019 ms (53.16%), Space: 74.5 MB (65.26%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
@@ -1,33 +1,40 @@
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
+  #     [[1,3],[2,1],[3,2]] [[2,3],[3,2]]
+   #                   P                   
+   #                                 Q
+    #  [[2,3]][6,1][9,2]
+    
+#     [[1,3],[2,3]] [[6,3],[3,3]]
+#              P             Q
+        
+#     [6,6]
+
         p1 = 0
         p2 = 0
         answer = []
         while p1 < len(encoded1) and p2 < len(encoded2):
-            val1, freq1 = encoded1[p1]
-            val2, freq2 = encoded2[p2]
+            num1, count1 = encoded1[p1]
+            num2, count2 = encoded2[p2]
             
-            if freq1 == freq2:
-                freqAnswer = freq1
+            ansNum = num1 * num2
+            ansCount = min(count1, count2)
+            if count1 < count2:
+                p1 += 1
+                newCount = count2 - count1
+                encoded2[p2][1] = newCount
+            elif count2 < count1:
+                p2 += 1
+                newCount = count1 - count2
+                encoded1[p1][1] = newCount
+            else: # equal
                 p1 += 1
                 p2 += 1
-            elif freq1 < freq2:
-                freqAnswer = freq1
-                encoded2[p2][1] = freq2-freq1
-                p1 += 1
-            else: # freq1 > freq2
-                freqAnswer = freq2
-                encoded1[p1][1] = freq1 - freq2
-                p2 += 1
-            
-            valAnswer = val1 * val2
-            
-            if answer and answer[-1][0] == valAnswer:
-                answer[-1] = [valAnswer, freqAnswer + answer[-1][1]]
+                
+            if answer and answer[-1][0] == ansNum:
+                answer[-1][1] += ansCount
             else:
-                answer.append([valAnswer, freqAnswer])
+                answer.append([ansNum, ansCount])
                 
         return answer
-    
-# Time: O(M+N)
-# Space: O(M+N)
+            
