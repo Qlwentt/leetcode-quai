b98b1a37ab2daa1abe3bb7300ae27a503035f8a0
Quai Wentt <quai.wentt@gmail.com>
2024-02-18 11:48:47 -0500

    Time: 739 ms (31.44%), Space: 98 MB (5.06%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -2,18 +2,14 @@ from collections import defaultdict, deque
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
         groups = defaultdict(lambda: deque([]))
-        ROWS = len(nums)
-        for r in range(ROWS):
+        
+        for r in range(len(nums)):
             for c in range(len(nums[r])):
                 groups[r+c].appendleft(nums[r][c])
-                
-        i = 0
-        answer = []
-        while i in groups:
-            answer.extend(groups[i])
-            i += 1
         
+        answer = []
+        for i in range(max(groups)+1):
+            answer.extend(groups[i])
+            
         return answer
             
-# Time: O(N) N is number of numbers in nums
-# Space: O(N)
