7f6e977c4e47e2723797bb97c99cedbacb591409
Quai Wentt <quai.wentt@gmail.com>
2024-02-18 11:38:44 -0500

    Time: 185 ms (9.93%), Space: 17.5 MB (59.63%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,25 +1,28 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
         balance = 0
-        
         i = 0
         add = 0
+        "))())("
+         
         while i < len(s):
             if s[i] == "(":
                 balance += 2
                 i += 1
-            else:                  
+            else:
                 balance -= 2
+                
                 if i + 1 in range(len(s)) and s[i+1] == ")":
                     i += 2
                 else:
                     i += 1
                     add += 1
+                
                 if balance < 0:
-                    if balance == -2: 
+                    if balance == -2:
                         add += 1
                     else:
                         add += 2
                     balance = 0
-                    
-        return balance + add
+        return add + balance
+        
