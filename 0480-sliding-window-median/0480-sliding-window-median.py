f4d53c8705c9b7d4f149fb15774adbef654dd913
Quai Wentt <quai.wentt@gmail.com>
2024-02-18 10:55:32 -0500

    Time: 507 ms (33.44%), Space: 34 MB (17.69%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -1,24 +1,16 @@
 from sortedcontainers import SortedSet
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        
+        window = SortedSet()
         L = 0
-        
-        window = SortedSet() 
-        
         answer = []
         for R in range(len(nums)):
             window.add((nums[R], R))
-            
-            while R - L + 1 > k:
+            while R - L + 1 == k:
+                median = window[k // 2][0] if k % 2 else (window[k//2][0] + window[k//2 -1][0]) / 2
+                answer.append(median)
                 window.remove((nums[L], L))
                 L += 1
             
-            if R - L + 1 == k:
-                median = window[k//2][0] if k % 2 else (window[k//2][0] + window[k//2 -1][0]) / 2
-                answer.append(median)
-        
         return answer
-
-    # O( N log(k))
-    # O(k)
+            
