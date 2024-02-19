fedfbb2569261021f382e8b876807e6a91575c1a
Quai Wentt <quai.wentt@gmail.com>
2024-02-19 10:12:18 -0500

    Time: 792 ms (5.78%), Space: 46.3 MB (5.03%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -3,29 +3,28 @@ class Solution:
         ROWS = len(matrix)
         COLS = len(matrix[0])
         memo = {}
-        
-        def dfs(r,c, prev):
+        def dfs(r,c,prev):
             if (r not in range(ROWS) or
                 c not in range(COLS) or
-                matrix[r][c] <= prev
+                prev >= matrix[r][c]
                ):
                 return 0
-            
-            if (r,c) in memo:
-                return memo[(r,c)]
+            if (r,c, prev) in memo:
+                return memo[(r,c, prev)]
             cur = matrix[r][c]
+            
             up = 1 + dfs(r-1, c, cur)
             down = 1 + dfs(r+1, c, cur)
             left = 1 + dfs(r, c-1, cur)
             right = 1 + dfs(r, c+1, cur)
             
-            answer = max(up, down, left, right)
-            memo[(r,c)] = answer
+            answer = max(up,down,left,right)
+            memo[(r,c,prev)] = answer
+            
             return answer
-        maxLen = 1
+        longest = float('-inf')
         for r in range(ROWS):
             for c in range(COLS):
-                maxLen = max(maxLen, dfs(r,c, float('-inf')))
-        return maxLen
-# Time: O(M*N)
-# Space: O(M*N)
+                longest = max(dfs(r,c, float('-inf')), longest)
+        
+        return longest
