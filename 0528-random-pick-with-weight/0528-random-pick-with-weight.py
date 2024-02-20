81d1c5877e91422e0ac3ced28ec84115d51e0c78
Quai Wentt <quai.wentt@gmail.com>
2024-02-20 13:15:28 -0500

    Time: 179 ms (38.96%), Space: 21.1 MB (87.39%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,24 +1,21 @@
-from random import randint
+import random
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.total = 0
-        self.choices = [0]* len(w)
-
+        self.totalSum = 0
+        self.choices = [0]*len(w)
         for i, weight in enumerate(w):
-            self.total+= weight
-            self.choices[i] = self.total
-            
+            self.totalSum += weight
+            self.choices[i] = self.totalSum
         
 
     def pickIndex(self) -> int:
-        target = randint(1,self.total)
-        
+        target = randint(1, self.totalSum)
         lo = 0
         hi = len(self.choices) - 1
         
         while lo <= hi:
-            mid = (lo+hi) // 2
+            mid = (lo + hi) // 2
             
             if self.choices[mid] >= target:
                 hi = mid - 1
