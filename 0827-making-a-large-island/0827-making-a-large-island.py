eae9f11d1e6ba8a34fbcbbfd8ae6295724ae6e7e
Quai Wentt <quai.wentt@gmail.com>
2024-02-23 16:30:04 -0500

    Time: 2967 ms (15.43%), Space: 32.2 MB (38.25%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,49 +1,50 @@
-from collections import defaultdict
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        i = 1
-        areas = defaultdict(int)
-        areas["0"] = 0
-        def getArea(r, c):
+        i = 0
+        def getArea(r,c):
             if (
                 r not in range(ROWS) or
-                c not in range(COLS) or 
-                isinstance(grid[r][c], str) or
-                grid[r][c] == 0
+                c not in range(COLS) or
+                grid[r][c] == 0 or
+                isinstance(grid[r][c], str) 
             ):
                 return 0
-            
             grid[r][c] = str(i)
-            
             up = getArea(r-1, c)
             down = getArea(r+1, c)
             left = getArea(r, c-1)
             right = getArea(r, c+1)
             
             return up + down + left + right + 1
-        
+        areasById = {'0':0}
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    area = getArea(r,c)
-                    areas[str(i)] = area
                     i += 1
-        directions = [[0,1],[1,0], [-1,0], [0, -1]]    
-        answer = float('-inf')
+                    area = getArea(r,c)
+                    areasById[str(i)] = area
+        largestIsland = float('-inf')
+        directions = [[0,1],[1,0], [0,-1], [-1,0]]
         for r in range(ROWS):
             for c in range(COLS):
-                ids = set()
                 if grid[r][c] == 0:
+                    idsForAreas = set()
                     for dr, dc in directions:
-                        newR, newC = dr + r, dc + c
-                        if newR in range(ROWS) and newC in range(COLS):
-                            ids.add(grid[r+dr][c+dc])
-                    sumAreas = 0
-                    for id_ in ids:
-                        sumAreas += areas[id_]
-                    answer = max(sumAreas+1, answer)                    
-                
-        return answer if answer != float('-inf') else max(areas.values())
-                
+                        newR = dr + r
+                        newC = dc + c
+                        if (
+                            newR in range(ROWS) and
+                            newC in range(COLS) and
+                            isinstance(grid[newR][newC], str)
+                        ):
+                            idsForAreas.add(grid[newR][newC])
+                    curArea = 1        
+                    for id_ in idsForAreas:
+                        curArea += areasById[id_]
+                    largestIsland = max(largestIsland, curArea)
+                    
+        return largestIsland if largestIsland != float('-inf') else max(areasById.values())
+                        
+        
