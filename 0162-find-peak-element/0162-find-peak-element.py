932d8bc088dac1799291f298e2eb4fb5f6ca6b9f
Quai Wentt <quai.wentt@gmail.com>
2024-02-23 18:23:23 -0500

    Time: 44 ms (77.85%), Space: 16.9 MB (45.69%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -4,16 +4,13 @@ class Solution:
         R = len(nums) - 1
         
         while L <= R:
-            mid = (L + R) // 2
-            1,2,3,1
-            if mid + 1 < len(nums) and nums[mid+1] > nums[mid]:
-                # search to the right
+            mid = (L+R) // 2
+            
+            if mid - 1 in range(len(nums)) and nums[mid-1] > nums[mid]:
+                R = mid -1
+            elif mid + 1 in range(len(nums)) and nums[mid+1] > nums[mid]:
                 L = mid + 1
-            elif mid - 1 >= 0 and nums[mid-1] > nums[mid]:
-                # search to the left
-                R = mid - 1
             else:
                 return mid
-        
         
         
