2347a7c38d46fe33e7547e8873d278f9bde51f6f
Quai Wentt <quai.wentt@gmail.com>
2024-02-25 15:05:49 -0500

    Time: 583 ms (14.31%), Space: 33.9 MB (78.21%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,13 +1,11 @@
 from collections import deque
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        maxSeen = float("-inf") # 3
+        maxSeen = float('-inf') 
         answer = deque([])
-     #
         for i, height in reversed(list(enumerate(heights))):
             if height > maxSeen:
                 answer.appendleft(i)
             maxSeen = max(height, maxSeen)
-        
+
         return answer
-        
