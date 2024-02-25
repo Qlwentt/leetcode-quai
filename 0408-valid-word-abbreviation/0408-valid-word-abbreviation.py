851449d233fa020567b1c27717e9fd21e1d33309
Quai Wentt <quai.wentt@gmail.com>
2024-02-25 12:25:49 -0500

    Time: 39 ms (38.35%), Space: 16.6 MB (74.44%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -2,24 +2,18 @@ class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         abbr = re.split(r'(\d+)', abbr)
         abbr = [item for item in abbr if item != ""]
-        p1 = 0
-        p2 = 0
         
-        while p2 < len(abbr):
+        p1 = 0
+        
+        for p2 in range(len(abbr)):
             if abbr[p2].isnumeric():
                 length = int(abbr[p2])
-                if abbr[p2][0] == '0' or p1 + length -1 not in range(len(word)):
+                if abbr[p2][0] == "0" or p1 + length - 1 not in range(len(word)):
                     return False
             else:
                 length = len(abbr[p2])
-                
                 if p1 + length -1 not in range(len(word)) or word[p1:p1+length] != abbr[p2]:
-                    
                     return False
             p1 += length
-            p2 += 1
-            
         return p1 == len(word)
-            
-                
         
