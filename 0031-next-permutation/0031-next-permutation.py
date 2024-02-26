f6c344b1b50bb5640af85a59513e8ba176b671a9
Quai Wentt <quai.wentt@gmail.com>
2024-02-26 13:02:07 -0500

    Time: 42 ms (69.68%), Space: 16.7 MB (47.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -8,21 +8,20 @@ class Solution:
                 nums[L], nums[R] = nums[R], nums[L]
                 L += 1
                 R -= 1
-            
-        pivot = None
         
-        for i in range(len(nums)-1, 0, -1):
-            if nums[i-1] < nums[i]:
-                pivot = i - 1
+        pivot = None
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                pivot = i
+                break
+        
+        if pivot == None:
+            nums.reverse()
+            return
+        
+        for i in range(len(nums)-1,pivot, -1):
+            if nums[i] > nums[pivot]:
+                nums[i], nums[pivot] = nums[pivot], nums[i]
+                reverse(pivot+1, len(nums)-1)
                 break
                 
-        if pivot != None:
-            for i in range(len(nums)-1, pivot, -1):
-                if nums[i] > nums[pivot]:
-                    nums[i], nums[pivot] = nums[pivot], nums[i]
-                    break
-            reverse(pivot+1, len(nums)-1)
-        else:
-            nums.reverse()
-        
-        return nums
