80629a863db18f6f53fa77243492e79b8a5effa2
Quai Wentt <quai.wentt@gmail.com>
2024-02-29 14:11:23 -0500

    Time: 49 ms (11.88%), Space: 16.9 MB (32.80%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -2,13 +2,11 @@ from collections import defaultdict
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
-        
         for s in strings:
-            charToNum = {char: i for i, char in enumerate("abcdefghijklmnopqrstuvwxyz")}
-            encode = []
+            encoded = []
+            letterToNum = {char:i for i, char in enumerate("abcdefghijklmnopqrstuvwxyz")}
             for i in range(len(s)-1):
-                diff = (charToNum[s[i]] - charToNum[s[i+1]]) % 26
-                encode.append(diff)
-            groups[tuple(encode)].append(s)
+                encoded.append((letterToNum[s[i]] - letterToNum[s[i+1]]) % 26)
+            groups[tuple(encoded)].append(s)
             
         return groups.values()
