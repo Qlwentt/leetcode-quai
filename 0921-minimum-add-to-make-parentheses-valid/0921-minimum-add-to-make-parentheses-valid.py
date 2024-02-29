ce51e77e55db19b59ac0e5a148cd89a914ff78d8
Quai Wentt <quai.wentt@gmail.com>
2024-02-29 14:02:26 -0500

    Time: 47 ms (5.75%), Space: 16.5 MB (61.47%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -2,14 +2,16 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         balance = 0
         add = 0
+        
         for char in s:
             if char == "(":
                 balance += 1
-            elif char == ")":
+            else:
                 balance -= 1
                 
-            if balance < 0:
-                add += 1
-                balance = 0
+                if balance < 0:
+                    add += 1
+                    balance = 0
         return balance + add
+        
         
