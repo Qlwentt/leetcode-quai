bfa08e956a366ad3d3e88cc1d59e72ad2539dac5
Quai Wentt <quai.wentt@gmail.com>
2024-02-29 12:51:04 -0500

    Time: 48 ms (10.04%), Space: 16.6 MB (72.89%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,13 +1,46 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        noNeg = re.sub(r"(\+|\-)", "", s)
-        if noNeg.isalpha():
-            return False
-        try:
-            num = float(s)
-        except ValueError:
-            return False
+        def isDecimal(s):
+            if s == "":
+                return False
+            if s[0] == "-" or s[0]== "+":
+                return isDecimal(s[1:])
+            s = s.split(".")
+            if len(s) != 2:
+                return False
+            if s[0] == "" and s[1] == "":
+                return False
+            
+            return (isInteger(s[0], True) or s[0] == "")  and (isInteger(s[1], False) or s[1] == "")
+        def isInteger(s, canHaveSign):
+            if s == "":
+                return False
+            if s[0] == "-" or s[0] == "+":
+                if not canHaveSign:
+                    return False
+                return isInteger(s[1:], False)
+            for i, char in enumerate(s):
+                if not char.isdigit():
+                    return False  
+            return True
         
-        return True
+        s = s.lower()
+        splitByE = re.split(r'e', s)
+        
+        if len(splitByE) > 2:
+            return False
+        if len(splitByE) == 0:
+            return False
+        if len(splitByE) == 1 and "e" in s:
+            return False
+        if len(splitByE) == 1:
+            return isDecimal(splitByE[0]) or isInteger(splitByE[0], True)
+        # len is 2
+        if splitByE[0] == "" or splitByE[1] == "":
+            return False
+        return (isDecimal(splitByE[0]) or isInteger(splitByE[0], True)) and isInteger(splitByE[1], True)
+            
+        
+        
             
         
