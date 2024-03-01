e5cbc02ef3b5531b1ffce5b9e4bbc457c37129a6
Quai Wentt <quai.wentt@gmail.com>
2024-03-01 12:04:08 -0500

    Time: 81 ms (40.49%), Space: 16.6 MB (49.45%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -5,8 +5,8 @@ class Solution:
         
         for r in range(ROWS):
             for c in range(COLS):
-                if r + 1 in range(ROWS) and c + 1 in range(COLS):
+                if r+1 in range(ROWS) and c + 1 in range(COLS):
                     if matrix[r][c] != matrix[r+1][c+1]:
                         return False
-                    
         return True
+        
