dd99b20a7a47f7279d4ef3bd94ba70836545eec2
Quai Wentt <quai.wentt@gmail.com>
2024-03-01 11:30:46 -0500

    Time: 8061 ms (5.01%), Space: 17.7 MB (24.08%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -11,18 +11,21 @@ class Solution:
             
             char = s[i]
             incr = 0
+            
             if char == "(":
                 incr = 1
             elif char == ")":
                 incr = -1
                 
             balance += incr
+            
             if balance >= 0:
                 curParens.append(char)
                 backtrack(i+1, curParens, balance)
                 curParens.pop()
-            balance -= incr
                 
-            backtrack(i+1, curParens,balance)
+            balance -= incr
+            backtrack(i+1, curParens, balance)
         backtrack(0, [], 0)
-        return answers[max(answers)] if answers else []
+        return answers[max(answers)] if len(answers) > 0 else []
+            
