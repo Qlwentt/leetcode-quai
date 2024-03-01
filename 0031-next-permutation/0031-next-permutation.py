c64c1c71ef8cde6bbf2b5c45d10c46f6753b6c1a
Quai Wentt <quai.wentt@gmail.com>
2024-03-01 13:07:53 -0500

    Time: 42 ms (67.80%), Space: 16.5 MB (95.66%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -10,18 +10,20 @@ class Solution:
                 R -= 1
         
         pivot = None
-        for i in range(len(nums)-2,-1,-1):
+        
+        for i in range(len(nums)-2, -1,-1):
             if nums[i] < nums[i+1]:
                 pivot = i
                 break
-        
+                
         if pivot == None:
             nums.reverse()
             return
         
-        for i in range(len(nums)-1,pivot, -1):
+        for i in range(len(nums)-1, pivot, -1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 reverse(pivot+1, len(nums)-1)
                 break
                 
+        
