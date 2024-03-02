925aa5b9a794f8b01bf5bac128d1168561aae7b1
Quai Wentt <quai.wentt@gmail.com>
2024-03-02 13:28:29 -0500

    Time: 59 ms (98.45%), Space: 16.9 MB (37.17%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,20 +1,21 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         stack = []
-        answer = [0]*n
+        answer = [0] * n
         for log in logs:
-            id_, command, time = log.split(":")
-            time = int(time)
+            id_, command, timestamp = log.split(":")
             id_ = int(id_)
+            timestamp = int(timestamp)
             
             if command == "start":
-                stack.append((id_,time))
+                stack.append((id_, timestamp))
             else:
-                sId , start = stack.pop()
-                end = time
+                end = timestamp
+                start = stack.pop()[1]
                 duration = end - start + 1
-                answer[sId] += duration
+                answer[id_] += duration
                 if stack:
-                    prevId, time = stack[-1]
+                    prevId = stack[-1][0]
                     answer[prevId] -= duration
         return answer
+        
