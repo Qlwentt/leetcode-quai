ecdc1f448deeb0d9939ef9c16967a7a4948d4e7d
Quai Wentt <quai.wentt@gmail.com>
2024-03-02 12:29:53 -0500

    Time: 6783 ms (5.71%), Space: 17.4 MB (49.28%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
@@ -3,27 +3,25 @@ class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        
         buildings = []
-        
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 0:
-                    grid[r][c] = (0,0)
-                else:
-                    if grid[r][c] == 1:
-                        buildings.append([r,c])
+                if grid[r][c] == 1:
+                    buildings.append((r,c))
                     grid[r][c] = str(grid[r][c])
+                elif grid[r][c] == 2:
+                    grid[r][c] = str(grid[r][c])
+                else:
+                    grid[r][c] = (0,0) # total distance to all buildings, # number of buildings can reach
+                    
         
         def bfs(startR, startC):
             q = deque([(startR, startC, 0)])
-            visited = set([(startR, startC)])
-            
-            directions = [[0,1],[1,0],[0,-1], [-1,0]]
-            
+            visited = set((startR, startC))
+            directions = [[0,1],[1,0],[0,-1], [-1, 0]]
             while q:
                 r, c, distance = q.popleft()
-                if not isinstance(grid[r][c], str):
+                if distance:
                     grid[r][c] = (grid[r][c][0] + distance, grid[r][c][1] + 1)
                 
                 for dr, dc in directions:
@@ -32,20 +30,20 @@ class Solution:
                     if (
                         newR in range(ROWS) and
                         newC in range(COLS) and
-                        (newR,newC) not in visited and
+                        (newR, newC) not in visited and
                         not isinstance(grid[newR][newC], str)
                     ):
-                        q.append((newR,newC,distance+1))
-                        visited.add((newR,newC))
+                        q.append((newR, newC, distance+1))
+                        visited.add((newR, newC))
                         
-        for r,c in buildings:
+        for r, c in buildings:
             bfs(r,c)
-        minDistance = float('inf')   
+        minDistance = float('inf')
         for r in range(ROWS):
             for c in range(COLS):
                 if not isinstance(grid[r][c], str) and grid[r][c][1] == len(buildings):
                     minDistance = min(minDistance, grid[r][c][0])
-                    
+        
         return minDistance if minDistance != float('inf') else -1
                 
-            
+        
