69d2d6fe6e5103a15e0eeca3853ea000540cf7fd
Quai Wentt <quai.wentt@gmail.com>
2024-03-02 15:33:07 -0500

    Time: 1978 ms (80.95%), Space: 74.5 MB (53.08%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
@@ -1,40 +1,27 @@
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
-  #     [[1,3],[2,1],[3,2]] [[2,3],[3,2]]
-   #                   P                   
-   #                                 Q
-    #  [[2,3]][6,1][9,2]
-    
-#     [[1,3],[2,3]] [[6,3],[3,3]]
-#              P             Q
-        
-#     [6,6]
-
         p1 = 0
         p2 = 0
         answer = []
         while p1 < len(encoded1) and p2 < len(encoded2):
-            num1, count1 = encoded1[p1]
-            num2, count2 = encoded2[p2]
+            val1, freq1 = encoded1[p1] 
+            val2, freq2 = encoded2[p2]
             
-            ansNum = num1 * num2
-            ansCount = min(count1, count2)
-            if count1 < count2:
+            ansVal = val1 * val2
+            ansFreq = min(freq1, freq2)
+            if freq1 < freq2:
                 p1 += 1
-                newCount = count2 - count1
-                encoded2[p2][1] = newCount
-            elif count2 < count1:
+                encoded2[p2][1] -= freq1
+            elif freq2 < freq1:
                 p2 += 1
-                newCount = count1 - count2
-                encoded1[p1][1] = newCount
+                encoded1[p1][1] -= freq2
             else: # equal
                 p1 += 1
                 p2 += 1
-                
-            if answer and answer[-1][0] == ansNum:
-                answer[-1][1] += ansCount
-            else:
-                answer.append([ansNum, ansCount])
-                
-        return answer
             
+            if answer and answer[-1][0] == ansVal:
+                answer[-1][1] += ansFreq
+            else:
+                answer.append([ansVal, ansFreq])
+        
+        return answer
