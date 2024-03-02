505a256113dccc0cf5bd5fcdc0ce8549d6926e12
Quai Wentt <quai.wentt@gmail.com>
2024-03-02 13:16:43 -0500

    Time: 30 ms (98.58%), Space: 16.8 MB (40.59%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -4,15 +4,16 @@ class Solution:
             return "1"
         
         last = self.countAndSay(n-1)
-        prev = None
         count = 0
+        prev = None
         answer = []
-        for item in last:
-            if prev and item != prev:
+        for num in last:
+            if prev and num != prev:
                 answer.append(str(count) + prev)
                 count = 1
             else:
                 count += 1
-            prev = item
+            prev = num
         answer.append(str(count) + prev)
         return "".join(answer)
+        
