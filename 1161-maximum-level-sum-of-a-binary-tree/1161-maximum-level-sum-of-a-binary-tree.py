a04a2cc001309aa6813db3c33633b87456a05fa2
Quai Wentt <quai.wentt@gmail.com>
2024-03-03 12:27:51 -0500

    Time: 157 ms (57.86%), Space: 20 MB (51.50%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -4,28 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return -1
         q = deque([root])
         maxSum = float('-inf')
-        level = 0
-        answer = 0
+        ansLevel = None
+        curLevel = 0
         while q:
-            runSum = 0
-            level += 1
+            curSum = 0
+            curLevel += 1
             for _ in range(len(q)):
                 node = q.popleft()
-                runSum += node.val
+                curSum += node.val
                 
                 if node.left:
                     q.append(node.left)
+                    
                 if node.right:
                     q.append(node.right)
-            if runSum > maxSum:
-                maxSum = runSum
-                answer = level
-        return answer
-            
+            if curSum > maxSum:
+                ansLevel = curLevel
+            maxSum = max(maxSum, curSum)
+        
+        return ansLevel
+        
