d2d2124477bf91db6e1bb97863e90a7fb89b7036
Quai Wentt <quai.wentt@gmail.com>
2024-03-03 10:03:47 -0500

    Time: 83 ms (90.09%), Space: 18.5 MB (52.00%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,27 +4,24 @@ class Solution:
         
         for course, pre in prerequisites:
             adjList[course].append(pre)
-            
         visited = set()
         visiting = set()
-        
         def dfs(course):
-            if course in visited:
-                return True
             if course in visiting:
                 return False
+            if course in visited:
+                return True
             
             visiting.add(course)
+            visited.add(course)
             for neigh in adjList[course]:
                 if not dfs(neigh):
                     return False
-            
             visiting.remove(course)
-            visited.add(course)
             return True
         
         for i in range(numCourses):
             if not dfs(i):
                 return False
-        
+            
         return True
