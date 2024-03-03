1c2e8cb9e8a51f520676986e67cc23be1252a379
Quai Wentt <quai.wentt@gmail.com>
2024-03-03 16:30:38 -0500

    Time: 43 ms (40.73%), Space: 16.7 MB (49.45%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,38 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # memoization
-#         wordDict = set(wordDict)
-#         memo = {}
-#         def backtrack(i):
-#             if i >= len(s):
-#                 return True
-            
-#             if i in memo:
-#                 return memo[i]
-            
-#             for j in range(i, len(s)):
-#                 curPart = s[i:j+1]
-#                 if curPart in wordDict:
-#                     if backtrack(j+1):
-#                         memo[i] = True
-#                         return memo[i]
-#             memo[i] = False      
-#             return memo[i]
-#         return backtrack(0)
-    
-        # dp
+        memo = {}
         wordDict = set(wordDict)
-        n = len(s)
-        dp = [False] * (n + 1)
-        dp[n] = True
-        
-        for i in range(n-1, -1,-1):
-            for j in range(n-1, i-1, -1):
+    
+        def backtrack(i):
+            if i == len(s):
+                return True
+            if i in memo:
+                return memo[i]
+            
+            for j in range(i, len(s)):
                 curPart = s[i:j+1]
                 if curPart in wordDict:
-                    dp[i] = dp[j+1]
-                    if dp[i]:
-                        break
-        return dp[0]
+                    if backtrack(j+1):
+                        memo[i] = True
+                        return True
+            memo[i] = False
+            return False
         
-        
+        return backtrack(0)
+            
+                    
+                    
+                        
