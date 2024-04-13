485dd78e9e1d2a428708780064dd71cd095238b3
Quai Wentt <quai.wentt@gmail.com>
2024-04-13 10:18:10 -0400

    Time: 163 ms (47.22%), Space: 21.1 MB (78.34%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -2,8 +2,8 @@ import random
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.picks = []
         curSum = 0
+        self.picks = []
         for weight in w:
             curSum += weight
             self.picks.append(curSum)
@@ -12,17 +12,16 @@ class Solution:
     def pickIndex(self) -> int:
         target = randint(1, self.picks[-1])
         L = 0
-        R = len(self.picks) - 1
+        R = len(self.picks) -1
         
         while L <= R:
-            mid = (L + R) // 2
-            if self.picks[mid] >= target: # too hi
+            mid = (L+R) // 2
+            
+            if self.picks[mid] >= target:
                 R = mid - 1
             else:
                 L = mid + 1
         return L
-        
-        
 
 
 # Your Solution object will be instantiated and called as such:
