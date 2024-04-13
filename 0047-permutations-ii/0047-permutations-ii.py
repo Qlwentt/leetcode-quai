bfb060923cca9c6b8ad30a9eb7dc1c724be95f6b
Quai Wentt <quai.wentt@gmail.com>
2024-04-13 18:40:57 -0400

    Time: 45 ms (92.12%), Space: 16.8 MB (81.43%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,23 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-    
-        perms = []
+        answer = []
         nums.sort()
-
         def backtrack(curPerm, curNums):
-            if len(curPerm) == len(nums):
-                perms.append(curPerm.copy())
+            if len(curPerm) == len(nums) :
+                answer.append(curPerm.copy())
                 return
-                        
+            
             for i in range(len(curNums)):
-                next_ = curNums[i+1] if i < len(curNums) - 1 else float('inf')
-                if curNums[i] != next_:
+                if i == 0 or curNums[i-1] != curNums[i]:
                     curPerm.append(curNums[i])
                     backtrack(curPerm, curNums[:i] + curNums[i+1:])
                     curPerm.pop()
-                    
         backtrack([], nums)
-        return perms
-            
+        return answer
                 
-            
