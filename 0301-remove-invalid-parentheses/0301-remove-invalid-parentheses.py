907e596b7ee6ae11a36b0eb730f870edb3041048
Quai Wentt <quai.wentt@gmail.com>
2024-04-16 16:39:41 -0400

    Time: 7896 ms (5.02%), Space: 17.7 MB (18.07%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -2,30 +2,30 @@ from collections import defaultdict
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
         answers = defaultdict(set)
-        
-        def backtrack(i, curParens, balance):
+        def backtrack(i, balance, curParens):
             if i == len(s):
                 if balance == 0:
                     answers[len(curParens)].add("".join(curParens))
                 return
             
             char = s[i]
-            incr = 0
-            
+            add = 0
             if char == "(":
-                incr = 1
+                add = 1
             elif char == ")":
-                incr = -1
-                
-            balance += incr
+                add = -1
+            balance += add
             
             if balance >= 0:
                 curParens.append(char)
-                backtrack(i+1, curParens, balance)
+                backtrack(i+1, balance, curParens)
                 curParens.pop()
                 
-            balance -= incr
-            backtrack(i+1, curParens, balance)
-        backtrack(0, [], 0)
-        return answers[max(answers)] if len(answers) > 0 else []
+            balance -= add
+            backtrack(i+1, balance, curParens)
+        backtrack(0, 0, [])
+        maxLen = max(answers)
+        return answers[maxLen]
             
+                
+        
