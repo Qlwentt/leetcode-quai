7387b788f4879e6025ecda3dfc743def414df8eb
Quai Wentt <quai.wentt@gmail.com>
2024-04-16 14:13:01 -0400

    Time: 664 ms (6.25%), Space: 19.3 MB (9.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,40 +1,31 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        visited = set()
         q = deque([])
-        
         if grid[0][0] == 0:
-            q.append((0,0,1, [(0,0)]))
-            visited.add((0,0))
-        
-        directions = [[0,1],[1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1, -1]]
-        
+            q.append((0,0, 1, [(0,0)]))
+            
+        answer = - 1
+        directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,-1], [1,-1], [-1,1]]
+        N = len(grid)
+        visited = set()
         while q:
-            r, c, level, curPath = q.popleft()
+            r, c, level, path = q.popleft()
             
-            
-            if (r,c) == (ROWS-1, COLS-1):
-                resultPath = curPath
-                # print(resultPath)
+            if r == N-1 and c == N-1:
+                # print(path)
                 return level
             
             for dr, dc in directions:
-                newR = r + dr
-                newC = c + dc
-                
-                if (newR in range(ROWS) and
-                    newC in range(COLS) and
+                newR = dr + r
+                newC = dc + c
+                if (
+                    newR in range(N) and
+                    newC in range(N) and
                     (newR, newC) not in visited and
                     grid[newR][newC] != 1
-                   ):
-                    
-                    q.append((newR, newC, level +1, curPath + [(newR, newC)]))
+                ):
+                    q.append((newR, newC, level+1, path + [(newR,newC)]))
                     visited.add((newR, newC))
-                    
-        
         # print([])
-        return -1
-        
+        return answer
