56d7eb2c4e54a12a5619be461a1a8cde522d4b43
Quai Wentt <quai.wentt@gmail.com>
2024-04-16 12:29:14 -0400

    Time: 2947 ms (8.66%), Space: 32.2 MB (41.36%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -2,14 +2,11 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        i = 1
         def getArea(r,c):
-            if (
-                r not in range(ROWS) or
+            if (r not in range(ROWS) or
                 c not in range(COLS) or
                 isinstance(grid[r][c], str) or
-                grid[r][c] == 0
-            ):
+                grid[r][c] == 0):
                 return 0
             
             grid[r][c] = str(i)
@@ -19,35 +16,34 @@ class Solution:
             left = getArea(r, c-1)
             right = getArea(r, c+1)
             
-            return up + down + left + right + 1
-        idsToArea = {"0": 0}
+            return up+down+left+right+1
+        areas = {"0": 0}
+        i = 1
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     area = getArea(r,c)
-                    idsToArea[str(i)] = area
+                    areas[str(i)] = area
                     i += 1
-        
-        largestIsland = float("-inf")
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        maxArea = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 0:
-                    directions = [[0,1], [1,0], [-1, 0], [0, -1]]
-                    idsForAreas = set()
+                    adjIds = set()
                     for dr, dc in directions:
                         newR = dr + r
                         newC = dc + c
-                        if (
-                            newR in range(ROWS) and
-                            newC in range(COLS) and
-                            isinstance(grid[newR][newC], str)
-                        ):
-                            idsForAreas.add(grid[newR][newC])
-                    totalArea = 1
-                    for id_ in idsForAreas:
-                        totalArea += idsToArea[id_]
-                    largestIsland = max(totalArea, largestIsland)
+                        if newR in range(ROWS) and newC in range(COLS) and grid[newR][newC] != 0:
+                            adjIds.add(grid[newR][newC])
+                    newArea = 1
+                    for id_ in adjIds:
+                        newArea += areas[id_]
+                    maxArea = max(newArea, maxArea)
                     
-        return largestIsland if largestIsland != float('-inf') else max(idsToArea.values())
+        return maxArea if maxArea != 0 else max(areas.values())
+                    
                         
                     
+                
+        
