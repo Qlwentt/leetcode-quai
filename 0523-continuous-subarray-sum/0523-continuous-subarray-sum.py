b354ecc52063bd27d8c02d364d54aa4c9c9c4f16
Quai Wentt <quai.wentt@gmail.com>
2024-04-17 15:06:22 -0400

    Time: 841 ms (40.99%), Space: 36.3 MB (71.26%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        remainderDict = {0: -1}
+        prefixDict = {}
+        prefixDict[0] = -1
         
-        runSum = 0
+        curSum = 0
         for R, num in enumerate(nums):
-            runSum += num
-            if runSum % k in remainderDict:
-                L = remainderDict[runSum % k]
+            curSum += num
+            if curSum%k in prefixDict:
+                L = prefixDict[curSum % k]
                 if R - L >= 2:
                     return True
-            else:
-                remainderDict[runSum % k] = R
+            else:  
+                prefixDict[curSum %k] = R
         
         return False
-                
+        
