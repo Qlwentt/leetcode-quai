1c28b7a3ae0bda4090e70fdb6565fdc9ff9f637f
Quai Wentt <quai.wentt@gmail.com>
2024-04-17 12:08:02 -0400

    Time: 41 ms (70.89%), Space: 16.5 MB (52.92%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,27 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # find first number that is decreasing from right to left
         def reverse(L,R):
             while L < R:
                 nums[L], nums[R] = nums[R], nums[L]
                 L += 1
                 R -= 1
-        
+            
         pivot = None
-        
-        for i in range(len(nums)-2, -1,-1):
-            if nums[i] < nums[i+1]:
-                pivot = i
+        for i in range(len(nums)-1,0,-1):
+            if nums[i-1] < nums[i]:
+                pivot = i - 1
                 break
-                
+        
         if pivot == None:
             nums.reverse()
             return
-        
-        for i in range(len(nums)-1, pivot, -1):
+        # find first number that is greater than pivot from right to left, swap
+        for i in range(len(nums)-1,-1,-1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
+                # reverse remaining
                 reverse(pivot+1, len(nums)-1)
-                break
-                
+                return
+        
+        
+        
+        
         
