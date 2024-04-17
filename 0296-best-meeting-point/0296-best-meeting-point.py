15368648f99e5f176411e6c5a1ab14328ebad93a
Quai Wentt <quai.wentt@gmail.com>
2024-04-17 14:28:18 -0400

    Time: 95 ms (67.44%), Space: 17.9 MB (73.53%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.py
+++ 0296-best-meeting-point/0296-best-meeting-point.py
@@ -1,27 +1,24 @@
 class Solution:
     def minTotalDistance(self, grid: List[List[int]]) -> int:
+        housesRows = []
         ROWS = len(grid)
         COLS = len(grid[0])
-        houses = []
-        rowHouses = []
-        colHouses = []
         
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    houses.append((r,c))
-                    rowHouses.append(r)
-                    
+                    housesRows.append(r)
+        housesCols = []
         for c in range(COLS):
             for r in range(ROWS):
                 if grid[r][c] == 1:
-                    colHouses.append(c)
-        
-        medianRows = rowHouses[len(rowHouses) // 2]
-        medianCols = colHouses[len(colHouses) // 2]
-        
+                    housesCols.append(c)
+                   
+        medianR = housesRows[len(housesRows) // 2]
+        medianC = housesCols[len(housesCols) // 2]
         distance = 0
-        for x1,y1 in houses:
-            distance += abs(x1-medianRows) + abs(y1-medianCols)
-
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    distance += abs(medianR-r) + abs(medianC - c)
         return distance
