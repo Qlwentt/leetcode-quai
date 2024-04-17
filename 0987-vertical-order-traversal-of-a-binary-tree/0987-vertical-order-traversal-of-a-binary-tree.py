acb2b1464edd42c94c4667e60dc88e5282a61709
Quai Wentt <quai.wentt@gmail.com>
2024-04-17 12:35:50 -0400

    Time: 43 ms (18.47%), Space: 17 MB (5.01%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,30 +7,24 @@
 from collections import deque, defaultdict
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        q = deque([(root, 0, 0)])
-        
+        q = deque([(root, 0,0)])
+        colGroups = defaultdict(list)
         minCol = 0
         maxCol = 0
-        answer = []
-        groups = defaultdict(list)
-        
         while q:
             node, row, col = q.popleft()
-            
-            groups[col].append((row, node.val))
             minCol = min(col, minCol)
             maxCol = max(col, maxCol)
-            
+            colGroups[col].append((row,node.val))
             if node.left:
-                q.append((node.left, row+1, col-1))
+                q.append((node.left, row+1,col-1))
             
             if node.right:
                 q.append((node.right, row+1, col+1))
-                
+        answer = []
         for i in range(minCol, maxCol+1):
-            answer.append([val for row, val in sorted(groups[i])])
+            answer.append([val for row, val in sorted(colGroups[i])])
             
         return answer
-            
         
         
