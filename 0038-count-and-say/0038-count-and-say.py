84b05acccb73af6c146253d739674034e01a5554
Quai Wentt <quai.wentt@gmail.com>
2024-04-18 12:31:08 -0400

    Time: 35 ms (93.29%), Space: 16.7 MB (29.34%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,18 +2,17 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
+        
         nMinus1 = self.countAndSay(n-1)
         prev = None
         count = 0
         answer = []
         for num in nMinus1:
-            
-            if prev and prev!= num:
-                answer.append(str(count) + prev)
+            if prev and prev != num:
+                answer.append(str(count)+prev)
                 count = 1
             else:
                 count += 1
             prev = num
-        answer.append(str(count)+ prev)
+        answer.append(str(count)+prev)
         return "".join(answer)
-        
