913d652b52f4b3e31f44ca36f01ce9999f9733af
Quai Wentt <quai.wentt@gmail.com>
2024-04-18 15:56:11 -0400

    Time: 675 ms (76.78%), Space: 33.8 MB (19.03%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.py
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.py
@@ -1,11 +1,12 @@
-from collections import defaultdict
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        dayToPerformTask = defaultdict(int)
         day = 0
+        dayToComplete = defaultdict(int)
         for task in tasks:
             day += 1
-            day = dayToPerformTask[task] if day < dayToPerformTask[task] else day
-            dayToPerformTask[task] = day + space + 1
-        
+            if dayToComplete[task] > day:
+                day = dayToComplete[task]
+            dayToComplete[task] = day + space + 1
         return day
+            
+        
