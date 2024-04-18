d86ebb9584340e469bc77ad9f5ebeed0bb389bac
Quai Wentt <quai.wentt@gmail.com>
2024-04-18 14:46:40 -0400

    Time: 38 ms (37.37%), Space: 16.5 MB (41.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mapping = {
-            '2' : 'abc',
-            '3' : 'def',
-            '4' : 'ghi',
-            '5' : 'jkl',
-            '6' : 'mno',
-            '7' : 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-        }
         
+        if not digits:
+            return ""
+        
+        numMap = {"2": 'abc', "3": 'def', "4":'ghi', "5": 'jkl', '6': 'mno', "7": 'pqrs', "8": 'tuv', "9": 'wxyz'}
         answer = []
-        
         def backtrack(i, curComb):
             if i == len(digits):
                 answer.append("".join(curComb))
                 return
             
-            for char in mapping[digits[i]]:
+            for char in numMap[digits[i]]:
                 curComb.append(char)
                 backtrack(i+1, curComb)
                 curComb.pop()
-        backtrack(0, [])   
-        return answer if digits != "" else []
+        
+        backtrack(0,[])
+        return answer
+                
         
