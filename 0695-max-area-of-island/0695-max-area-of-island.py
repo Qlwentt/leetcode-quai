96e8a1fe57f6f2dd2501dd2a6cb4836b04c02ad4
Quai Wentt <quai.wentt@gmail.com>
2024-04-18 15:36:24 -0400

    Time: 141 ms (6.13%), Space: 18.5 MB (15.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,23 +5,28 @@ class Solution:
         visited = set()
         
         def dfs(r,c):
-            if (
+            if ( 
                 r not in range(ROWS) or
                 c not in range(COLS) or
                 (r,c) in visited or
-                grid[r][c] == 0
-            ):
+                grid[r][c] == 0):
                 return 0
+            
             visited.add((r,c))
-            up = dfs(r-1, c)
+            
+            up = dfs(r-1,c)
             down = dfs(r+1, c)
             left = dfs(r, c-1)
             right = dfs(r, c+1)
             
-            return up + down + left + right + 1
+            return up+down+left+right+1
         maxArea = 0
         for r in range(ROWS):
             for c in range(COLS):
-                maxArea = max(dfs(r,c), maxArea)
-                
+                if grid[r][c] == 1:
+                    maxArea = max(dfs(r,c), maxArea)
+                    
         return maxArea
+                
+            
+        
