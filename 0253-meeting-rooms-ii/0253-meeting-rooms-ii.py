fb32e7c427dc786c9e46ad11dc9ab2fee1aabf50
Quai Wentt <quai.wentt@gmail.com>
2024-04-19 15:02:36 -0400

    Time: 78 ms (35.22%), Space: 20.4 MB (14.58%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
         times = []
         
         for start, end in intervals:
-            times.append([start, 1])
-            times.append([end, -1])
+            times.append((start, 1))
+            times.append((end, -1))
             
         times.sort()
         rooms = 0
-        maxRooms = 0
+        answer = float('-inf')
         for time, incr in times:
             rooms += incr
-            maxRooms = max(rooms, maxRooms)
-                    
-        return maxRooms
+            answer = max(answer, rooms)
+        
+        return answer
+            
         
