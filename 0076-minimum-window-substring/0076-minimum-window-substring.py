77de32578e2b8683bdd99973620a8715a39ff643
Quai Wentt <quai.wentt@gmail.com>
2024-04-19 17:06:29 -0400

    Time: 1083 ms (5.00%), Space: 17.2 MB (97.60%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,24 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        
-        counterS = defaultdict(int)
         counterT = Counter(t)
-        
+        counterS = defaultdict(int)
         
         L = 0
+        minLen = len(s) + 1
         ansL = None
         ansR = None
-        minLength = float('inf')
         for R in range(len(s)):
-            counterS[s[R]] += 1
-            
-            while all(counterS[char] >= counterT[char] for char in counterT):
-                minLength = min((R - L + 1), minLength)
-                if R - L + 1 == minLength:
+            char = s[R]
+            counterS[char] += 1
+            while all([counterS[char] >= counterT[char] for char in counterT]):
+                if R - L + 1 < minLen:
                     ansL = L
                     ansR = R
+                minLen = min(minLen, R - L + 1)
                 counterS[s[L]] -= 1
                 L += 1
-        
+                
         return s[ansL:ansR+1] if ansL != None else ""
