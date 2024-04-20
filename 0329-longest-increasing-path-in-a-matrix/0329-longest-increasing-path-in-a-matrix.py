503aaf80c937bf24a8277e3e60e52676ac24a3b2
Quai Wentt <quai.wentt@gmail.com>
2024-04-20 12:27:12 -0400

    Time: 436 ms (11.18%), Space: 21.3 MB (31.10%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,32 +1,32 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        memo = {}
         ROWS = len(matrix)
         COLS = len(matrix[0])
-        
-        memo = {}
-        def dfs(prev, r,c):
-            
-            
+        def dfs(r,c, prev):
             if (
                 r not in range(ROWS) or
                 c not in range(COLS) or
-                prev >= matrix[r][c]
+                matrix[r][c] <= prev
             ):
                 return 0
+            
             if (r,c) in memo:
                 return memo[(r,c)]
             cur = matrix[r][c]
-            up = dfs(cur, r - 1, c) 
-            down = dfs(cur, r+1, c)
-            left = dfs(cur, r, c-1)
-            right = dfs(cur, r, c+ 1)
+            up = dfs(r-1, c, cur) 
+            down = dfs(r+1, c, cur)
+            left = dfs(r, c-1, cur)
+            right = dfs(r, c+1, cur)
             
-            answer = max(up, down, left, right) + 1
-            memo[(r,c)] = answer
-            return answer
-        longestPath = 0
+            ans = max(up, down, left, right) + 1
+            memo[(r,c)] = ans
+            return ans
+        
         for r in range(ROWS):
             for c in range(COLS):
-                longestPath = max(dfs(float('-inf'), r,c), longestPath)
+                dfs(r,c, float('-inf'))
         
-        return longestPath
+        return max(memo.values())
+            
+        
