27c5c98f7d75565a9e1972d317cca51ac362c65f
Quai Wentt <quai.wentt@gmail.com>
2024-05-01 19:19:26 -0400

    Time: 35 ms (93.17%), Space: 16.7 MB (29.09%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,12 +1,19 @@
-from itertools import groupby
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
         
         nMinus1 = self.countAndSay(n-1)
-        answer = [str(len(list(group)))+key for key, group in groupby(nMinus1)]
+        count = 0
+        prev = None
+        answer = []
+        for item in nMinus1:
+            if prev != None and item != prev:
+                answer.append(str(count)+ prev)
+                count = 1
+            else:
+                count += 1
+            prev = item
+        answer.append(str(count)+ prev)
         return "".join(answer)
-        
-        
         
