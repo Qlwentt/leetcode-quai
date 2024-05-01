f007bee254495216eca250550448feace5b6c85a
Quai Wentt <quai.wentt@gmail.com>
2024-05-01 12:19:34 -0400

    Time: 31 ms (90.59%), Space: 16.6 MB (85.54%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,5 +1,6 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        
         path = path.split("/")
         path = [item for item in path if item != ""]
         
@@ -15,3 +16,22 @@ class Solution:
                 stack.append(item)
         
         return "/" + "/".join(stack)
+#     def getPath(pwd,cd):
+#         if not cd:
+#             return pwd
+        
+#         if cd[0] == "/":
+#             pwd = ""
+            
+#         path = pwd + "/" + cd
+#         path = path.split("/")
+#         stack = []
+#         for item in path:
+#             if item == ".":
+#                 continue
+#             elif item == "..":
+#                 if stack:
+#                     stack.pop
+#             else:
+#                 if item:
+#                     stack.append(item)
