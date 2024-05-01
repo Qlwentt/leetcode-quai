46f2e9afecfd2259e622da6393f6a73163d20b55
Quai Wentt <quai.wentt@gmail.com>
2024-05-01 19:07:25 -0400

    Time: 119 ms (38.42%), Space: 26 MB (5.09%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,41 +1,38 @@
-import re
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
-        s = re.split(r'(\d*)', s)
+        s = re.split(r'(\+|\-|\*|\/)', s)
         s = [item for item in s if item != ""]
         
-        answer = 0
         i = 0
         stack = []
+        answer = 0
         while i < len(s):
-            item = s[i]
-            if item == "+":
+            char = s[i]
+            if char == "+":
                 answer += stack.pop()
                 stack.append(int(s[i+1]))
                 i += 2
-            elif item == "-":
+            elif char == "-":
                 answer += stack.pop()
-                stack.append(int(s[i+1])*-1)
+                a = int(s[i+1]) * -1
+                stack.append(a)
                 i += 2
-            elif item == "*":
+            elif char == "*":
                 a = stack.pop()
                 b = int(s[i+1])
                 stack.append(a*b)
                 i += 2
-            elif item == "/":
+            elif char == "/":
                 a = stack.pop()
                 b = int(s[i+1])
                 stack.append(int(a/b))
                 i += 2
             else:
-                stack.append(int(item))
+                stack.append(int(char))
                 i += 1
-        
+                
         if stack:
             answer += stack.pop()
             
         return answer
-                
-                
-        
