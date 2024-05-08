fc9e46e4c9b1dae858afa92859d7ce66d2f252c0
Quai Wentt <quai.wentt@gmail.com>
2024-05-08 19:46:17 -0400

    Time: 88 ms (28.05%), Space: 23.3 MB (7.45%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,40 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+    
+        s = s.replace(" ", "")
+        s = re.split(r'(\d*)', s)
+        s = [item for item in s if item != ""]
+        stack = []
+        i = 0
+        while i < len(s):
+            item = s[i]
+            if item == ")":
+                current = 0
+                while stack and stack[-1] != "(":
+                    current += stack.pop()
+                if stack:
+                    stack.pop()
+                if stack and stack[-1] == "-":
+                    stack.pop()
+                    stack.append(current*-1)
+                else:
+                    stack.append(current)
+                i += 1
+            elif item.isdigit():
+                stack.append(int(item))
+                i += 1
+            elif item == "(":
+                stack.append(item)
+                i += 1
+            elif item == "-":
+                if i+1 < len(s) and s[i+1].isdigit():
+                    stack.append(int(s[i+1]) * -1)
+                    i += 2
+                else:
+                    stack.append(item)
+                    i += 1
+            else:
+                i += 1
+        return sum(stack)
+                
+                
