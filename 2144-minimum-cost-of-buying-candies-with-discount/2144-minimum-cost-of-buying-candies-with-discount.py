eff581dff5ae046ead5e84b3907578595d8d7b01
Quai Wentt <quai.wentt@gmail.com>
2024-05-25 17:28:01 -0400

    Time: 41 ms (88.13%), Space: 16.4 MB (94.29%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,28 +1,13 @@
-import heapq
+
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
-        maxHeap = [-candy for candy in cost]
-        heapify(maxHeap)
-        minCost = 0
-      
-        while len(maxHeap) >= 2:
-            candy1 = heappop(maxHeap) * -1
-            candy2 = heappop(maxHeap) * -1
-
-            minCost += candy1 + candy2
-            if maxHeap:
-                rejectedCandies = []
-                while maxHeap:
-                    freeCandy = heappop(maxHeap) * -1
-                    if freeCandy <= candy1 + candy2:
-                        break
-                    else:
-                        rejectedCandies.append(freeCandy * -1)
-                while rejectedCandies:
-                    heappush(maxHeap, rejectedCandies.pop())
-        if maxHeap:
-            minCost += heappop(maxHeap) * -1
+        cost.sort()
+        cost.reverse()
         
+        minCost = 0
+        for i, price in enumerate(cost):
+            if (i+1) % 3 != 0:
+                minCost += price
         return minCost
             
         
