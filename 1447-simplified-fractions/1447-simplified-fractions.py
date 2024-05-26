52fe79ae92966695f74c7f2862fd8f43fb579849
Quai Wentt <quai.wentt@gmail.com>
2024-05-26 15:26:49 -0400

    Time: 132 ms (60.67%), Space: 17.4 MB (29.33%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -2,11 +2,16 @@ class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
         answer = []
         
+        answer = []
+        fractions = set()
+        
         for numerator in range(1, n):
             denominator = n
             while denominator:
-                if (numerator / denominator) < 1 and math.gcd(numerator, denominator) == 1:
+                if (numerator / denominator) < 1 and (numerator/denominator) not in fractions:
                     answer.append(f'{numerator}/{denominator}')
+                    fractions.add(numerator/denominator)
+                    
                 denominator -= 1
         return answer
             
