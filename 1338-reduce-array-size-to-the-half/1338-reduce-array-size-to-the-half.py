b5f520ebd32fb57c5b9708c214e310e66c86a06c
Quai Wentt <quai.wentt@gmail.com>
2024-05-29 13:36:36 -0400

    Time: 432 ms (65.23%), Space: 34.1 MB (93.54%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -9,12 +9,9 @@ class Solution:
         answer = 0
         for freq in range(len(bucket)-1, 0, -1):
             num_of_nums = bucket[freq]
-            if num_of_nums:
-                max_to_subtract = math.ceil(target / freq)
-                num_to_subtract = min(max_to_subtract, num_of_nums)
-                target -= freq * num_to_subtract
-                answer += num_to_subtract
-                if target <= 0:
-                    break
+            while target > 0 and num_of_nums > 0:
+                target -= freq
+                num_of_nums -= 1
+                answer += 1
         return answer
         
