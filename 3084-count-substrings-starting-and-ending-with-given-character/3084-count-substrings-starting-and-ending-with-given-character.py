3343514f25772403da3199b50bfe28822654492c
Quai Wentt <quai.wentt@gmail.com>
2024-06-01 12:05:55 -0400

    Time: 47 ms (53.89%), Space: 17.5 MB (52.14%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.py
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.py
@@ -2,10 +2,6 @@ class Solution:
     def countSubstrings(self, s: str, c: str) -> int:
         counts = collections.Counter(s)
         n = counts[c]
-        answer = 0
-        while n:
-            answer += n
-            n -= 1
-        return answer
+        return math.comb(n+1, 2)
         
         
