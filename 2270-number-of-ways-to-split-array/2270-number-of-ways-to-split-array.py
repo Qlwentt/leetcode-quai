86a7ae46be368164d31e43c9d399dc46d6aacd9e
Quai Wentt <quai.wentt@gmail.com>
2024-06-01 18:34:46 -0400

    Time: 671 ms (59.40%), Space: 31.9 MB (42.76%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -1,27 +1,34 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-#         [10,14,6,7]
-#         [13,3,-1,7]
         
-#         10 > 3  += 1
-#         14 > -1  += 1
-#         6 > 7    += 0
+#         cur_sum = 0
+#         forward_sums = []
+#         for num in nums:
+#             cur_sum += num
+#             forward_sums.append(cur_sum)
         
-        cur_sum = 0
-        forward_sums = []
-        for num in nums:
-            cur_sum += num
-            forward_sums.append(cur_sum)
-        
-        back_sums = [0] * len(nums)
-        cur_sum = 0
-        for i in range(len(nums)-1,-1,-1):
-            num = nums[i]
-            cur_sum += num
-            back_sums[i] = cur_sum
+#         back_sums = [0] * len(nums)
+#         cur_sum = 0
+#         for i in range(len(nums)-1,-1,-1):
+#             num = nums[i]
+#             cur_sum += num
+#             back_sums[i] = cur_sum
             
+#         answer = 0
+#         for a,b in zip(forward_sums,back_sums[1:]):
+#             if a >= b:
+#                 answer += 1
+#         return answer
+        right = sum(nums)
+        left = 0
+        
+        n = len(nums)
         answer = 0
-        for a,b in zip(forward_sums,back_sums[1:]):
-            if a >= b:
+        for i, num in enumerate(nums):
+            left += num
+            right -= num
+            
+            if i != n - 1 and left >= right:
                 answer += 1
+                
         return answer
