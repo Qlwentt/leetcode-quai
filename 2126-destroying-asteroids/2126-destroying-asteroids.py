18b1a2fcd95f6a0178aad8c4296246f7832d89ea
Quai Wentt <quai.wentt@gmail.com>
2024-06-03 10:56:50 -0400

    Time: 807 ms (80.50%), Space: 28.3 MB (90.63%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,32 +1,10 @@
 from sortedcontainers import SortedList
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        asteroids = SortedList(asteroids)
-        cur_mass = mass
-        def get_best_asteroid(target):
-            
-            L = 0
-            R = len(asteroids) -1
-            
-            while L <= R:
-                mid = (L+R) // 2
-                
-                if asteroids[mid] > target:
-                    R = mid - 1
-                else:
-                    L = mid + 1
-            return R if R >= 0 else None
-        
-        while len(asteroids):
-            # pick = get_best_asteroid(cur_mass)
-            pick = asteroids.bisect_right(cur_mass) - 1
-            if pick is -1:
+        asteroids.sort(reverse=True)
+        while asteroids:
+            if mass >= asteroids[-1]:
+                mass += asteroids.pop()
+            else:
                 return False
-            
-            cur_mass += asteroids[pick]
-            asteroids.pop(pick)
-           
-        
         return True
-            
-            
