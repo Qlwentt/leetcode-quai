d8d0bb8c6a09743bccfe23e4c6137be616d0140f
Quai Wentt <quai.wentt@gmail.com>
2024-06-05 16:09:04 -0400

    Time: 84 ms (71.37%), Space: 17.2 MB (92.14%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,16 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        # "abacaba"
-        #    R
         charSet = set()
         answer = 0
         for char in s:
             if char in charSet:
                 charSet = set()
                 answer += 1
-            
-            
             charSet.add(char)
                 
-        answer += 1
-        return answer
+        return answer + 1
