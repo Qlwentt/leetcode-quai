eb986e2a3ea06fc55efddbdd1da999113d40f434
Quai Wentt <quai.wentt@gmail.com>
2024-06-06 11:58:27 -0400

    Time: 40 ms (49.69%), Space: 16.4 MB (98.55%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -3,11 +3,7 @@ class Solution:
         nums = set([int(num,2) for num in nums])
         n = len(nums)
         
-        print(nums)
         for i in range(2**n):
             if i not in nums:
                 binary = bin(i)[2:]
                 return "".join(["0"] * (n - len(binary))) + binary
-        
-        # 000, 001, 010, 011, 100, 101, 110, 111
-        #  0    1    2    3    4
