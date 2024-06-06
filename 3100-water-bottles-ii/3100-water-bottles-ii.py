01e04dfe185078ec337ade14db7fdd789ee7299e
Quai Wentt <quai.wentt@gmail.com>
2024-06-06 15:46:33 -0400

    Time: 37 ms (49.28%), Space: 16.4 MB (84.00%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.py
+++ 3100-water-bottles-ii/3100-water-bottles-ii.py
@@ -4,13 +4,11 @@ class Solution:
         empty = 0
         drank = 0
         while full != 0 or empty - numExchange >= 0:
-            drank += full # 10
-            empty += full# 10, 7, 3
-            full = 0 # 0, 1, 2
-            while empty - numExchange >= 0: # numEx: 3,4,5
-                # print(empty, full, numExchange)
+            drank += full
+            empty += full
+            full = 0 
+            while empty - numExchange >= 0:
                 empty -= numExchange
                 full += 1
                 numExchange += 1
-                # print(empty, full, numExchange)
         return drank
