20ca5996de28b2d7820f6c762bf230c03dd3a106
Quai Wentt <quai.wentt@gmail.com>
2024-06-07 20:01:04 -0400

    Time: 356 ms (88.19%), Space: 18.2 MB (53.13%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -2,18 +2,18 @@ class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        rows_set = collections.defaultdict(int)
-        cols_set = collections.defaultdict(int)
+        rows_dict = collections.defaultdict(int)
+        cols_dict = collections.defaultdict(int)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    rows_set[r] += 1
-                    cols_set[c] += 1
+                    rows_dict[r] += 1
+                    cols_dict[c] += 1
         answer = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1 and (rows_set[r] > 1 or cols_set[c] > 1):
+                if grid[r][c] == 1 and (rows_dict[r] > 1 or cols_dict[c] > 1):
                     answer += 1
         
         return answer
