50b8a32fb57d52c597d145132eac7e135d2a68df
Quai Wentt <quai.wentt@gmail.com>
2024-06-10 12:17:26 -0400

    Time: 138 ms (14.60%), Space: 17.5 MB (98.54%) - LeetHub

--- 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.py
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.py
@@ -1,23 +1,13 @@
 class Solution:
-    def maxValue(self, n: str, x: int) -> str:         
-        n = list(n)
-        
-        def get_max(n, is_negative=False):
-            for i, digit in enumerate(n):
-                if int(digit) < x and not is_negative:
-                    return i
-                elif int(digit) > x and is_negative:
-                    return i + 1
-            return len(n) + is_negative
+    def maxValue(self, n: str, x: int) -> str:                       
+        for i, digit in enumerate(n):
+            if digit == "-":
+                continue
+            if (int(digit) < x and n[0] != "-") or (int(digit) > x and n[0]== "-") :
+                return n[:i] + str(x) + n[i:]
+        return n + str(x)
             
                     
         
-        if n[0] == "-":
-            i = get_max(n[1:], True)
-            
-        else:
-            i = get_max(n)
-        n.insert(i, str(x)) 
-        return "".join(n)
         
         
