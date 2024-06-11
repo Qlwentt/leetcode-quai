a0b9b8b22cd4d1713e5b48333d9f1fdcb7196c4c
Quai Wentt <quai.wentt@gmail.com>
2024-06-11 13:47:51 -0400

    Time: 175 ms (41.86%), Space: 16.5 MB (88.37%) - LeetHub

--- 1860-incremental-memory-leak/1860-incremental-memory-leak.py
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.py
@@ -5,14 +5,14 @@ class Solution:
         while True:
             if memory1 >= memory2:
                 if memory1 - i < 0:
-                    break
+                    return [i, memory1, memory2]
                 memory1 -= i
             else:
                 if memory2 - i < 0:
-                    break
+                    return [i, memory1, memory2]
                 memory2 -= i
             
             i += 1
-        return [i, memory1, memory2]
+        return 
             
         
