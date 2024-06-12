51201266743443e597339019e211347ae5b53e01
Quai Wentt <quai.wentt@gmail.com>
2024-06-12 10:53:09 -0400

    Time: 47 ms (35.16%), Space: 16.7 MB (72.66%) - LeetHub

---
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        words = set(words)
+        answer = []
+        for i in range(len(text)):
+            for j in range(i, len(text)):
+                if text[i:j+1] in words: 
+                    answer.append([i,j])
+                
+        return sorted(answer)
+        
