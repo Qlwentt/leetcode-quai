c98a0a0df9e7ad27068e0b4d0b6b756805dee458
Quai Wentt <quai.wentt@gmail.com>
2024-06-25 19:33:23 -0400

    Time: 61 ms (20.93%), Space: 16.6 MB (64.48%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @cache
-        def climb(i):
-            if i >= len(cost):
-                return 0
+#         @cache
+#         def climb(i):
+#             if i >= len(cost):
+#                 return 0
             
-            one = cost[i] + climb(i+1)
-            two = cost[i] + climb(i+2)
-            return min(one, two)
-        return min(climb(0), climb(1))
+#             one = cost[i] + climb(i+1)
+#             two = cost[i] + climb(i+2)
+#             return min(one, two)
+#         return min(climb(0), climb(1))
+
+        dp = collections.defaultdict(int)
+        
+        i = len(cost) - 1
+        
+        while i >= 0:
+            dp[i] = min(cost[i]+dp[i+1], cost[i]+ dp[i+2])
+            i -= 1
+        return min(dp[0], dp[1])
