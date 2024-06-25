c995a7e7952b54304cb0f3d3d89039c824d41e7c
Quai Wentt <quai.wentt@gmail.com>
2024-06-25 17:27:31 -0400

    Time: 1351 ms (7.55%), Space: 305.6 MB (14.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,35 +2,22 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         s1 = text1
         s2 = text2
-    # memoization
-#         memo = {}
-#         def dfs(i1, i2):
-#             if i1 >= len(s1) or i2 >= len(s2):
-#                 return 0
+        memo = {}
+        def dfs(i1,i2):
+            if i1 == len(s1) or i2 == len(s2):
+                return 0
             
-#             if (i1,i2) in memo:
-#                 return memo[(i1,i2)]
+            if (i1, i2) in memo:
+                return memo[(i1,i2)]
             
-#             if s1[i1] == s2[i2]:
-#                 memo[(i1,i2)] = 1 + dfs(i1+1, i2+1)
-#                 return memo[(i1,i2)]
-            
-#             memo[(i1,i2)] = max(dfs(i1+1, i2), dfs(i1, i2+1))
-#             return memo[(i1,i2)]
+            if s1[i1] == s2[i2]:
+                ans = 1 + dfs(i1+1, i2+1)
+                memo[(i1,i2)] = ans
+                return ans
+            inc1 = dfs(i1+1, i2)
+            inc2 = dfs(i1, i2+1)
+            ans = max(inc1, inc2)
+            memo[(i1,i2)] = ans
+            return ans
+        return dfs(0,0)
         
-#         return dfs(0,0)
-    
-    # dp
-        n = len(s1)
-        m = len(s2)
-        dp = [[0]*(m+1) for _ in range(n+1)]
-        
-        for i1 in range(n-1, -1, -1):
-            for i2 in range(m-1, -1, -1):
-                if s1[i1] == s2[i2]:
-                    dp[i1][i2] = 1 + dp[i1+1][i2+1]
-                else:
-                    dp[i1][i2] = max(dp[i1+1][i2], dp[i1][i2+1])
-        
-        return dp[0][0]
-    
