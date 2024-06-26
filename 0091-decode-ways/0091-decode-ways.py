710a0efdde524fa6e44f58a4eea92593b3aa5fd4
Quai Wentt <quai.wentt@gmail.com>
2024-06-26 13:07:10 -0400

    Time: 31 ms (86.67%), Space: 16.7 MB (13.80%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,17 +1,37 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+#         @cache
+#         def backtrack(i):
+#             if i >= len(s):
+#                 return 1
+#             count  = 0
+#             for j in range(i,len(s)):
+#                 substring = s[i:j+1]
+#                 if substring[0] != "0" and int(substring) in range(1,27):
+#                     count += backtrack(j+1)
+            
+#             return count
+            
+#         return backtrack(0)
         @cache
         def backtrack(i):
-            if i >= len(s):
+            if i == len(s):
                 return 1
-            count  = 0
-            for j in range(i,len(s)):
-                substring = s[i:j+1]
-                if substring[0] != "0" and int(substring) in range(1,27):
-                    count += backtrack(j+1)
+            
+            # take one
+            next_num = s[i]
+            count = 0
+            if int(next_num) in range(1,27) and next_num[0] != "0":
+                count += backtrack(i+1)
+                
+            if i+1 in range(len(s)):
+                next_num = s[i] + s[i+1]
+                if int(next_num) in range(1,27) and next_num[0] != "0":
+                    count += backtrack(i+2)
             
             return count
-            
-        return backtrack(0)  
+        return backtrack(0)
+                
+        
              
         
