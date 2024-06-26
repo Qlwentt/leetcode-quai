5f23894d02a1d282d10a4499669a2d7adb973435
Quai Wentt <quai.wentt@gmail.com>
2024-06-25 20:36:38 -0400

    Time: 35 ms (63.51%), Space: 16.4 MB (95.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def helper(i):
-            if i >= len(nums):
-                return 0
+#         @cache
+#         def helper(i):
+#             if i >= len(nums):
+#                 return 0
             
-            take = nums[i] + helper(i+2)
-            skip = helper(i+1)
-            return max(take, skip)
-        return helper(0)
+#             take = nums[i] + helper(i+2)
+#             skip = helper(i+1)
+#             return max(take, skip)
+#         return helper(0)
+        dp = collections.defaultdict(int)
+    
+        for i in range(len(nums)-1,-1,-1):
+            take = nums[i] + dp[i+2]
+            skip = dp[i+1]
+            dp[i] = max(take, skip)
+            
+        return dp[0]
+        
+    
+    
         
