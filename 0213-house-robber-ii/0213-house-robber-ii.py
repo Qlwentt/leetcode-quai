5fc5a0c5a321fbac579fe3da93934d435127c1d3
Quai Wentt <quai.wentt@gmail.com>
2024-06-25 21:37:12 -0400

    Time: 38 ms (44.76%), Space: 16.4 MB (60.87%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,17 +1,30 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        def helper(i, nums, memo):
-            if i >= len(nums):
-                return 0
-            if i in memo:
-                return memo[i]
-            take = nums[i] + helper(i+2, nums, memo)
-            skip = helper(i+1, nums, memo)
-            ans = max(take,skip)
-            memo[i] = ans
-            return ans
-        if len(nums) >= 2:
-            return max(helper(0, nums[:-1], {}), helper(1, nums, {}))
-        return nums[0] 
+        # def helper(i, nums, memo):
+        #     if i >= len(nums):
+        #         return 0
+        #     if i in memo:
+        #         return memo[i]
+        #     take = nums[i] + helper(i+2, nums, memo)
+        #     skip = helper(i+1, nums, memo)
+        #     ans = max(take,skip)
+        #     memo[i] = ans
+        #     return ans
+        # if len(nums) >= 2:
+        #     return max(helper(0, nums[:-1], {}), helper(1, nums, {}))
+        # return nums[0] 
+        
+        if len(nums) == 1:
+            return nums[0]
+        def robber(nums):
+            dp = collections.defaultdict(int)
+            
+            for i in range(len(nums)-1,-1,-1):
+                take = nums[i] + dp[i+2]
+                skip = dp[i+1]
+                dp[i] = max(take, skip)
+            
+            return dp[0]
+        return max(robber(nums[:-1]), robber(nums[1:]))
         
