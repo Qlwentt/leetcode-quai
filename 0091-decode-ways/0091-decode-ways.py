7db06f6e821bcc9d669ef0a4c0fdcbafa28fc460
Quai Wentt <quai.wentt@gmail.com>
2024-06-26 13:12:24 -0400

    Time: 48 ms (5.02%), Space: 16.5 MB (80.72%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -13,24 +13,39 @@ class Solution:
 #             return count
             
 #         return backtrack(0)
-        @cache
-        def backtrack(i):
-            if i == len(s):
-                return 1
+#         @cache
+#         def backtrack(i):
+#             if i == len(s):
+#                 return 1
             
-            # take one
-            next_num = s[i]
-            count = 0
-            if int(next_num) in range(1,27) and next_num[0] != "0":
-                count += backtrack(i+1)
+#             # take one
+#             next_num = s[i]
+#             count = 0
+#             if int(next_num) in range(1,27) and next_num[0] != "0":
+#                 count += backtrack(i+1)
                 
-            if i+1 in range(len(s)):
+#             if i+1 in range(len(s)):
+#                 next_num = s[i] + s[i+1]
+#                 if int(next_num) in range(1,27) and next_num[0] != "0":
+#                     count += backtrack(i+2)
+            
+#             return count
+#         return backtrack(0)
+        n = len(s)
+        dp = collections.defaultdict(int)
+        dp[n] = 1
+        
+        for i in range(n-1,-1,-1):
+            next_num = s[i]
+            if int(next_num) in range(1,27) and next_num[0] != "0":
+                dp[i] += dp[i+1]
+            if i + 1 in range(n):
                 next_num = s[i] + s[i+1]
                 if int(next_num) in range(1,27) and next_num[0] != "0":
-                    count += backtrack(i+2)
-            
-            return count
-        return backtrack(0)
+                    dp[i] += dp[i+2]
+        
+        return dp[0]
+                
                 
         
              
