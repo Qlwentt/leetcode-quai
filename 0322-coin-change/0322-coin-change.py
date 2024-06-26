d26dbfb7405f6a477efb16cb2c630d6aafae4c3d
Quai Wentt <quai.wentt@gmail.com>
2024-06-26 13:35:16 -0400

    Time: 1779 ms (5.00%), Space: 621.7 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,44 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # memoization
-#         memo = [[-1]*(amount) for _ in range(len(coins))]
+        @cache
+        def dp(i, cur_amount):
+            if cur_amount == 0:
+                return 0
+            if cur_amount < 0:
+                return float('inf')
+            if i == len(coins):
+                return float('inf')
+            
+            # take
+            take = 1 + dp(i, cur_amount - coins[i])
+            
+            # stop taking
+            skip = dp(i+1, cur_amount)
+            
+            return min(take, skip)
         
-#         def backtrack(i, curAmount):
-#             if i >= len(coins) or curAmount > amount :
-#                 return float('inf')
-            
-#             if curAmount == amount:
-#                 return 0
-            
-#             if memo[i][curAmount] != -1:
-#                 return memo[i][curAmount]
-            
-#             # choose coin between 1 and unlimited times
-#             coins1 = 1 + backtrack(i, curAmount+coins[i])
-#             # skip coin
-#             coins2 = backtrack(i+1, curAmount)
-            
-#             memo[i][curAmount] =  min(coins1, coins2)
-#             return memo[i][curAmount]
-#         answer = backtrack(0,0)
-#         print(memo)
-#         return  answer if answer != float('inf') else -1 
-        
-#        # dp
-        N = len(coins) # rows
-        M = amount # cols
-        
-        dp = [[float('inf')]*(M+1) for _ in range(N+1)]
-        
-        for i in range(N+1):
-            dp[i][M] = 0
-            
-        for i in range(N-1, -1,-1):
-            for curAmount in range(M-1,-1,-1):
-                if curAmount + coins[i] > amount:
-                    take = float('inf')
-                else:
-                    take = 1 + dp[i][curAmount+coins[i]]
-                skip = dp[i+1][curAmount]
-                dp[i][curAmount] = min(take, skip)
-        return dp[0][0] if dp[0][0] != float('inf') else -1
+        answer = dp(0, amount)
+        return answer if answer != float('inf') else -1
+    
