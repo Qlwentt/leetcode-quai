eefc54972b5446bd68e0ca48767bbbcaa576a714
Quai Wentt <quai.wentt@gmail.com>
2024-06-26 12:24:31 -0400

    Time: 79 ms (5.02%), Space: 16.7 MB (13.80%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,41 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        numMap = {str(num+1): letter for num, letter in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
-#         memo = {}
-#         def backtrack(i):
-#             if i > len(s):
-#                 return 0
-#             if i == len(s):
-#                 return 1
-#             if i in memo:
-#                 return memo[i]
+        @cache
+        def backtrack(i):
+            if i >= len(s):
+                return 1
+            count  = 0
+            for j in range(i,len(s)):
+                substring = s[i:j+1]
+                if substring[0] != "0" and int(substring) in range(1,27):
+                    count += backtrack(j+1)
             
-#             res = 0
-#             # take only one num
-#             nextNum = s[i]
-#             if nextNum in numMap:
-#                 res += backtrack(i+1)
+            return count
             
-#             # take two nums
-#             nextNum = s[i:i+2]
-#             if nextNum in numMap:
-#                 res += backtrack(i+2)
-
-#             memo[i] = res    
-#             return res
-                
-#         return backtrack(0)
-        def decodingDP():
-            n = len(s)
-            dp = [0]*(n+1)
-            dp[n-1], dp[n] = 1 if s[-1] in numMap else 0 ,1
-            
-            i = n-2
-            while i >= 0:
-                dp[i] = (dp[i+1] if s[i] in numMap else 0) + (dp[i+2] if s[i:i+2] in numMap else 0)
-                i -= 1
-            return dp[0]
-        
-        return decodingDP()
-            
+        return backtrack(0)  
+             
         
