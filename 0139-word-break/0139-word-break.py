ef896ce2f24c4ab1c78b20ac5674c986af3ec218
Quai Wentt <quai.wentt@gmail.com>
2024-06-26 18:18:59 -0400

    Time: 38 ms (70.60%), Space: 16.6 MB (47.94%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         word_dict = set(wordDict)
-        @cache
-        def dp(i):
-            if i == len(s):
-                return True
+#         @cache
+#         def dp(i):
+#             if i == len(s):
+#                 return True
             
-            for j in range(i, len(s)):
+#             for j in range(i, len(s)):
+#                 cur_part = s[i:j+1]
+#                 if cur_part in word_dict:
+#                     if dp(j+1):
+#                         return True
+            
+#             return False
+#         return dp(0)
+        dp = collections.defaultdict(lambda:False)
+        n = len(s)
+        dp[n] = True
+        
+        for i in range(n-1,-1,-1):
+            for j in range(n-1,i-1, -1):
                 cur_part = s[i:j+1]
                 if cur_part in word_dict:
-                    if dp(j+1):
-                        return True
-            
-            return False
-        return dp(0)
-        # dp = collections.defaultdict(lambda:False)
-        # n = len(s)
-        # dp[n-1] = True
-        
+                    if dp[j+1]:
+                        dp[i] = True
+        return dp[0]
             
         
