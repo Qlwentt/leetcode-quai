ec9c4845d22faae95ea9b49201ea8e37c9630d05
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 13:48:49 -0400

    Time: 74 ms (8.21%), Space: 20.4 MB (5.63%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @cache
+        def dp(i,j):
+            if i == len(triangle):
+                return 0
+            
+            left = triangle[i][j] + dp(i+1,j)
+            right = float('inf')
+            if j + 1 in range(len(triangle[i])):
+                right = triangle[i][j+1] + dp(i+1,j+1)
+            
+            return min(left,right)
+        return dp(0,0)
+            
+            
