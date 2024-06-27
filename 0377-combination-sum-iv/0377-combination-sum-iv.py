a1542504556425c5b6bbec1c63a72046b881e0ee
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 19:43:40 -0400

    Time: 268 ms (5.10%), Space: 26.5 MB (5.03%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        @cache
+        def dp(i, cur_sum):
+            if cur_sum > target:
+                return 0
+            if cur_sum == target:
+                return 1
+            if i >= len(nums):
+                return 0
+            answer = 0
+            for j in range(len(nums)):
+                answer += dp(j, nums[j]+ cur_sum)
+            return answer
+        
+        return dp(0,0)
+        
