8ffa90e37d2faf74beaaa60e97ef805cbb450473
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 15:11:34 -0400

    Time: 73 ms (70.34%), Space: 23.2 MB (12.52%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,29 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)
+        nums = list(counts.keys())
+        nums.sort()
+        n = len(nums)
+        next_i = [n] * n
+        
+        p = 0
+        for q in range(n):
+            while nums[q] > nums[p] + 1:
+                next_i[p] = q
+                p += 1
+        @cache      
+        def dp(i):
+            if i == len(nums):
+                return 0
+            
+            take = counts[nums[i]] * nums[i] + dp(next_i[i])
+            skip = dp(i+1)
+            
+            return max(take, skip)
+        
+        return dp(0)
+        
+        
+        
+        
+        
