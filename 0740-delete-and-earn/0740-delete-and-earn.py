1150e6c7398c540e282e930bf6600c3969b992d3
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 15:25:31 -0400

    Time: 80 ms (36.94%), Space: 19.2 MB (29.70%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -11,18 +11,25 @@ class Solution:
             while nums[q] > nums[p] + 1:
                 next_i[p] = q
                 p += 1
-        @cache      
-        def dp(i):
-            if i == len(nums):
-                return 0
+#         @cache      
+#         def dp(i):
+#             if i == len(nums):
+#                 return 0
             
-            take = counts[nums[i]] * nums[i] + dp(next_i[i])
-            skip = dp(i+1)
+#             take = counts[nums[i]] * nums[i] + dp(next_i[i])
+#             skip = dp(i+1)
             
-            return max(take, skip)
+#             return max(take, skip)
         
-        return dp(0)
-        
+#         return dp(0)
+
+        dp = collections.defaultdict(int)
+        n = len(nums)
+        for i in range(n-1,-1,-1):
+            take = counts[nums[i]] * nums[i] + dp[next_i[i]]
+            skip = dp[i+1]
+            dp[i] = max(take, skip)
+        return dp[0]
         
         
         
