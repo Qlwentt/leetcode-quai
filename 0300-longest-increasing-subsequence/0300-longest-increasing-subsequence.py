9e697c47654334793ec9715eb81ee8194d23ae4b
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 11:16:06 -0400

    Time: 2224 ms (13.45%), Space: 21.1 MB (5.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,63 +1,35 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # not optimal memoization - TLE
-#         memo = {i: {} for i in range(len(nums))}
-#         def backtrack(i, prev):
-#             if i >= len(nums):
-#                 return 0
-            
-#             if prev in memo[i]:
-#                 return memo[i][prev]
-
-#             take = 0
-#             num = nums[i]
-#             if num > prev:
-#                 # take number
-#                 take = 1 + backtrack(i+1, num)
-#             # skip number
-#             skip = backtrack(i+1, prev)
-
-#             memo[i][prev] = max(take,skip)
-#             return memo[i][prev]
-        
-#         return backtrack(0, float('-inf'))
 
 # memoization
-#         memo = {}
-#         def backtrack(i):
-#             if i >= len(nums):
-#                 return 0
-            
-#             if i in memo:
-#                 return memo[i]
-            
-#             maxTake = 1
-#             for j in range(i+1, len(nums)):
-#                 take = 1
-#                 if nums[i] < nums[j]:
-#                     take += backtrack(j)
-#                     maxTake = max(take,maxTake)
-                                
-#             memo[i] = maxTake
-#             return memo[i]
+        @cache
+        def backtrack(i):
+            if i >= len(nums):
+                return 0
+            max_take = 1
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    take = 1 + backtrack(j)
+                    max_take = max(max_take, take)
+            return max_take
+        answer = 1
+        for i in range(len(nums)):
+            answer = max(backtrack(i), answer)
+        return answer
         
-#         answer = 1
-#         for i in range(len(nums)):
-#             answer = max(answer, backtrack(i))
-#         return answer
 
 # dp
-        n = len(nums)
-        dp = [1] * (n)
+#         n = len(nums)
+#         dp = [1] * (n)
         
-        for i in range(n-1, -1,-1):
-            for j in range(n-1, i-1, -1):
-                a = nums[i]
-                b = nums[j]
-                if a < b:
-                    dp[i] = max(dp[i], 1 + dp[j])
+#         for i in range(n-1, -1,-1):
+#             for j in range(n-1, i-1, -1):
+#                 a = nums[i]
+#                 b = nums[j]
+#                 if a < b:
+#                     dp[i] = max(dp[i], 1 + dp[j])
         
-        return max(dp)                
+#         return max(dp)                
         
         
 
