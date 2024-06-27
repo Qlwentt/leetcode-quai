880beb4eb81f91acd745c75c087a5b10c3b78cb1
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 13:02:02 -0400

    Time: 5905 ms (5.01%), Space: 326.9 MB (8.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,54 +1,35 @@
-
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # memoization
-#         memo = {}
-#         numsSum = sum(nums)
-#         def backtrack(i, curTotal):
-#             otherHalf = numsSum - curTotal
-#             if curTotal > otherHalf:
-#                 return False
-#             if curTotal == otherHalf:
-#                 return True
-#             if i >= len(nums):
-#                 return False
-#             if (i, curTotal) in memo:
-#                 return memo[(i, curTotal)]
-            
-#             # take num
-#             take = backtrack(i+1, nums[i]+curTotal)
-#             # skip num
-#             skip = backtrack(i+1, curTotal)
-            
-#             memo[(i, curTotal)] = take or skip
-#             return memo[(i, curTotal)]
-        
-#         return backtrack(0,0)
-
-# dp
-        if sum(nums) % 2:
+        sum_ = sum(nums)
+        if sum_ % 2:
             return False
+        half = sum_ // 2
+        
+#         @cache
+#         def dp(i, cur_total):
+#             if cur_total < 0:
+#                 return False
+#             if i == len(nums) and cur_total == 0:
+#                 return True
+#             if i == len(nums):
+#                 return False
+            
+#             take = dp(i+1, cur_total-nums[i])
+#             skip = dp(i+1, cur_total)
+            
+#             return take or skip
+#         return dp(0,half)
         n = len(nums)
-        half = sum(nums) // 2
-        dp = [[False] * (half+1) for _ in range(n+1)]
+        dp = collections.defaultdict(lambda:False)
+        dp[(n, 0)] = True
         
-        # when curTotal == half, set to True
-        for i in range(n+1):
-            dp[i][half] = True
+        for i in range(n-1,-1,-1):
+            for cur_total in range(1, half+1):
+                take = dp[(i+1, cur_total-nums[i])]
+                skip = dp[(i+1, cur_total)]
+                dp[(i,cur_total)] = take or skip
         
-        for i in range(n-1, -1, -1):
-            for curTotal in range(half, -1, -1):
-                if nums[i] + curTotal > half:
-                    take = False
-                else:
-                    take = dp[i+1][nums[i]+curTotal]
-                skip = dp[i+1][curTotal]
-                dp[i][curTotal] = skip or take
-                
-        return dp[0][0]
+        return dp[(0,half)]
+    
         
         
-            
-            
-            
-                
