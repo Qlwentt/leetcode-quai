f18ab5999bfe1402fe111fa7e190b909dda45712
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 18:06:31 -0400

    Time: 58 ms (60.34%), Space: 16.7 MB (38.36%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,18 +1,34 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         
-        @cache
-        def backtrack(i, prev):
-            if i == len(costs):
-                return 0
-            min_take = float('inf')
-            for j in range(len(costs[i])):
-                if j != prev:
-                    take = costs[i][j] + backtrack(i+1, j)
-                    min_take = min(take, min_take)
-            return min_take
+#         @cache
+#         def dp(i, prev):
+#             if i == len(costs):
+#                 return 0
+#             min_take = float('inf')
+#             for j in range(len(costs[i])):
+#                 if j != prev:
+#                     take = costs[i][j] + dp(i+1, j)
+#                     min_take = min(take, min_take)
+#             return min_take
         
-        return backtrack(0, float('inf'))
+#         return dp(0, float('inf'))
+
+        dp = collections.defaultdict(lambda: collections.defaultdict(int))
+        for i in range(len(costs)-1,-1,-1):
+            for prev in range(len(costs[i])):
+                min_take = float('inf')
+                for j in range(len(costs[i])):
+                    if j != prev:
+                        take = costs[i][j] + dp[i+1][j]
+                        min_take = min(take, min_take) 
+                dp[i][prev] = min_take
+                
+        return min(dp[0].values())
+    
+        
+    
+        
                     
             
             
