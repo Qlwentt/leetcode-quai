352cc15410435c02e511437c71860af255c2b087
Quai Wentt <quai.wentt@gmail.com>
2024-06-27 11:56:36 -0400

    Time: 1914 ms (20.10%), Space: 17 MB (15.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,35 +2,35 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
 
 # memoization
-        @cache
-        def backtrack(i):
-            if i >= len(nums):
-                return 0
-            max_take = 1
-            for j in range(i+1, len(nums)):
-                if nums[i] < nums[j]:
-                    take = 1 + backtrack(j)
-                    max_take = max(max_take, take)
-            return max_take
-        answer = 1
-        for i in range(len(nums)):
-            answer = max(backtrack(i), answer)
-        return answer
+        # @cache
+        # def backtrack(i):
+        #     if i >= len(nums):
+        #         return 0
+        #     max_take = 1
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] < nums[j]:
+        #             take = 1 + backtrack(j)
+        #             max_take = max(max_take, take)
+        #     return max_take
+        # answer = 1
+        # for i in range(len(nums)):
+        #     answer = max(backtrack(i), answer)
+        # return answer
         
 
 # dp
-#         n = len(nums)
-#         dp = [1] * (n)
+        n = len(nums)
+        dp = collections.defaultdict(int)
         
-#         for i in range(n-1, -1,-1):
-#             for j in range(n-1, i-1, -1):
-#                 a = nums[i]
-#                 b = nums[j]
-#                 if a < b:
-#                     dp[i] = max(dp[i], 1 + dp[j])
+        for i in range(n-1,-1,-1):
+            max_take = 1
+            for j in range(i+1,n):
+                if nums[i] < nums[j]:
+                    take = 1 + dp[j]
+                    max_take = max(max_take, take)
+            dp[i] = max_take
         
-#         return max(dp)                
-        
+        return max(dp.values())
         
 
         
