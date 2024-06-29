983247a003133da0822293c765d2628eded30e4d
Quai Wentt <quai.wentt@gmail.com>
2024-06-29 19:15:49 -0400

    Time: 36 ms (65.32%), Space: 16.7 MB (12.11%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,15 +1,15 @@
 class Solution:
-    def integerBreak(self, n: int) -> int:
+    def integerBreak(self, N: int) -> int:
         @cache
         def dp(n):
             if n == 1:
                 return 1
             
-            max_product = float('-inf')
+            max_product = float('-inf') if n == N else n
             for i in range(1,n):
                 product = i * dp(n-i)
-                max_product = max(product, i * (n-i) , max_product)
+                max_product = max(product, max_product)
                 
             return max_product
         
-        return dp(n)
+        return dp(N)
