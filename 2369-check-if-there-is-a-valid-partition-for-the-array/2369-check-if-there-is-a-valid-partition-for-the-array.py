90db6b7da10918a4b2ca5d59dbb79d2ea3d21ea5
Quai Wentt <quai.wentt@gmail.com>
2024-06-29 09:55:31 -0400

    Time: 755 ms (43.67%), Space: 38.8 MB (45.55%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,19 +1,36 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        @cache
-        def dp(i):
-            if i == len(nums):
-                return True
+        # @cache
+#         def dp(i):
+#             if i == len(nums):
+#                 return True
             
-            if i + 1 < len(nums) and nums[i] == nums[i+1]:
-                if dp(i+2):
-                    return True
-                if i+ 2 < len(nums) and nums[i+1] == nums[i+2]:
-                    if dp(i+3):
-                        return True
-            if i+1 < len(nums) and i+2 < len(nums) and nums[i] + 1 == nums[i+1] and nums[i+1] +1 == nums[i+2]:
-                if dp(i+3):
-                    return True
-            return False
+#             if i + 1 < len(nums) and nums[i] == nums[i+1]:
+#                 if dp(i+2):
+#                     return True
+#                 if i+ 2 < len(nums) and nums[i+1] == nums[i+2]:
+#                     if dp(i+3):
+#                         return True
+#             if i+1 < len(nums) and i+2 < len(nums) and nums[i] + 1 == nums[i+1] and nums[i+1] +1 == nums[i+2]:
+#                 if dp(i+3):
+#                     return True
+#             return False
         
-        return dp(0)
+#         return dp(0)
+        n = len(nums)
+        dp = collections.defaultdict(lambda:False)
+        dp[n] = True
+        
+        for i in range(n-1,-1,-1):
+            answer = False
+            if i + 1 < n and nums[i] == nums[i+1]:
+                answer |= dp[i+2]
+                if i + 2 < n and nums[i+1] == nums[i+2]:
+                    answer |= dp[i+3]
+            if i + 1 < n and i+2 < n and nums[i] + 1 == nums[i+1] and nums[i+1] + 1 == nums[i+2]:
+                answer |= dp[i+3]
+            dp[i] = answer
+        return dp[0]
+                
+    
+    
