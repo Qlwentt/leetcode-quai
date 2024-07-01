bf145d137f90c09a8c48bbe46beabeba15b4ceb6
Quai Wentt <quai.wentt@gmail.com>
2024-07-01 19:32:12 -0400

    Time: 71 ms (100.00%), Space: 16.8 MB (66.67%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,13 +1,22 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
         target = len(set(nums))
+        L = 0
+        window = collections.defaultdict(int)
         answer = 0
-        for i in range(len(nums)):
-            sub_array = set()
-            for j in range(i,len(nums)):
-                sub_array.add(nums[j])
-                answer += len(sub_array) == target
+        for R in range(len(nums)):
+            window[nums[R]] += 1
+            while len(window) == target:
+                answer += (len(nums) - R)
+                window[nums[L]] -= 1
+                if window[nums[L]] == 0:
+                    del window[nums[L]]
+                L += 1
+                
+            
+            
         return answer
+                
         
          
         
