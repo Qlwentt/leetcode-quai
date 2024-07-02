a61ddc6ae295adcf963a040115bab691b00c8198
Quai Wentt <quai.wentt@gmail.com>
2024-07-02 15:16:41 -0400

    Time: 397 ms (49.90%), Space: 62.4 MB (5.11%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -6,22 +6,19 @@
 #         self.right = right
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        def dfs(root,nodes, length):
+        def dfs(root,nodes):
             if root.val in nodes:
                 nodes.remove(root.val)
             else:
                 nodes.add(root.val)
             if not root.left and not root.right:
-                if length % 2 == 0:
-                    return 1 if len(nodes) == 0 else 0
-                else:
-                    return 1 if len(nodes) == 1 else 0
+                return 1 if len(nodes) <= 1 else 0
             left = 0
             right = 0
             if root.left:
-                left = dfs(root.left, nodes.copy(), length+1)
+                left = dfs(root.left, nodes.copy())
             if root.right:
-                right = dfs(root.right, nodes.copy(), length+1)
+                right = dfs(root.right, nodes.copy())
             
             return left + right
-        return dfs(root, set(), 1)
+        return dfs(root, set())
