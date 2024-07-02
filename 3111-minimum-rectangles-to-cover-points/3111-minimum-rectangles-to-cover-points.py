dafed131deed07580d088b226c805bb7e3fbb0fd
Quai Wentt <quai.wentt@gmail.com>
2024-07-02 11:39:15 -0400

    Time: 997 ms (77.68%), Space: 62.2 MB (88.62%) - LeetHub

--- 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.py
+++ 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.py
@@ -1,15 +1,27 @@
 class Solution:
     def minRectanglesToCoverPoints(self, points: List[List[int]], w: int) -> int:
-        min_heap = points
-        heapq.heapify(min_heap)
+#         min_heap = points
+#         heapq.heapify(min_heap)
         
-        rectangles = []
-        while min_heap:
-            min_x, min_y = heapq.heappop(min_heap)
-            lower_x, upper_x, = rectangles[-1] if rectangles else [None, None]
-            if lower_x is None or min_x not in range(lower_x, upper_x+1):
-                rectangles.append([min_x, min_x+w])
+#         rectangles = []
+#         while min_heap:
+#             min_x, min_y = heapq.heappop(min_heap)
+#             lower_x, upper_x, = rectangles[-1] if rectangles else [None, None]
+#             if lower_x is None or min_x not in range(lower_x, upper_x+1):
+#                 rectangles.append([min_x, min_x+w])
         
-        return len(rectangles)
+#         return len(rectangles)
+
+            x_es = [x for x,y in points]
+            x_es.sort()
+            
+            upper_x = -1 
+            count = 0
+            
+            for x in x_es:
+                if x > upper_x:
+                    upper_x = x + w
+                    count += 1
+            return count
         
         
