fac2f4202d346b6039e8fd8713e101f8de6d56ed
Quai Wentt <quai.wentt@gmail.com>
2024-07-05 19:10:52 -0400

    Time: 260 ms (46.77%), Space: 18.1 MB (41.61%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -2,19 +2,16 @@ class Solution:
     def distinctPrimeFactors(self, nums: List[int]) -> int:
         target = math.prod(nums)
         def get_primes(n):
-            def is_prime(n):
-                if n <= 1:
-                    return False
-                
-                for i in range(2,int(math.sqrt(n))+1):
-                    if n % i == 0:
-                        return False
-                return True
-            primes = []
-            for i in range(n+1):
-                if is_prime(i):
-                    primes.append(i)
-            return primes
+            primes = [True] * (n+1)
+            primes[0] = False
+            primes[1] = False
+            
+            for i in range(2,len(primes)):
+                if primes[i]:
+                    for j in range(i+i, len(primes), i):
+                        primes[j] = False
+            
+            return [i for i, is_prime in enumerate(primes) if is_prime == True]
     
         primes = get_primes(1000)
         factors = []
