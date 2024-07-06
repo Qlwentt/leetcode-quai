37213ecf59885e085e5a10ebe5583dd35758d38f
Quai Wentt <quai.wentt@gmail.com>
2024-07-06 17:07:32 -0400

    Time: 39 ms (84.21%), Space: 16.5 MB (40.23%) - LeetHub

--- 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.py
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.py
@@ -4,10 +4,12 @@ class Solution:
         curEnergy = initialEnergy
         curExperience = initialExperience
         for opponentEnergy, opponentExperience in zip(energy, experience):
-            add += (opponentEnergy + 1) - curEnergy if (opponentEnergy + 1) - curEnergy > 0 else 0
-            add += (opponentExperience + 1) - curExperience if (opponentExperience + 1) - curExperience > 0 else 0
-            curEnergy += (opponentEnergy + 1) - curEnergy if (opponentEnergy + 1) - curEnergy > 0 else 0
-            curExperience += (opponentExperience + 1) - curExperience if (opponentExperience + 1) - curExperience > 0 else 0
+            energyDelta = (opponentEnergy + 1) - curEnergy if (opponentEnergy + 1) - curEnergy > 0 else 0
+            experienceDelta = (opponentExperience + 1) - curExperience if (opponentExperience + 1) - curExperience > 0 else 0
+            add += energyDelta
+            add += experienceDelta
+            curEnergy += energyDelta
+            curExperience += experienceDelta
             curEnergy -= opponentEnergy
             curExperience += opponentExperience
         return add
