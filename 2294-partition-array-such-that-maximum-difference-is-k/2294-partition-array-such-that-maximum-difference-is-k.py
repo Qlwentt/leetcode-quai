6822059af2e8b5e1aad7179abe40aaa2af2af0b7
Quai Wentt <quai.wentt@gmail.com>
2024-07-08 18:31:49 -0400

    Time: 713 ms (19.63%), Space: 30.5 MB (62.20%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.py
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.py
@@ -4,17 +4,13 @@ class Solution:
         #[1, 2, 3, 5, 6] 
         nums.sort()
         cur_min = float('inf')
-        cur_max = float('-inf')
         num_subsequences = 1
         for num in nums:
             possible_min = min(cur_min, num)
-            possible_max = max(cur_max, num)
-            if possible_max - possible_min <= k:
+            if num - possible_min <= k:
                 cur_min = possible_min
-                cur_max = possible_max
             else:
                 num_subsequences += 1
                 cur_min = num
-                cur_max = num
         return num_subsequences
         
