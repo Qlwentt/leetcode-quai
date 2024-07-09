dbba53d487756765f75a646aaffa76997981931e
Quai Wentt <quai.wentt@gmail.com>
2024-07-09 17:37:40 -0400

    Time: 148 ms (61.40%), Space: 17.2 MB (12.03%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -0,0 +1,17 @@
+from sortedcontainers import SortedList
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        def check(arr):
+            arr.sort()
+            diff = arr[1] - arr[0]
+            
+            for i in range(2, len(arr)):
+                if arr[i] - arr[i-1] != diff:
+                    return False
+                
+            return True
+                    
+        answers = []
+        for L, R in zip(l,r):
+            answers.append(check(nums[L:R+1]))
+        return answers            
