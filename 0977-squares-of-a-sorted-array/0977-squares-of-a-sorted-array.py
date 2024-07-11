674fb700546844cdb45b7b22ca67004ad84329b1
Quai Wentt <quai.wentt@gmail.com>
2024-07-11 16:10:07 -0400

    Time: 165 ms (17.06%), Space: 18.9 MB (23.61%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,28 +1,31 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-   # [16,1,0,9,100]
-  #     L R
-        L = None
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                L = i
-            nums[i] = nums[i] ** 2
-        
-        if L == None:
-            return nums
-        if L == len(nums) - 1:
-            nums.reverse()
-            return nums
-        R = L + 1
-        
+        # [-4,-1,0,3,10]
+        #   16 1 0 9 100
         answer = []
-        while L >= 0 or R < len(nums):
-            numL = nums[L] if L in range(len(nums)) else float('inf')
-            numR = nums[R] if R in range(len(nums)) else float('inf')
-            if numL < numR:
-                answer.append(numL)
-                L -= 1 
-            else:
-                answer.append(numR)
-                R += 1
-        return answer
+        last_negative = None
+        for i, num in enumerate(nums):
+            if num < 0:
+                last_negative = i
+            answer.append(num*num)
+        
+        if last_negative is None:
+            return answer
+        a = answer[(last_negative+1):]
+        b = answer[:last_negative+1][::-1]
+        def merge(a,b):
+            answer = []
+            p1 = 0
+            p2 = 0
+            while p1 < len(a) or p2 < len(b):
+                val1 = a[p1] if p1 < len(a) else float('inf')
+                val2 = b[p2] if p2 < len(b) else float('inf')
+                
+                if val1 < val2:
+                    p1 += 1
+                    answer.append(val1)
+                else:
+                    p2 += 1
+                    answer.append(val2)
+            return answer
+        return merge(a,b)
