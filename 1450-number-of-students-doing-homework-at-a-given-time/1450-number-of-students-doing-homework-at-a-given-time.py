2d43584dbb25c9e9e9e7f4dbb915bb1aa23fc44f
Quai Wentt <quai.wentt@gmail.com>
2024-07-11 15:53:35 -0400

    Time: 39 ms (66.77%), Space: 16.7 MB (7.80%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,13 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        # [1,3],[2,2],[3,7]
+        # -----
+        #   -
+        #     ---------
+        # 1 2 3 4 5 6 7
+        count = 0
+        for start, end in zip(startTime, endTime):
+            if queryTime in range(start,end+1):
+                count += 1
+        
+        return count
