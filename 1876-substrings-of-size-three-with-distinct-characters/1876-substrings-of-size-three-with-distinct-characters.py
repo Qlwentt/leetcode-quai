13e399dd68b4dbe6b2012a847972aee0bb831d14
Quai Wentt <quai.wentt@gmail.com>
2024-07-12 13:21:38 -0400

    Time: 39 ms (42.26%), Space: 16.5 MB (58.81%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -4,12 +4,12 @@ class Solution:
         letter_dict = collections.defaultdict(int)
         L = 0
         for R in range(len(s)):
-            letter_dict[s[R]] += 1
             if R-L+1 > 3:
                 letter_dict[s[L]] -= 1
                 if letter_dict[s[L]] == 0:
                     del letter_dict[s[L]]
                 L += 1
+            letter_dict[s[R]] += 1
             if len(letter_dict) == 3:
                 count += 1
         return count
