16278f8353e044c3542f92967fc670f75414c7f6
Quai Wentt <quai.wentt@gmail.com>
2024-07-19 18:23:22 -0400

    Time: 551 ms (42.01%), Space: 16.8 MB (5.70%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,23 +1,56 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 
-        nums.sort()
-        answer = set()
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                a = nums[i]
-                b = nums[j]
-                lo = j+1
-                hi = len(nums) - 1
+#         nums.sort()
+#         answer = set()
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 a = nums[i]
+#                 b = nums[j]
+#                 lo = j+1
+#                 hi = len(nums) - 1
                 
-                while lo < hi:
-                    if a + b + nums[lo] + nums[hi] > target:
-                        hi -= 1
-                    elif a + b + nums[lo] + nums[hi] < target:
-                        lo += 1
-                    else:
-                        answer.add(tuple([a,b,nums[lo], nums[hi]]))
-                        lo += 1
-                        hi -= 1
+#                 while lo < hi:
+#                     if a + b + nums[lo] + nums[hi] > target:
+#                         hi -= 1
+#                     elif a + b + nums[lo] + nums[hi] < target:
+#                         lo += 1
+#                     else:
+#                         answer.add(tuple([a,b,nums[lo], nums[hi]]))
+#                         lo += 1
+#                         hi -= 1
                                
-        return answer
+#         return answer
+
+        def kSum(k, target, arr):
+            answers = set()
+            if k == 2:
+                return twoSumII(target, arr)
+            else:
+                for i in range(len(arr)):
+                    a = arr[i]
+                    kminus1Answers = kSum(k-1, target-a, arr[i+1:])
+                    for answer in kminus1Answers:
+                        answer = list(answer)
+                        answers.add(tuple([a]+ answer))
+                        
+            return answers
+            
+        def twoSumII(target, nums):
+            lo = 0
+            hi = len(nums) - 1
+            answer = set()
+            
+            while lo < hi:
+                if nums[lo] + nums[hi] > target:
+                    hi -= 1
+                elif nums[lo] + nums[hi] < target:
+                    lo += 1
+                else:
+                    answer.add(tuple([nums[lo], nums[hi]]))
+                    lo += 1
+                    hi -= 1
+            return answer
+        nums.sort()
+        return kSum(4, target, nums)
+        
