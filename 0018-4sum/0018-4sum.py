7439142db71ed062cefbc0e3bea0137712bb90bc
Quai Wentt <quai.wentt@gmail.com>
2024-07-21 14:11:19 -0400

    Time: 372 ms (80.31%), Space: 16.8 MB (5.64%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 # Iterative only 4 sum
-        nums.sort()
-        answer = set()
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                a = nums[i]
-                b = nums[j]
-                lo = j+1
-                hi = len(nums) - 1
+#         nums.sort()
+#         answer = set()
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 a = nums[i]
+#                 b = nums[j]
+#                 lo = j+1
+#                 hi = len(nums) - 1
                 
-                while lo < hi:
-                    if a + b + nums[lo] + nums[hi] > target:
-                        hi -= 1
-                    elif a + b + nums[lo] + nums[hi] < target:
-                        lo += 1
-                    else:
-                        answer.add(tuple([a,b,nums[lo], nums[hi]]))
-                        lo += 1
-                        hi -= 1
+#                 while lo < hi:
+#                     if a + b + nums[lo] + nums[hi] > target:
+#                         hi -= 1
+#                     elif a + b + nums[lo] + nums[hi] < target:
+#                         lo += 1
+#                     else:
+#                         answer.add(tuple([a,b,nums[lo], nums[hi]]))
+#                         lo += 1
+#                         hi -= 1
                                
-        return answer
+#         return answer
 
 # Recursive, k sum:
         def kSum(k, target, arr):
