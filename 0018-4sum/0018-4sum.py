4098bbbac6c79938cd381887781b4b69976eb1c3
Quai Wentt <quai.wentt@gmail.com>
2024-07-22 13:22:28 -0400

    Time: 372 ms (80.49%), Space: 16.7 MB (24.19%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,73 +1,71 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-# Iterative only 4 sum
-
-#        [-2,-1,-1,1,1,2,2]
-         #    i
-         #      j
-    
-        nums.sort()
-        answer = []
-        for i in range(len(nums)):
-            a = nums[i]
-            if i > 0 and a == nums[i-1]:
-                continue
-            for j in range(i+1, len(nums)):
-                if j > i+1 and nums[j] == nums[j-1]:
-                    continue
-                b = nums[j]
-                lo = j+1
-                hi = len(nums) - 1
+        # Iterative only 4 sum
+        '''
+        [-2,-1,-1,1,1,2,2]
+          
+        '''
+#         nums.sort()
+#         answer = []
+#         for i in range(len(nums)):
+#             a = nums[i]
+#             if i > 0 and a == nums[i-1]:
+#                 continue
+#             for j in range(i+1, len(nums)):
+#                 if j > i+1 and nums[j] == nums[j-1]:
+#                     continue
+#                 b = nums[j]
+#                 lo = j+1
+#                 hi = len(nums) - 1
                 
-                while lo < hi:
-                    if a + b + nums[lo] + nums[hi] > target:
-                        hi -= 1
-                    elif a + b + nums[lo] + nums[hi] < target:
-                        lo += 1
-                    else:
-                        answer.append([a,b,nums[lo], nums[hi]])
+#                 while lo < hi:
+#                     if a + b + nums[lo] + nums[hi] > target:
+#                         hi -= 1
+#                     elif a + b + nums[lo] + nums[hi] < target:
+#                         lo += 1
+#                     else:
+#                         answer.append([a,b,nums[lo], nums[hi]])
                         
-                        lo += 1
-                        hi -= 1
-                        while lo < hi and nums[lo] == nums[lo-1]:
-                            lo += 1
-                               
-        return answer
+#                         lo += 1
+#                         hi -= 1
+#                         while lo < hi and nums[lo] == nums[lo-1]:
+#                             lo += 1
+        
+#         return answer
 
 # # Recursive, k sum:
-#         def kSum(k, target, arr):
-#             answers = []
-#             if k == 2:
-#                 return twoSumII(target, arr)
-#             else:
-#                 for i in range(len(arr)):
-#                     a = arr[i]
-#                     if i > 0 and arr[i] == arr[i-1]:
-#                         continue
-#                     kminus1Answers = kSum(k-1, target-a, arr[i+1:])
-#                     for answer in kminus1Answers:
-#                         answer.append(a)
-#                         answers.append(answer)
+        def kSum(k, target, arr):
+            answers = []
+            if k == 2:
+                return twoSumII(target, arr)
+            else:
+                for i in range(len(arr)):
+                    a = arr[i]
+                    if i > 0 and arr[i] == arr[i-1]:
+                        continue
+                    kminus1Answers = kSum(k-1, target-a, arr[i+1:])
+                    for answer in kminus1Answers:
+                        answers.append([a]+answer)
                         
-#             return answers
+            return answers
             
-#         def twoSumII(target, nums):
-#             lo = 0
-#             hi = len(nums) - 1
-#             answer = []
+        def twoSumII(target, nums):
+            lo = 0
+            hi = len(nums) - 1
+            answer = []
             
-#             while lo < hi:
-#                 if nums[lo] + nums[hi] > target:
-#                     hi -= 1
-#                 elif nums[lo] + nums[hi] < target:
-#                     lo += 1
-#                 else:
-#                     answer.append([nums[lo], nums[hi]])
-#                     lo += 1
-#                     hi -= 1
-#                     while lo < hi and lo > 0 and nums[lo] == nums[lo-1]:
-#                         lo += 1
-#             return answer
-#         nums.sort()
-#         return kSum(4, target, nums)
+            while lo < hi:
+                if nums[lo] + nums[hi] > target:
+                    hi -= 1
+                elif nums[lo] + nums[hi] < target:
+                    lo += 1
+                else:
+                    answer.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+                    while lo < hi and lo > 0 and nums[lo] == nums[lo-1]:
+                        lo += 1
+            return answer
+        nums.sort()
+        return kSum(4, target, nums)
         
