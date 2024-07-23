67cd51cc6ca17696581b80be9a6cbaa3a0e7b36c
Quai Wentt <quai.wentt@gmail.com>
2024-07-23 18:52:24 -0400

    Time: 132 ms (33.28%), Space: 18.2 MB (15.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,7 +7,13 @@ class Solution:
         left = 0
         for right in range(len(nums)):
             if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
+                # without python magic
+                temp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = temp
+
+                # python magic way to swap variables
+                # nums[left], nums[right] = nums[right], nums[left]
                 left += 1
         
 # Time: O(N)
