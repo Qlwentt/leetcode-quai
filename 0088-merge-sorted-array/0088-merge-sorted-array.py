f2a84618e5f8a24b8aaca2acec0d32ee8214fb2e
Quai Wentt <quai.wentt@gmail.com>
2024-07-24 13:13:48 -0400

    Time: 40 ms (54.51%), Space: 16.4 MB (86.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,21 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # [1,2,3,0,0,0]
-        #            P3
+        
         p1 = m - 1
         p2 = n - 1
-        p3 = len(nums1) - 1
         
-        while p1 >= 0 or p2 >= 0:
+        for i in range(len(nums1)-1,-1,-1):
             num1 = nums1[p1] if p1 >= 0 else float('-inf')
             num2 = nums2[p2] if p2 >= 0 else float('-inf')
-            
             if num1 > num2:
-                nums1[p1], nums1[p3] = nums1[p3], nums1[p1]
+                nums1[i] = num1
                 p1 -= 1
             else:
-                nums2[p2], nums1[p3] = nums1[p3], nums2[p2]
+                nums1[i] = num2
                 p2 -= 1
-            p3 -= 1
+        
         
