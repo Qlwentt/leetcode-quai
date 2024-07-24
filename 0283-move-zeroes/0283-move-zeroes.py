fc2279a8db5aba264a6dfbf6c064bd2338ee58df
Quai Wentt <quai.wentt@gmail.com>
2024-07-23 21:51:13 -0400

    Time: 124 ms (66.92%), Space: 18.1 MB (50.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,17 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+     #   [0,1,0,3,12]
+      #  P
+      #  Q
         
         left = 0
         for right in range(len(nums)):
             if nums[right] != 0:
-                # without python magic
-                temp = nums[left]
                 nums[left] = nums[right]
-                nums[right] = temp
-
-                # python magic way to swap variables
-                # nums[left], nums[right] = nums[right], nums[left]
+                if right != left:
+                    nums[right] = 0
                 left += 1
         
 # Time: O(N)
