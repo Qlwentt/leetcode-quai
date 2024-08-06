404abb621098dfd4dca9328e588d89531f5ed122
Quai Wentt <quai.wentt@gmail.com>
2024-08-06 13:30:38 -0400

    Time: 265 ms (69.76%), Space: 25.7 MB (47.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,20 +4,17 @@ class Solution:
         #[1,1,2,6]
         #[24,12,4,1]
         
-        forward = [1] * len(nums)
-        backward = [1] * len(nums)
+        answer = [1] * len(nums)
         
         cur_prod = 1
         for i, num in enumerate(nums):
-            forward[i] = cur_prod
+            answer[i] = cur_prod
             cur_prod *= num
         cur_prod = 1
         for i in range(len(nums)-1,-1,-1):
-            backward[i] = cur_prod
+            answer[i] *= cur_prod 
             cur_prod *= nums[i]
         
-        answer = []
-        for f, b in zip(forward, backward):
-            answer.append(f*b)
+        
         
         return answer
