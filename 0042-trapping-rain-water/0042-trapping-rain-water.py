a4d4ed37d0321f3d69b2dc86779af2252eec5a53
Quai Wentt <quai.wentt@gmail.com>
2024-08-06 22:01:45 -0400

    Time: 127 ms (9.70%), Space: 18.6 MB (20.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,42 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-#         # min(L,R) - h[i]
+        # [0,1,0,2,1,0,1,3,2,1,2,1]
+        #  3 3 3 3 3 3 3 3 2 2 2 1
+        #  0 1 1 2 2 2 2 3 3 3 3 3
+        forward_maxes = [0] * len(height)
+        backward_maxes = [0] * len(height)
         
-#         Lmaxes = [0] * len(height)
-#         Rmaxes = [0] * len(height)
+        max_ = float('-inf')
+        for i, h in enumerate(height):
+            max_ = max(h, max_)
+            forward_maxes[i] = max_
+        max_ = float('-inf')
+        for i in range(len(height)-1,-1,-1):
+            max_ = max(height[i], max_)
+            backward_maxes[i] = max_
+        area = 0
+        for i, h in enumerate(height):
+            area += max(min(forward_maxes[i], backward_maxes[i])-h,0)
         
-#         Lmax = 0
-#         for i in range(len(height)):
-#             Lmax = max(Lmax, height[i])
-#             Lmaxes[i] = Lmax
+        return area
         
-#         Rmax = 0
-#         for i in range(len(height)-1, -1 , -1):
-#             Rmax = max(Rmax,height[i])
-#             Rmaxes[i] = Rmax
             
-#         water = 0
-#         for i, elevation in enumerate(height):
-#             water += min(Lmaxes[i], Rmaxes[i]) - elevation
-            
-#         return water
-        L = 0
-        R = len(height) - 1
-        
-        water = 0
-        Lmax = 0
-        Rmax = 0
-        while L < R:
-            if height[L] < height[R]:
-                Lmax = max(Lmax, height[L])
-                water += Lmax - height[L]
-                L +=1
-            else:
-                Rmax = max(Rmax, height[R])
-                water += Rmax - height[R]
-                R -= 1
-        return water
-                
-            
-            
-        
