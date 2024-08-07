288d72fab3e3b1178367b9ca650fe1c34ed2aeab
Quai Wentt <quai.wentt@gmail.com>
2024-08-07 16:46:56 -0400

    Time: 848 ms (6.69%), Space: 33.2 MB (6.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxPrices = prices.copy()
-        for i in range(len(prices)-2,-1,-1):
-            maxPrices[i] = max(prices[i], maxPrices[i+1])
+        # [7,1,5,3,6,4]
+        #  7 6 6 6 6 4
         
-        maxProfit = 0
-        for buyDay in range(len(prices)-1):
-            buyPrice = prices[buyDay]
-            sellPrice = maxPrices[buyDay+1]
-            maxProfit = max(maxProfit, sellPrice-buyPrice)
-        return maxProfit
+        maxes = [0] * len(prices)
+        cur_max = float('-inf')
+        
+        for i, p in reversed(list(enumerate(prices))):
+            cur_max = max(cur_max,p)
+            maxes[i] = cur_max
+        max_profit = 0
+        for i, p in enumerate(prices):
+            best_sell = maxes[i]
+            buy = p
+            max_profit = max(max_profit, best_sell-buy)
+        return max_profit
+            
+        
