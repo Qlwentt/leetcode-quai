b82a668798620a2d58d7d75a1992951e36f2eed3
Quai Wentt <quai.wentt@gmail.com>
2024-08-07 23:04:37 -0400

    Time: 251 ms (87.27%), Space: 18.1 MB (44.68%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,28 +1,20 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-    #   [3,6,7,11]
-    # 3// 1 + 6//1 + 7//1 + 11//1
-    # 3//min(3,4) + 6 // (4,6) 
-
-        def getHours(k):
-            hours = 0            
-            for pile in piles:
-                hours += (pile // min(k,pile)) + (pile % min(k,pile) != 0)
-            return hours
-        # 11,10,9,9,8,7,7,6,6,6,6,6,5,5,5,5,5
-        #               L       H           H
+        
+        def can_finish(k):
+            hours = 0
+            for bananas in piles:
+                hours += math.ceil(bananas / k)
+            return hours <= h
         
         lo = 1
         hi = max(piles)
         
-        while lo < hi:
+        while lo <= hi:
             mid = (lo + hi) // 2
-            hours = getHours(mid)
-            if hours > h: # k(mid) is too fast(high)
-                lo = mid + 1
-            else: # k(mid) is too slow (low)
-                hi = mid
-        return lo # first instance of hours == h
             
-    
-        
+            if can_finish(mid):
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
