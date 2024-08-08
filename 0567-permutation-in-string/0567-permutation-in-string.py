2288127694f0345575d82e2e952a6570bb7c7cdf
Quai Wentt <quai.wentt@gmail.com>
2024-08-07 20:08:38 -0400

    Time: 328 ms (22.71%), Space: 16.8 MB (17.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,43 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+        counts_s1 = collections.Counter(s1)
+        counts_s2 = collections.defaultdict(int) 
         
-        counterS1 = {char:0 for char in "abcdefghijklmnopqrstuvwxyz"}
-        for char in s1:
-            counterS1[char] += 1
-            
-        counterS2 = {char:0 for char in "abcdefghijklmnopqrstuvwxyz"}
-
-        L = 0
-        R = len(s1) - 1
-        
-        matches = 0
-        for char in s2[L:R+1]:
-            counterS2[char] += 1
-        for char, count in counterS1.items():
-            if counterS2[char] == counterS1[char]:
-                matches += 1
-        
-        while R < len(s2) - 1:                
-            if matches == 26:
+        # "dcda"
+        #   L
+        #    R
+        l = 0
+        for r in range(len(s2)):
+            counts_s2[s2[r]] += 1
+            while any(item2 not in counts_s1 for item2 in counts_s2) or any(counts_s2[item2] > counts_s1[item2] for item2 in counts_s2):
+                counts_s2[s2[l]] -= 1
+                if counts_s2[s2[l]] == 0:
+                    del counts_s2[s2[l]]
+                l += 1
+                    
+            if counts_s2 == counts_s1:
                 return True
-                        
-            prevChar = s2[L]
-            nextChar = s2[R+1]
-            
-            counterS2[prevChar] -= 1
-            if counterS1[prevChar] == counterS2[prevChar]: 
-                matches += 1
-            elif counterS1[prevChar] == counterS2[prevChar] + 1:
-                matches -= 1
-            
-            counterS2[nextChar] += 1
-            if counterS1[nextChar] == counterS2[nextChar]:
-                matches += 1
-            elif counterS1[nextChar] == counterS2[nextChar] - 1:
-                matches -= 1
+        return False
                 
-            L += 1
-            R += 1
-        return matches == 26
+            
