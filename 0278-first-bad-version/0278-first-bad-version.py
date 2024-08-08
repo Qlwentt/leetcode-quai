a27e63ece18ca46de1a334f0fd2757a7f144a02f
Quai Wentt <quai.wentt@gmail.com>
2024-08-08 18:18:26 -0400

    Time: 36 ms (46.27%), Space: 16.4 MB (84.95%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -5,15 +5,17 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         lo = 1
         hi = n
-        lowest_bad = None
+        #lowest_bad_version = None
         while lo <= hi:
-            mid = (lo + hi) // 2
+            mid = (lo+hi) // 2
             
             if isBadVersion(mid):
-                lowest_bad = mid
                 hi = mid - 1
+                #lowest_bad_version = mid
             else:
                 lo = mid + 1
-                
-        return lowest_bad
+        return lo
+        #return lowest_bad_version
+    
+    
         
