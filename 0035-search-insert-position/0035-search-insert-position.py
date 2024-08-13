d03a9e93f19e3e9d7302a696e3275a06cc886377
Quai Wentt <quai.wentt@gmail.com>
2024-08-13 14:25:45 -0400

    Time: 45 ms (82.78%), Space: 17.2 MB (45.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -7,11 +7,21 @@ class Solution:
         l = 0
         r = len(nums) - 1
         
+#         while l <= r:
+#             mid = (l+r) // 2
+            
+#             if nums[mid] >= target:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return l
+
         while l <= r:
             mid = (l+r) // 2
-            
-            if nums[mid] >= target:
+            if nums[mid] > target:
                 r = mid - 1
-            else:
+            elif nums[mid] < target:
                 l = mid + 1
+            else:
+                return mid
         return l
