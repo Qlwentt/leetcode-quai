47bb6de5b69b1e657aaa1d853f61a84aa8b8a50f
Quai Wentt <quai.wentt@gmail.com>
2024-08-13 14:06:36 -0400

    Time: 681 ms (61.19%), Space: 38.8 MB (40.41%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,16 +1,24 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        posNSpeed = [(pos,spd) for pos, spd in zip(position,speed)]
-        posNSpeed.sort(reverse=True)
-        stack = [] # time to target
-        for p, s in posNSpeed:
-            time = (target-p)/s
-            stack.append(time)
-            if len(stack) > 1 and stack[-1] <= stack[-2]:
+        data = sorted([(p,s) for p,s in zip(position, speed)]) 
+        #[(0, 1), (3, 3), (5, 1), (8, 4), (10, 2)]
+        # [12,7,1]
+         #   12     3
+        stack = []
+        for p, s in data:
+            arrive_time = (target - p) / s
+            while stack and stack[-1] <= arrive_time: 
                 stack.pop()
+            stack.append(arrive_time)
         return len(stack)
-            
-            
-            
-            
-    
+        
+        
+        
+        # 3 miles/hr, start mile 3     1 miles/hr start mile 5
+        # 
+        # mile 4  - 1/3 hr              mile 6  - 1 hr
+        # mile 5  - 2/3 hr
+        # mile 6  - 1 hr 
+        
+       
+        
