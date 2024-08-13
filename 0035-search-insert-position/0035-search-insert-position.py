d185a6bda7ea7a95dae490919a60778e318fb92d
Quai Wentt <quai.wentt@gmail.com>
2024-08-13 18:18:26 -0400

    Time: 50 ms (53.38%), Space: 17.2 MB (45.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,19 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
                 
-        # l = 0
-        # r = len(nums) - 1
+        l = 0
+        r = len(nums) - 1
 
 #         more intuitive solution for this problem
-        # while l <= r:
-        #     mid = (l+r) // 2
-        #     if nums[mid] > target:
-        #         r = mid - 1
-        #     elif nums[mid] < target:
-        #         l = mid + 1
-        #     else:
-        #         return mid
-        # return l
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+        return l
     
 # shorter solution and works for other problems like LC 34       
 #         while l <= r:
@@ -27,4 +27,4 @@ class Solution:
 
 
 # one line solution in python (cheating using a library function)
-        return bisect.bisect_left(nums,target)
+        # return bisect.bisect_left(nums,target)
