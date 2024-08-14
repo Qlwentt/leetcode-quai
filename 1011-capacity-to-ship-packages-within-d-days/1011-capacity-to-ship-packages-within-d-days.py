4ed3678ee137ce6335065b166bf78d7ffa1f9da2
Quai Wentt <quai.wentt@gmail.com>
2024-08-14 00:43:20 -0400

    Time: 385 ms (71.23%), Space: 22.5 MB (20.88%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,28 +1,29 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def canShipWithCapacity(capacity):
-            curWeight = 0
-            remainingDays = days
-            for R in range(len(weights)):
-                curWeight += weights[R]
-                if weights[R] > capacity:
+        def can_ship(capacity):
+            days_taken = 1
+            total_weight = 0
+            for w in weights:
+                if w > capacity:
                     return False
-                while curWeight > capacity:
-                    curWeight = weights[R]
-                    remainingDays -= 1
-            
-            return remainingDays > 0
-        L = max(weights)
-        R = sum(weights)
+                total_weight += w
+                if total_weight > capacity:
+                    days_taken += 1
+                    total_weight = w
+            return days_taken <= days
         
-        while L <= R:
-            mid = (L+R) // 2
+        lo = max(weights)
+        hi = sum(weights)
+        
+        while lo <= hi:
+            mid = (lo + hi) // 2
             
-            if canShipWithCapacity(mid): # look for lower
-                R = mid - 1
+            if can_ship(mid):
+                hi = mid - 1
             else:
-                L = mid + 1
-        return L
-        
-                    
-            
+                lo = mid + 1
+        return lo
+    
+# Time: O(Nlog(N))
+# Space: O(1)
+                
