b63e97cf651a17f712e7abe3e380184f41b54d75
Quai Wentt <quai.wentt@gmail.com>
2024-08-15 17:01:07 -0400

    Time: 741 ms (5.08%), Space: 16.5 MB (19.46%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,27 +1,27 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         
-#         rows = 0 
+        rows = 0 
         
-#         while n > 0: # -1
-#             n -= (rows + 1)
-#             rows += 1
+        while n - (rows + 1) >= 0:
+            n -= (rows + 1)
+            rows += 1
             
-#         return rows-1 if n < 0 else rows
+        return rows
         
-        def is_filled(row):
-            return (row * (row+1)) / 2 <= n
+#         def is_filled(row):
+#             return (row * (row+1)) / 2 <= n
 
-        lo = 1
-        hi = n                      
-        last_complete = None
-        while lo <= hi:
-            mid = (lo+hi) //2
-            if is_filled(mid):
-                lo = mid + 1
-                last_complete = mid
-            else:
-                hi = mid - 1
-        return last_complete
+#         lo = 1
+#         hi = n                      
+#         last_complete = None
+#         while lo <= hi:
+#             mid = (lo+hi) //2
+#             if is_filled(mid):
+#                 lo = mid + 1
+#                 last_complete = mid
+#             else:
+#                 hi = mid - 1
+#         return last_complete
         
             
