3f94c85de0c594b48671f9e1e9f3bdd6348442b8
Quai Wentt <quai.wentt@gmail.com>
2024-08-15 18:57:52 -0400

    Time: 39 ms (74.14%), Space: 16.5 MB (60.61%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,29 +1,29 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+# brute force        
+#         row = 1 
         
-        row = 1 
-        
-        while n - row > 0:
-            n -= row
-            row += 1
+#         while n - row > 0:
+#             n -= row
+#             row += 1
             
-        return row if n - row == 0 else row - 1
+#         return row if n - row == 0 else row - 1
         
-#         def is_filled(row):
-#             return (row * (row+1)) / 2 <= n
-
-#         lo = 1
-#         hi = n                      
-#         # last_complete = None
-#         while lo <= hi:
-#             mid = (lo+hi) //2
-#             if is_filled(mid):
-#                 lo = mid + 1
-#                 # last_complete = mid
-#             else:
-#                 hi = mid - 1
-#         # return last_complete
-#         return hi
+        def is_filled(row):
+            return (row * (row+1)) / 2 <= n
+# binary search
+        lo = 1
+        hi = n                      
+        # last_complete = None
+        while lo <= hi:
+            mid = (lo+hi) //2
+            if is_filled(mid):
+                lo = mid + 1
+                # last_complete = mid
+            else:
+                hi = mid - 1
+        # return last_complete
+        return hi
         
 
 
