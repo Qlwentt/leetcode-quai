00e75fdeead3a4c9c6b3c80b2ec901fdb8992114
Quai Wentt <quai.wentt@gmail.com>
2024-08-19 14:59:14 -0400

    Time: 43 ms (50.05%), Space: 16.6 MB (68.64%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,13 +1,16 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-#         cur_sum = 0
-#         answer = []
-#         for num in nums:
-#             cur_sum += num
-#             answer.append(cur_sum)
-#         return answer
+        cur_sum = 0
+        answer = []
+        for num in nums:
+            cur_sum += num
+            answer.append(cur_sum)
+        return answer
 # # Time: O(N)
 # Space: O(N) counting the output
+
+# in place
+# might not be considered best practice to alter an array while you are iterating through it
 
         cur_sum = 0
         for i in range(len(nums)):
