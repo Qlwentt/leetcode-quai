e0a8bf0731ad0739686aa81c4419d47caa04a988
Quai Wentt <quai.wentt@gmail.com>
2024-08-20 18:17:46 -0400

    Time: 110 ms (96.96%), Space: 17.7 MB (83.35%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -3,25 +3,41 @@ class Solution:
     # [0,  1, 8,11,17,22]
     # [27,20,17,11,6, 0]
     
-        cur_sum = 0
-        left = []
-        for num in nums:
-            left.append(cur_sum)
-            cur_sum += num
+#         cur_sum = 0
+#         left = []
+#         for num in nums:
+#             left.append(cur_sum)
+#             cur_sum += num
         
-        cur_sum = 0
-        right = [0] * len(nums)
+#         cur_sum = 0
+#         right = [0] * len(nums)
         
-        for i in range(len(nums)-1,-1,-1):
-            right[i] = cur_sum
-            cur_sum += nums[i]
+#         for i in range(len(nums)-1,-1,-1):
+#             right[i] = cur_sum
+#             cur_sum += nums[i]
         
-        for i in range(len(left)):
-            if left[i] == right[i]:
-                return i
+#         for i in range(len(left)):
+#             if left[i] == right[i]:
+#                 return i
             
-        return -1
+#         return -1
 # Time: O(N)
 # Space: O(N)
+
+#  constant space solution
+
+        sum_left = 0
+        sum_right = sum(nums)
+
+        for i, num in enumerate(nums):
+            sum_right -= num
+            if sum_left == sum_right:
+                return i
+            sum_left += num
+
+        return -1
+
+# Time: O(N)
+# Space: O(1)
     
     
