7cfba551b6ced693f4a5f44fbc4cc2cc1a18d6cd
Quai Wentt <quai.wentt@gmail.com>
2024-08-21 15:03:51 -0400

    Time: 265 ms (67.65%), Space: 26.3 MB (20.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,6 +1,30 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+    # O(N) space solution
+    
+        forward = [1] * len(nums)
+        backward = [1] * len(nums)
+        
+        cur_prod = 1
+        for i, num in enumerate(nums):
+            forward[i] = cur_prod
+            cur_prod *= num
+        cur_prod = 1
+        for i in range(len(nums)-1,-1,-1):
+            backward[i] = cur_prod
+            cur_prod *= nums[i]
+        
+        answer = []
+        for f, b in zip(forward, backward):
+            answer.append(f*b)
+        
+        return answer
+    # Time: O(N)
+    # Space: O(N)
+        
+    # constant space solution
         #[1,2,3,4]
+        
         #[1,1,2,6]
         #[24,12,4,1]
         
@@ -10,11 +34,17 @@ class Solution:
         for i, num in enumerate(nums):
             answer[i] = cur_prod
             cur_prod *= num
+        
         cur_prod = 1
         for i in range(len(nums)-1,-1,-1):
             answer[i] *= cur_prod 
             cur_prod *= nums[i]
         
         
-        
         return answer
+    
+# Time: O(N)
+# Space: O(1) not counting output
+
+
+            
