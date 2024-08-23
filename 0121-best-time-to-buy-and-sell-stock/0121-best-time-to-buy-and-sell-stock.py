4d802d076002c5c2b0ad93af2ec8ae55a33f438b
Quai Wentt <quai.wentt@gmail.com>
2024-08-22 23:33:48 -0400

    Time: 776 ms (25.46%), Space: 27.3 MB (82.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_prices = collections.deque([])
-        cur_max = float('-inf')
-        for price in reversed(prices):
-            cur_max = max(cur_max, price)
-            max_prices.appendleft(cur_max)
+#         max_prices = collections.deque([])
+#         cur_max = float('-inf')
+#         for price in reversed(prices):
+#             cur_max = max(cur_max, price)
+#             max_prices.appendleft(cur_max)
             
-        # [7,6,6,6,4]
-        max_profit = 0
-        for i, buy_price in enumerate(prices):
-            today_profit = max_prices[i] - buy_price
-            max_profit = max(today_profit, max_profit)
+#         # [7,6,6,6,4]
+#         max_profit = 0
+#         for i, buy_price in enumerate(prices):
+#             today_profit = max_prices[i] - buy_price
+#             max_profit = max(today_profit, max_profit)
         
+#         return max_profit
+# Time: O(N)
+# Space: O(N)
+
+    #[7,1,5,3,6,4]
+
+# constant space solution
+        cur_max = float('-inf')
+        max_profit = 0
+        for price in reversed(prices):
+            cur_max = max(price, cur_max)
+            today_profit = cur_max - price
+            max_profit = max(today_profit, max_profit)
         return max_profit
