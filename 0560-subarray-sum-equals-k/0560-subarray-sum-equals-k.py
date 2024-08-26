b2714871e12c6ab4380c7ae94613835d95a83698
Quai Wentt <quai.wentt@gmail.com>
2024-08-26 10:08:22 -0400

    Time: 238 ms (23.67%), Space: 19.4 MB (86.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,18 @@
+from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixDict = defaultdict(int)
-        prefixDict[0] = 1
+        prefix_dict = defaultdict(int)
+        prefix_dict[0] = 1
         
-        curSum = 0
+        cur_sum = 0
         answer = 0
         for num in nums:
-            curSum += num
-            if curSum - k in prefixDict:
-                answer += prefixDict[curSum-k]
-            prefixDict[curSum] += 1
-        return answer
+            cur_sum += num
+            if cur_sum - k in prefix_dict:
+                answer += prefix_dict[cur_sum-k]
+            prefix_dict[cur_sum] += 1
         
+        return answer
+    
+# Time: O(N)
+# Space: O(N)
