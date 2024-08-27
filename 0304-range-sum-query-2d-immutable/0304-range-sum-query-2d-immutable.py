175c5491b88f7c6f90ae05e59abf4b9fe9e01ea2
Quai Wentt <quai.wentt@gmail.com>
2024-08-26 22:43:17 -0400

    Time: 1178 ms (13.81%), Space: 37.9 MB (5.44%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,26 +1,23 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
-        self.sumMatrix = [[0] * (COLS + 1) for _ in range(ROWS+1)]
+        self.prefix_matrix = defaultdict(int)
         
-        for row in range(ROWS):
-            prefixSumRow = 0
-            for col in range(COLS):
-                prefixSumRow += matrix[row][col]
-                r = row + 1
-                c = col + 1
-                self.sumMatrix[r][c] = self.sumMatrix[r-1][c] + prefixSumRow        
+        for row in range(len(matrix)):
+            prefix_row = 0
+            for col in range(len(matrix[0])):
+                prefix_row += matrix[row][col]
+                self.prefix_matrix[(row,col)] = self.prefix_matrix[(row-1,col)] + prefix_row 
+        print(self.prefix_matrix)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        row1, col1, row2, col2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
-        bottomRight = self.sumMatrix[row2][col2]
-        aboveTopLeft = self.sumMatrix[row1-1][col1-1]
-        beforeBottomLeft = self.sumMatrix[row2][col1-1]
-        aboveTopRight = self.sumMatrix[row1-1][col2]
+        cross = self.prefix_matrix[(row1-1,col1-1)]
+        left = self.prefix_matrix[(row2, col1-1)]
+        top = self.prefix_matrix[(row1-1,col2)]
+        bottom_right = self.prefix_matrix[(row2,col2)]        
+        return bottom_right - (left+top-cross)
         
-        return bottomRight - (beforeBottomLeft + aboveTopRight) + aboveTopLeft
+        
         
 
 
