01b987e0524ba3a9c81444cc4c58e589ad77dd7c
Quai Wentt <quai.wentt@gmail.com>
2024-08-28 11:08:30 -0400

    Time: 548 ms (89.77%), Space: 30.2 MB (46.91%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -1,18 +1,29 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        postfix_sums = [0] * len(shifts)
-        cur_sum = 0
-        for i in range(len(shifts)-1,-1,-1):
-            cur_sum += shifts[i]
-            postfix_sums[i] = cur_sum
+#         postfix_sums = [0] * len(shifts)
+#         cur_sum = 0
+#         for i in range(len(shifts)-1,-1,-1):
+#             cur_sum += shifts[i]
+#             postfix_sums[i] = cur_sum
         
+#         answer = []
+#         char_to_index = {char: i for i, char in enumerate('abcdefghijklmnopqrstuvwxyz')}
+#         index_to_char = "abcdefghijklmnopqrstuvwxyz"
+#         for i, char in enumerate(s):
+#             shifted_ord = (char_to_index[char] + postfix_sums[i]) % 26
+#             answer.append(index_to_char[shifted_ord])
+            
+#         return "".join(answer)
+# Time: O(N)
+# Space: O(N)
+        postfix_sum = sum(shifts)
         answer = []
         char_to_index = {char: i for i, char in enumerate('abcdefghijklmnopqrstuvwxyz')}
         index_to_char = "abcdefghijklmnopqrstuvwxyz"
         for i, char in enumerate(s):
-            shifted_ord = (char_to_index[char] + postfix_sums[i]) % 26
-            answer.append(index_to_char[shifted_ord])
-            
+            shifted_i = (char_to_index[char] + postfix_sum) % 26
+            answer.append(index_to_char[shifted_i])
+            postfix_sum -= shifts[i]
         return "".join(answer)
-# Time: O(N)
-# Space: O(N)
+        
+        
