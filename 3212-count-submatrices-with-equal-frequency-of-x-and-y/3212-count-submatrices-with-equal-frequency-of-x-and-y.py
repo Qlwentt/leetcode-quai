ffad4a3d2efcbb16fb7bd9e0922e37ca49c80115
Quai Wentt <quai.wentt@gmail.com>
2024-08-29 11:47:42 -0400

    Time: 3698 ms (50.93%), Space: 187.3 MB (10.79%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -6,17 +6,17 @@ class Solution:
         prefix_y = collections.defaultdict(lambda: collections.defaultdict(int))
         
         for r in range(ROWS):
-            prev_x = 0
-            prev_y = 0
+            prefix_row_x = 0
+            prefix_row_y = 0
             for c in range(COLS):
-                prev_x += 1 if grid[r][c] == "X" else 0
-                prev_y += 1 if grid[r][c] == "Y" else 0
+                prefix_row_x += 1 if grid[r][c] == "X" else 0
+                prefix_row_y += 1 if grid[r][c] == "Y" else 0
                 
                 above_x = prefix_x[r-1][c]
                 above_y = prefix_y[r-1][c]
                 
-                prefix_x[r][c] = above_x + prev_x
-                prefix_y[r][c] = above_y + prev_y
+                prefix_x[r][c] = above_x + prefix_row_x
+                prefix_y[r][c] = above_y + prefix_row_y
         
         count = 0
         for r in range(ROWS):
@@ -24,6 +24,9 @@ class Solution:
                 if prefix_x[r][c] == prefix_y[r][c] and prefix_x[r][c] > 0:
                     count += 1
         return count
+    
+# Time: O(N^2)
+# Space: O(N^2)
                 
         
         
