6da2385cbce16d477889861be78ae951eda169d3
Quai Wentt <quai.wentt@gmail.com>
2024-08-30 13:12:14 -0400

    Time: 137 ms (10.88%), Space: 18.5 MB (24.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -21,5 +21,11 @@ class Solution:
             area += max(min(left_maxes[i], right_maxes[i])-h,0)
         
         return area
-        
+# Time: O(N)
+# Space: O(N)
+
+# There is a more optimal solution (O(1) space) using two pointers
+# but this solution is good enough for an interview, especially since this is a hard level problem
+
+# if you are curious, I would encourage you to look it up - neetcode video: https://www.youtube.com/watch?v=ZI2z5pq0TqA
             
