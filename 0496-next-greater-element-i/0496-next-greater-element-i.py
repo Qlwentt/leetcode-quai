2f2e19e854d9de0a9a73490bc7ec86b7f3dacc56
Quai Wentt <quai.wentt@gmail.com>
2024-09-09 19:29:28 -0400

    Time: 49 ms (56.97%), Space: 16.8 MB (69.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,24 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-#     [1,4,3,5,6] [4 ,5 ,6 ]
-#     [6,5,4]
-#     [4,3,5,6,0]
-     # monotonically decreasing - go in reverse
-        indices = {num:i for i,num in enumerate(nums1)}
+        num_to_i = {num: i for i, num in enumerate(nums1)}
+        
+        stack = [] # 
         answer = [-1] * len(nums1)
-        stack = []
-        #[1,3,4,2]
-        for i in range(len(nums2)-1,-1,-1):
-            num = nums2[i]
-            while stack and num >= stack[-1]:
-                stack.pop()
-            if stack and num in indices:
-                answer[indices[num]] = stack[-1]
+        for num in nums2:
+            while stack and stack[-1] < num:
+                lesser = stack.pop()
+                if lesser in num_to_i:
+                    answer[num_to_i[lesser]] = num
             stack.append(num)
-        
         return answer
-
-
-
-        
-    
+                
