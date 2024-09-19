1505c8257ebcce16299784f3936df404ecc81af1
Quai Wentt <quai.wentt@gmail.com>
2024-09-19 14:51:40 -0400

    Time: 187 ms (82.40%), Space: 22.3 MB (17.88%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -12,6 +12,8 @@ class RecentCounter:
         return len(self.q)
         
 
+# Time O(3000 * N) = O(N) where N is number of calls to ping
+# Space O(3000) = O(1)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
