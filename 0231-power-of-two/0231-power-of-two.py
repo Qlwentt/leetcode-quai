8715b9571f1756e2505bc6eb2a25f26d15142395
Quai Wentt <quai.wentt@gmail.com>
2024-09-24 16:03:58 -0400

    Time: 131 ms (7.01%), Space: 16.6 MB (26.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -4,8 +4,7 @@ class Solution:
             return False 
         if n == 1:
             return True
-        if n % 2 != 0:
-            return False
+        
         
         return self.isPowerOfTwo(n / 2)
         
