f671497beadcc7c93d4081fdfe14f62e5edfa701
Quai Wentt <quai.wentt@gmail.com>
2024-09-26 17:30:39 -0400

    Time: 59 ms (76.43%), Space: 16.7 MB (10.30%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,13 +1,23 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        # if n <= 0:
-        #     return False
-        # if n == 1:
-        #     return True
-        # if n % 3 != 0:
-        #     return False
-        # return self.isPowerOfThree(n/3)
+        # recursive solution
         if n <= 0:
             return False
-        return (math.log10(n) / math.log10(3)) % 1 == 0
+        if n == 1:
+            return True
+        if n % 3 != 0:
+            return False
+        return self.isPowerOfThree(n/3)
+# Time: O(log(N)) (base 3)
+# Space: O(log(N)) (base 3)        
+        
+        # optimized math solution
+        # return true if log base 3 of n is an integer
+        if n <= 0:
+            return False
+        return (math.log10(n) / math.log10(3)) % 1 == 0 
+
+# Time: O(1) in python (depends on time complexity of log library function)
+# Space: O(1)
+
         
