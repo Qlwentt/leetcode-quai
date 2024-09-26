7ae221cbdf2c09e84e47e4e3ed3c731119bd4c58
Quai Wentt <quai.wentt@gmail.com>
2024-09-26 17:24:40 -0400

    Time: 31 ms (80.33%), Space: 16.6 MB (53.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -13,8 +13,7 @@ class Solution:
         def power(x,n):
             if n == 0:
                 return 1
-            if n == 1:
-                return x
+            
             
             halfPower = power(x, n // 2)
             if n % 2: # odd
