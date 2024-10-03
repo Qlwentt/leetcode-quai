390840ce5820fcf7d23cb11c28176f8de166533a
Quai Wentt <quai.wentt@gmail.com>
2024-10-02 23:02:07 -0400

    Time: 44 ms (6.18%), Space: 16.5 MB (24.05%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,17 +6,33 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        answer = []
-        def inorder(node):
-            if not node:
-                return 
+#         answer = []
+#         def inorder(node):
+#             if not node:
+#                 return 
             
-            inorder(node.left)
-            answer.append(node.val)
-            inorder(node.right)
-        inorder(root)
-        return answer
+#             inorder(node.left)
+#             answer.append(node.val)
+#             inorder(node.right)
+#         inorder(root)
+#         return answer
 # Time: O(N)
 # Space: O(H) where h is the height of the tree. O(N) for a skewed tree, O(log(N)) for a balanced tree
+        stack = [(root, False)]
+        answer = []
+        while stack:
+            node, to_visit = stack.pop()
+            if not node:
+                continue
+            if to_visit:
+                answer.append(node.val)
+            else:
+                stack.append((node.right, False))
+                stack.append((node, True))
+                stack.append((node.left, False))
+            
+        return answer
+            
+        
         
         
