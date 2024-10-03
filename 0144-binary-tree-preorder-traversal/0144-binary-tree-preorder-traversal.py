27783d6ad02f2e0b1a0229efe3cf9a42b370a4e9
Quai Wentt <quai.wentt@gmail.com>
2024-10-02 23:49:42 -0400

    Time: 37 ms (42.79%), Space: 16.5 MB (24.58%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,14 +6,31 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        answer = []
-        def preorder(node):
-            if not node:
-                return
-            answer.append(node.val)
-            preorder(node.left)
-            preorder(node.right)
-        preorder(root)
-        return answer
+        # answer = []
+        # def preorder(node):
+        #     if not node:
+        #         return
+        #     answer.append(node.val)
+        #     preorder(node.left)
+        #     preorder(node.right)
+        # preorder(root)
+        # return answer
 # Time: O(N)
 # Space: O(H) where H is the height of the tree
+
+        stack = [(root, False)]
+        answer = []
+        
+        while stack:
+            node, to_visit = stack.pop()
+            if not node:
+                continue
+            
+            if to_visit:
+                answer.append(node.val)
+            else:
+                stack.append((node.right, False))
+                stack.append((node.left,False))
+                stack.append((node, True))
+        return answer
+                
