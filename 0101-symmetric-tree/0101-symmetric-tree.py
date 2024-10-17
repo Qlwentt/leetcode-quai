7680b3add905c670e2f86da7756ca0276afeebba
Quai Wentt <quai.wentt@gmail.com>
2024-10-17 18:39:35 -0400

    Time: 3 ms (100.00%), Space: 16.8 MB (5.44%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -7,44 +7,66 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 # Recursive DFS
-        if not root.left and not root.right:
-            return True
+#         if not root.left and not root.right:
+#             return True
         
-        def are_mirrors(root1,root2):
-            if not root1 and not root2:
-                return True
-            if not root1 or not root2:
-                return False
+#         def are_mirrors(root1,root2):
+#             if not root1 and not root2:
+#                 return True
+#             if not root1 or not root2:
+#                 return False
             
-            if root1.val != root2.val:
-                return False
+#             if root1.val != root2.val:
+#                 return False
             
-            left_1_matches_right_2 = are_mirrors(root1.left, root2.right)
-            right1_matches_left_2 = are_mirrors(root1.right, root2.left)
+#             left_1_matches_right_2 = are_mirrors(root1.left, root2.right)
+#             right1_matches_left_2 = are_mirrors(root1.right, root2.left)
             
-            return left_1_matches_right_2 and right1_matches_left_2
+#             return left_1_matches_right_2 and right1_matches_left_2
         
-        return are_mirrors(root.left, root.right)
-# Time: O(N)
-# Space: O(H)
+#         return are_mirrors(root.left, root.right)
+# # Time: O(N)
+# # Space: O(H)
 
-# iterative BFS
-        q = collections.deque([root])
+# # iterative BFS
+#         q = collections.deque([root])
         
-        while q:
-            traversal = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                traversal.append(node.val if node else None)
-                if node:
-                    q.append(node.left)
-                    q.append(node.right)
-            if traversal != traversal[::-1]:
-                return False
+#         while q:
+#             traversal = []
+#             for _ in range(len(q)):
+#                 node = q.popleft()
+#                 traversal.append(node.val if node else None)
+#                 if node:
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if traversal != traversal[::-1]:
+#                 return False
             
-        return True
+#         return True
 
 # Time: O(N)
 # Space: O(W)
+
+    # or slightly more optimal N vs 2N
+        if not root.left and not root.right:
+            return True
+        q = collections.deque([(root.left, root.right)])
+        
+        while q:
+            node1, node2 = q.popleft()
+            
+            if not node1 and not node2:
+                continue
+            
+            if not node1 or not node2:
+                return False
+            
+            if node1.val != node2.val:
+                return False
+            
+            q.append((node1.left, node2.right))
+            q.append((node1.right, node2.left))
+        
+        return True
         
         
