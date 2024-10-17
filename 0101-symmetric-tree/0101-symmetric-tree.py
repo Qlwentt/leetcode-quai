00f376fa326bed5329af13424fd351a5a7708e60
Quai Wentt <quai.wentt@gmail.com>
2024-10-17 14:32:40 -0400

    Time: 0 ms (100.00%), Space: 16.6 MB (25.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,42 +6,40 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-#         if not root.left and not root.right:
-#             return True
+        if not root.left and not root.right:
+            return True
         
-#         def are_mirrors(root1,root2):
-#             if not root1 and not root2:
-#                 return True
-#             if not root1 and root2:
-#                 return False
-#             if not root2 and root1:
-#                 return False
+        def are_mirrors(root1,root2):
+            val1 = root1.val if root1 else float('-inf')
+            val2 = root2.val if root2 else float('-inf')
+    
+            if val1 != val2:
+                return False
+            if val1 == float('-inf'):
+                return True
             
-#             if root1.val != root2.val:
-#                 return False
+            left = are_mirrors(root1.left, root2.right)
+            right = are_mirrors(root1.right, root2.left)
             
-#             left = are_mirrors(root1.left, root2.right)
-#             right = are_mirrors(root1.right, root2.left)
-            
-#             return left and right
+            return left and right
         
-#         return are_mirrors(root.left, root.right)
+        return are_mirrors(root.left, root.right)
 
 # iterative BFS
-        q = collections.deque([root])
+#         q = collections.deque([root])
         
-        while q:
-            traversal = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                traversal.append(node.val if node else None)
-                if node:
-                    q.append(node.left)
-                    q.append(node.right)
-            if traversal != traversal[::-1]:
-                return False
+#         while q:
+#             traversal = []
+#             for _ in range(len(q)):
+#                 node = q.popleft()
+#                 traversal.append(node.val if node else None)
+#                 if node:
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if traversal != traversal[::-1]:
+#                 return False
             
-        return True
+#         return True
         
         
         
