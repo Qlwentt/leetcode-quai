56c3ab758d9d3c0eda95ba2ceab67b98a715bdbb
Quai Wentt <quai.wentt@gmail.com>
2024-10-17 14:27:37 -0400

    Time: 3 ms (100.00%), Space: 16.7 MB (25.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,26 +6,43 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root.left and not root.right:
-            return True
+#         if not root.left and not root.right:
+#             return True
         
-        def are_mirrors(root1,root2):
-            if not root1 and not root2:
-                return True
-            if not root1 and root2:
-                return False
-            if not root2 and root1:
-                return False
+#         def are_mirrors(root1,root2):
+#             if not root1 and not root2:
+#                 return True
+#             if not root1 and root2:
+#                 return False
+#             if not root2 and root1:
+#                 return False
             
-            if root1.val != root2.val:
-                return False
+#             if root1.val != root2.val:
+#                 return False
             
-            left = are_mirrors(root1.left, root2.right)
-            right = are_mirrors(root1.right, root2.left)
+#             left = are_mirrors(root1.left, root2.right)
+#             right = are_mirrors(root1.right, root2.left)
             
-            return left and right
+#             return left and right
         
-        return are_mirrors(root.left, root.right)
+#         return are_mirrors(root.left, root.right)
+
+# iterative BFS
+        q = collections.deque([root])
+        
+        while q:
+            traversal = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                traversal.append(node.val if node else None)
+                if node:
+                    q.append(node.left)
+                    q.append(node.right)
+            print(traversal)
+            if traversal != traversal[::-1]:
+                return False
+            
+        return True
         
         
         
