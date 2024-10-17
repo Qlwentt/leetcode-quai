d49213532c8333a26d40c5f0e6276a177f70dc6e
Quai Wentt <quai.wentt@gmail.com>
2024-10-16 22:21:37 -0400

    Time: 34 ms (70.37%), Space: 16.6 MB (54.41%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -6,21 +6,39 @@
 #         self.right = right
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        stack = []
-        current = root
-        min_diff = float('inf')
+        # stack = []
+        # current = root
+        # min_diff = float('inf')
+        # prev = None
+        # while current or stack:
+        #     if current:
+        #         stack.append(current)
+        #         current = current.left
+        #     else:
+        #         current = stack.pop()
+        #         if prev is not None:
+        #             min_diff = min(min_diff, abs(prev -current.val))
+        #         prev = current.val
+        #         current = current.right
+        # return min_diff
+        
         prev = None
-        while current or stack:
-            if current:
-                stack.append(current)
-                current = current.left
-            else:
-                current = stack.pop()
-                if prev is not None:
-                    min_diff = min(min_diff, abs(prev -current.val))
-                prev = current.val
-                current = current.right
+        min_diff = float('inf')
+        def inorder(root):
+            nonlocal prev, min_diff
+            if not root:
+                return
+            
+            inorder(root.left)
+            if prev != None:
+                min_diff = min(min_diff, abs(prev-root.val))
+            prev = root.val
+            inorder(root.right)
+        
+        inorder(root)
         return min_diff
+    
+        
                 
         
         
