5dfabf4b17be6ebbc683c0fb4a48ce055a457bf7
Quai Wentt <quai.wentt@gmail.com>
2024-10-23 13:53:01 -0400

    Time: 2 ms (96.44%), Space: 18.7 MB (5.92%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -13,11 +13,11 @@ class Solution:
                 return True
             
             left = inorder(root.left)
-            isValid = root.val > prev
+            root_valid = root.val > prev
             prev = root.val
             right = inorder(root.right)
             
-            return left and right and isValid
+            return left and right and root_valid
         
         return inorder(root)
 # Time: O(N)
