8c0800becb11e853b9b735cb5996ec7d1abd0c52
Quai Wentt <quai.wentt@gmail.com>
2024-10-30 18:55:28 -0400

    Time: 1872 ms (39.62%), Space: 16.8 MB (63.58%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        lis_length = [1] * N
+        lds_length = [1] * N
+
+        # Stores the length of longest increasing subsequence that ends at i.
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    lis_length[i] = max(lis_length[i], lis_length[j] + 1)
+
+        # Stores the length of longest decreasing subsequence that starts at i.
+        for i in range(N - 1, -1, -1):
+            for j in range(i + 1, N):
+                if nums[i] > nums[j]:
+                    lds_length[i] = max(lds_length[i], lds_length[j] + 1)
+
+        min_removals = float("inf")
+        for i in range(N):
+            if lis_length[i] > 1 and lds_length[i] > 1:
+                min_removals = min(
+                    min_removals, N - lis_length[i] - lds_length[i] + 1
+                )
+
+        return min_removals 
