2e3588bcfd640ecc16e4ba3a49b065c819d8a2d6
Quai Wentt <quai.wentt@gmail.com>
2024-10-31 12:11:15 -0400

    Time: 2108 ms (28.00%), Space: 109.9 MB (21.09%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        robot.sort()
+        factory.sort(key=lambda x: x[0])
+        factory_positions = []
+        for f in factory:
+            factory_positions.extend([f[0]] * f[1])
+        robot_count = len(robot)
+        factory_count = len(factory_positions)
+
+        dp = [[None] * (factory_count + 1) for _ in range(robot_count + 1)]
+
+        def _calculate_min_distance(robot_idx: int, factory_idx: int) -> int:
+            if dp[robot_idx][factory_idx] is not None:
+                return dp[robot_idx][factory_idx]
+            if robot_idx == robot_count:
+                dp[robot_idx][factory_idx] = 0
+                return 0
+            if factory_idx == factory_count:
+                dp[robot_idx][factory_idx] = int(1e12)
+                return int(1e12)
+
+            assign = abs(
+                robot[robot_idx] - factory_positions[factory_idx]
+            ) + _calculate_min_distance(robot_idx + 1, factory_idx + 1)
+
+            skip = _calculate_min_distance(robot_idx, factory_idx + 1)
+
+            dp[robot_idx][factory_idx] = min(assign, skip)
+            return dp[robot_idx][factory_idx]
+
+        return _calculate_min_distance(0, 0)
