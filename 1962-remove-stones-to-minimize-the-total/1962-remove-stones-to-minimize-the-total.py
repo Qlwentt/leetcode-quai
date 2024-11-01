7ef5b424b6e15ec5435f7b734bf0764210b38a06
Quai Wentt <quai.wentt@gmail.com>
2024-11-01 10:57:45 -0400

    Time: 635 ms (11.87%), Space: 28.5 MB (94.28%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -4,10 +4,12 @@ class Solution:
         heapq.heapify(max_heap)
         
         while k:
-            greatest = heapq.heappop(max_heap)
-            greatest = greatest * -1
+            greatest = heapq.heappop(max_heap) * -1
             greatest -= int(greatest/2)
             heapq.heappush(max_heap, -greatest)
             k -= 1
             
         return -sum(max_heap)
+    
+    # Time: O(Klog(N))
+    # Space: O(N)
