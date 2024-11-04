b3543e8495e29913d3d523b67c8ea033185b0372
Quai Wentt <quai.wentt@gmail.com>
2024-11-04 10:09:07 -0500

    Time: 3 ms (11.59%), Space: 16.8 MB (11.52%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        answer = []
+        power_set = []
         
-        def backtrack(i, curSubset):
-            if i == len(nums):
-                answer.append(curSubset.copy())
+        def backtrack(i, cur_set):
+            if i >= len(nums):
+                power_set.append(cur_set.copy())
                 return
             
-            curSubset.append(nums[i])
-            backtrack(i+1, curSubset)
-            curSubset.pop()
-            backtrack(i+1, curSubset)
+            # choose number
+            cur_set.append(nums[i])
+            backtrack(i+1, cur_set)
+            
+            # skip number
+            cur_set.pop()
+            backtrack(i+1, cur_set)
+
         backtrack(0, [])
-        return answer
+        return power_set
+# Time: O(N*2^N)
+# Space: O(N) (not counting output)
