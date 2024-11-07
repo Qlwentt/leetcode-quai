e54c8c4a637ab3841625430ec4338f2a5bebb67b
Quai Wentt <quai.wentt@gmail.com>
2024-11-06 19:07:37 -0500

    Time: 15 ms (32.60%), Space: 16.9 MB (11.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,40 +1,47 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # combs = []
+        combs = []
         nums = candidates
 
-#         def backtrack(i, curComb, curSum):
-#             if curSum == target:
-#                 combs.append(curComb.copy())
-#                 return
+        def backtrack(i, curComb, curSum):
+            if curSum == target:
+                combs.append(curComb.copy())
+                return
             
-#             if i >= len(nums) or curSum > target:
-#                 return
+            if i == len(nums) or curSum > target:
+                return
             
-#             # choose num between 1 and unlimited times
-#             curComb.append(nums[i])
-#             backtrack(i, curComb, curSum + nums[i])
+            # choose num between 1 and unlimited times
+            curComb.append(nums[i])
+            backtrack(i, curComb, curSum + nums[i])
             
-#             # skip num
-#             curComb.pop()
-#             backtrack(i+1, curComb, curSum)
+            # skip (or stop choosing num)
+            curComb.pop()
+            backtrack(i+1, curComb, curSum)
         
-#         backtrack(0, [], 0)
-#         return combs
+        backtrack(0, [], 0)
+        return combs
 # Time: O(2^T*N) where T is the target value and N is the number of candidates
 # Space: O(T) where T is the target value
-        
-        combs = set()
+   
+    
+# kind of cheating, naive method
+        combs = []
+        nums = candidates
+
         def backtrack(i, cur_comb, cur_sum):
             if cur_sum > target:
                 return
             if cur_sum == target:
-                combs.add(tuple(sorted(cur_comb)))
+                combs.append(cur_comb.copy)
             for j in range(i, len(nums)):
                 cur_comb.append(nums[j])
                 backtrack(j, cur_comb, cur_sum+ nums[j])
                 cur_comb.pop()
         backtrack(0,[], 0)
         return list(combs)
+    
+    # Time: Approximately O(N^T*NlogN)
+    # Space: Approximately O(T)
         
         
