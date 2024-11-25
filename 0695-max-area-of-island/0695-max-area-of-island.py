9437cebc6b51776f076207bbb4b2ff39e29a5c8a
Quai Wentt <quai.wentt@gmail.com>
2024-11-25 13:48:48 -0500

    Time: 49 ms (6.45%), Space: 18.7 MB (11.46%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,10 +3,8 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
         visited = set()
-        
         def dfs(r,c):
-            if ( 
-                r not in range(ROWS) or
+            if (r not in range(ROWS) or
                 c not in range(COLS) or
                 (r,c) in visited or
                 grid[r][c] == 0):
@@ -14,19 +12,20 @@ class Solution:
             
             visited.add((r,c))
             
-            up = dfs(r-1,c)
+            up = dfs(r-1, c)
             down = dfs(r+1, c)
             left = dfs(r, c-1)
             right = dfs(r, c+1)
             
-            return up+down+left+right+1
+            return up + down + left + right + 1
         maxArea = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    maxArea = max(dfs(r,c), maxArea)
+                    area = dfs(r,c)    
+                    maxArea = max(area, maxArea)
                     
         return maxArea
-                
-            
-        
+    
+# Time: O (M*N)
+# Space: O(M*N)
