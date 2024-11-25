aa5ae63a3b2b712cd60eba6c812f5ace58cceb86
Quai Wentt <quai.wentt@gmail.com>
2024-11-25 17:16:34 -0500

    Time: 352 ms (10.87%), Space: 18.4 MB (33.58%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,31 +1,36 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        q = deque([])
-        if grid[0][0] == 0:
-            q.append((0,0, 1, [(0,0)]))
-            
-        answer = - 1
-        directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,-1], [1,-1], [-1,1]]
-        N = len(grid)
+        ROWS = len(grid)
+        COLS = len(grid[0])
         visited = set()
+        q = deque([])
+        
+        if grid[0][0] == 0:
+            q.append((0,0,1))
+            visited.add((0,0))
+        
+        directions = [[0,1],[1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1, -1]]
+        
         while q:
-            r, c, level, path = q.popleft()
+            r, c, level = q.popleft()
             
-            if r == N-1 and c == N-1:
-                # print(path)
+            
+            if (r,c) == (ROWS-1, COLS-1):
                 return level
             
             for dr, dc in directions:
-                newR = dr + r
-                newC = dc + c
-                if (
-                    newR in range(N) and
-                    newC in range(N) and
+                newR = r + dr
+                newC = c + dc
+                
+                if (newR in range(ROWS) and
+                    newC in range(COLS) and
                     (newR, newC) not in visited and
                     grid[newR][newC] != 1
-                ):
-                    q.append((newR, newC, level+1, path + [(newR,newC)]))
+                   ):
+                    
+                    q.append((newR, newC, level +1))
                     visited.add((newR, newC))
         # print([])
-        return answer
+        return -1
+        
