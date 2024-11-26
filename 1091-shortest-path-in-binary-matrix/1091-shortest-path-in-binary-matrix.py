0424647dc4e26b7f896e09501b9f17d0f8e669ab
Quai Wentt <quai.wentt@gmail.com>
2024-11-26 15:03:41 -0500

    Time: 405 ms (5.35%), Space: 19 MB (7.71%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -8,14 +8,13 @@ class Solution:
         
         if grid[0][0] == 0:
             q.append((0,0,1))
+            visited.add((0,0))
         
         directions = [[0,1],[1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1, -1]]
         
         while q:
             r, c, level = q.popleft()
-            if (r,c) in visited:
-                continue
-            visited.add((r,c))
+            
             
             if (r,c) == (ROWS-1, COLS-1):
                 return level
@@ -26,10 +25,12 @@ class Solution:
                 
                 if (newR in range(ROWS) and
                     newC in range(COLS) and
+                    (newR, newC) not in visited and
                     grid[newR][newC] != 1
                    ):
                     
                     q.append((newR, newC, level +1))
+                    visited.add((newR, newC))
         
         return -1
         
