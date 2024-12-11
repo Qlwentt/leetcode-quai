04c996c61804535618a9cf55804eb16f1358d721
Quai Wentt <quai.wentt@gmail.com>
2024-12-11 12:16:50 -0500

    Time: 8 ms (12.30%), Space: 17.4 MB (9.69%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,32 +5,32 @@ class Solution:
         COLS = len(grid[0])
         visited = set()
         q = deque([])
-        numFresh = 0
+        num_fresh = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append((r,c, 0))
                     visited.add((r,c))
                 elif grid[r][c] == 1:
-                    numFresh += 1
+                    num_fresh += 1
         directions = [[0,1], [1,0], [0,-1], [-1,0]]
-        minMins = 0
+        answer = 0
         while q:
             r,c,time = q.popleft()
-            minMins = max(minMins, time)
-            numFresh -= grid[r][c] == 1
+            answer = max(answer, time)
             
             for dr, dc in directions:
-                newR = dr + r
-                newC = dc + c
-                if (newR in range(ROWS) and
-                    newC in range(COLS) and
-                    (newR, newC) not in visited and
-                    grid[newR][newC] != 0):
-                    q.append((newR,newC, time+1))
-                    visited.add((newR,newC))
+                new_r = dr + r
+                new_c = dc + c
+                if (new_r in range(ROWS) and
+                    new_c in range(COLS) and
+                    (new_r, new_c) not in visited and
+                    grid[new_r][new_c] == 1):
+                    q.append((new_r,new_c, time+1))
+                    visited.add((new_r,new_c))
+                    num_fresh -= 1
 
-        return minMins if numFresh == 0 else -1
+        return answer if num_fresh == 0 else -1
     # Time: O(N*M)
     # Space: O(N*M)
         
