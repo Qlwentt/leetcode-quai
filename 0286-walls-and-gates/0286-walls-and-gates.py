11d68f28396136b26b2768c451815564dc3c4dc0
Quai Wentt <quai.wentt@gmail.com>
2024-12-12 18:37:50 -0500

    Time: 65 ms (33.23%), Space: 20.8 MB (54.10%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -7,29 +7,29 @@ class Solution:
         q = deque([])
         ROWS = len(rooms)
         COLS = len(rooms[0])
-        visited = set()
+        INF = 2**31 -1
+        
         # initialize q with gates
         
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     q.append((r,c, 0))
-                    visited.add((r,c))
+                    
                     
         directions = [[0,1], [1,0], [0,-1], [-1,0]]
         while q:
             r, c, distance = q.popleft()
-            rooms[r][c] = distance
+            
             
             for dr, dc in directions:
                 newR = dr + r
                 newC = dc + c
                 if (newR in range(ROWS) and 
                     newC in range(COLS) and
-                    (newR, newC) not in visited and
-                   rooms[newR][newC] != -1):
+                   rooms[newR][newC] == INF):
                     q.append((newR, newC, distance + 1))
-                    visited.add((newR, newC))
+                    rooms[newR][newC] = distance+1
                     
         
                     
