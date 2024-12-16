321bc0b97251808c7d5667506f61240d712a62f9
Quai Wentt <quai.wentt@gmail.com>
2024-12-16 18:58:38 -0500

    Time: 273 ms (5.16%), Space: 35.7 MB (7.59%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,16 +1,33 @@
 from sortedcontainers import SortedList
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        sorted_list = SortedList(words)
-        count = 0
+        # sorted_list = SortedList(words)
+        # count = 0
+        # extra = False
+        # while sorted_list:
+        #     word = sorted_list.pop(0)
+        #     if word[::-1] in sorted_list:
+        #         count += 2
+        #         sorted_list.remove(word[::-1])
+        #     elif word[0]+word[0] == word:
+        #         extra = True
+        # return (count+extra) * 2
+        
+        count_words = collections.Counter(words)
+        pali_words = 0
         extra = False
-        while sorted_list:
-            word = sorted_list.pop(0)
-            if word[::-1] in sorted_list:
-                count += 2
-                sorted_list.remove(word[::-1])
+        
+        while count_words:
+            word, count = count_words.popitem()
+            if count != 1:
+                count_words[word] = (count - 1)
+            if word[::-1] in count_words:
+                pali_words += 2
+                count_words[word[::-1]] -= 1
+                if count_words[word[::-1]] == 0:
+                    del count_words[word[::-1]]
             elif word[0]+word[0] == word:
                 extra = True
-        return (count+extra) * 2
+        return (pali_words + extra) *2
   
     
