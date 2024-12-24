a5c4c08373567fd1e05304c4aebbb22ccfbb269c
Quai Wentt <quai.wentt@gmail.com>
2024-12-24 17:38:40 -0500

    Time: 524 ms (54.36%), Space: 79.9 MB (82.55%) - LeetHub

---
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.py
@@ -0,0 +1,57 @@
+class Solution:
+    def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
+    # Calculate the number of nodes for each tree
+        n = len(edges1) + 1
+        m = len(edges2) + 1
+
+        # Build adjacency lists for both trees
+        adj_list1 = self.build_adj_list(n, edges1)
+        adj_list2 = self.build_adj_list(m, edges2)
+
+        # Calculate the diameters of both trees
+        diameter1 = self.find_diameter(n, adj_list1)
+        diameter2 = self.find_diameter(m, adj_list2)
+
+        # Calculate the longest path that spans across both trees
+        combined_diameter = ceil(diameter1 / 2) + ceil(diameter2 / 2) + 1
+
+        # Return the maximum of the three possibilities
+        return max(diameter1, diameter2, combined_diameter)
+
+    def build_adj_list(self, size, edges):
+        adj_list = [[] for _ in range(size)]
+        for edge in edges:
+            adj_list[edge[0]].append(edge[1])
+            adj_list[edge[1]].append(edge[0])
+        return adj_list
+
+    def find_diameter(self, n, adj_list):
+        # First BFS to find the farthest node from an arbitrary node (e.g., 0)
+        farthest_node, _ = self.find_farthest_node(n, adj_list, 0)
+
+        # Second BFS to find the diameter starting from the farthest node
+        _, diameter = self.find_farthest_node(n, adj_list, farthest_node)
+        return diameter
+
+    def find_farthest_node(self, n, adj_list, source_node):
+        queue = deque([source_node])
+        visited = [False] * n
+        visited[source_node] = True
+
+        maximum_distance = 0
+        farthest_node = source_node
+
+        while queue:
+            for _ in range(len(queue)):
+                current_node = queue.popleft()
+                farthest_node = current_node
+
+                for neighbor in adj_list[current_node]:
+                    if not visited[neighbor]:
+                        visited[neighbor] = True
+                        queue.append(neighbor)
+
+            if queue:
+                maximum_distance += 1
+
+        return farthest_node, maximum_distance
