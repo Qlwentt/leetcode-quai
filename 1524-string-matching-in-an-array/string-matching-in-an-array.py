5f3a17fcf814f27a3287c52a2da9ffa98040a8a7
Quai Wentt <quai.wentt@gmail.com>
2025-01-06 23:45:52 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (28.23%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,18 +1,10 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        substrings = set()
-        for word in words:
-            for i in range(len(word)):
-                for j in range(i, len(word)):
-                    if not (i==0 and j==len(word)-1):
-                        substrings.add(word[i:j+1])
-            
-        
-        answer = []
-        for word in words:
-            if word in substrings:
-                answer.append(word)
-
-        return answer
+        s = ' '.join(words)
+        res = []
+        for w in words:
+            if s.count(w) > 1:
+                res.append(w)
+        return res
 
      
