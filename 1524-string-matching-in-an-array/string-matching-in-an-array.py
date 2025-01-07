cf7d2193ccc24f4e8dd0305f287b4a13a4b21c84
Quai Wentt <quai.wentt@gmail.com>
2025-01-07 00:29:39 -0500

    Time: 16 ms (7.66%) | Memory: 18.8 MB (6.61%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,10 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        word = collections.deque(list(word))
+        while word: 
+            cur = self.root
+            for char in word:
+                if char not in cur.children:
+                    cur.children[char] = TrieNode()
+                cur = cur.children[char]
+                cur.count += 1
+            word.popleft()
+    
+    def is_substring(self,word):
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                return False
+            cur = cur.children[char]
+        return cur.count > 1
+
+    
+
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        s = ' '.join(words)
-        res = []
-        for w in words:
-            if s.count(w) > 1:
-                res.append(w)
-        return res
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        answer = []
+        for word in words:
+            if trie.is_substring(word):
+                answer.append(word)
+
+        return answer
+        
 
      
