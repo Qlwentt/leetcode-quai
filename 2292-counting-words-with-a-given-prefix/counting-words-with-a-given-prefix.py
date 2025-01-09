9e46fcc78b1db1b49932500be47282916354919a
Quai Wentt <quai.wentt@gmail.com>
2025-01-08 23:23:18 -0500

    Time: 51 ms (5.73%) | Memory: 18.8 MB (9.54%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+            current.count += 1
+
+    def prefix_count(self, prefix):
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return 0
+            current = current.children[char]
+        return current.count
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+
+        return trie.prefix_count(pref)
