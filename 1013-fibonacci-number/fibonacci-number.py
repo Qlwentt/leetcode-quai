cdfde273ef93d33e6e2f267545b0a65432ed44f3
Quai Wentt <quai.wentt@gmail.com>
2025-01-12 20:22:29 -0500

    Time: 35 ms (73.35%) | Memory: 17.7 MB (18.16%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    memo = {}
+    def fib(self, n: int) -> int:
+        # if n <= 1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
+# Time: O(2^N)
+# Space: O(N)
+
+# Memoization
+        if n <= 1:
+            return n
+        if n in self.memo:
+            return self.memo[n]
+
+        result = self.fib(n-1) + self.fib(n-2)
+        self.memo[n] = result
+        return result
+
+# Time: O(N)
+# Space: O(N)
+        
