d061a5a22835b5780e9b712bc08a79965a222514
Quai Wentt <quai.wentt@gmail.com>
2025-01-14 20:38:03 -0500

    Time: 40 ms (50.05%) | Memory: 17.7 MB (30.26%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,23 +1,50 @@
 class Solution:
-    @cache
+    # memo = {}
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
+#         if n <= 1:
+#             return n
 
-        return self.fib(n-1) + self.fib(n-2)
-# Time: O(2^N)
-# Space: O(N)
+#         return self.fib(n-1) + self.fib(n-2)
+# # Time: O(2^N)
+# # Space: O(N)
 
-# Memoization
-        # if n <= 1:
-        #     return n
-        # if n in self.memo:
-        #     return self.memo[n]
+# # Memoization
+#         if n <= 1:
+#             return n
+#         if n in self.memo:
+#             return self.memo[n]
 
-        # result = self.fib(n-1) + self.fib(n-2)
-        # self.memo[n] = result
-        # return result
+#         result = self.fib(n-1) + self.fib(n-2)
+#         self.memo[n] = result
+#         return result
 
 # Time: O(N)
 # Space: O(N)
+
+# Bottom-Up - Tabulation
+
+        # if n <= 1:
+        #     return n
         
+        # dp = [0] * (n+1) # set up dp array
+        # dp[1] = 1 # "base case"
+        # for x in range(2,n+1):
+        #     dp[x] = dp[x-1] + dp[x-2] # "recursive case"
+        
+        # return dp[n] # return result
+
+# Time: O(N)
+# Space: O(N)
+
+# Bottom-Up Tabulation - Space Optimized
+        if n <= 1:
+            return n
+        
+        n_minus_2 = 0
+        n_minus_1 = 1
+        fn = 0
+        for _ in range(2, n+1):
+            fn = n_minus_2 + n_minus_1 
+            n_minus_1, n_minus_2 = fn, n_minus_1
+        return fn
+
