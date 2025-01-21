1ddc433dcd45603d9261ddb994f0a17c7ebed60d
Quai Wentt <quai.wentt@gmail.com>
2025-01-21 16:57:50 -0500

    Time: 3 ms (9.00%) | Memory: 17.8 MB (32.78%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,20 +2,23 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
 
         # Top Down Memoization
-        cache = {}
-        def dp(i):
-            if i >= len(nums):
-                return 0
-            if i in cache:
-                return cache[i]
+        # cache = {}
+        # def dp(i):
+        #     if i >= len(nums):
+        #         return 0
+        #     if i in cache:
+        #         return cache[i]
 
-            take = nums[i] + dp(i+2)
-            skip = dp(i+1)
-            result =  max(take,skip)
-            cache[i] = result
-            return result
+        #     take = nums[i] + dp(i+2)
+        #     skip = dp(i+1)
+        #     result =  max(take,skip)
+        #     cache[i] = result
+        #     return result
 
-        return dp(0)
+        # return dp(0)
+
+# Time: O(N)
+# Space: O(N)
 
         # Bottom Up - Tabulation
 
@@ -26,6 +29,21 @@ class Solution:
         #     dp[i] = max(take, skip)
 
         # return dp[0]
-    
+# Time: O(N)
+# Space: O(N)
+
+        # Tabulation - Optimized
+        i_plus_1 = 0
+        i_plus_2 = 0
+        dp_i = 0
+        for i in range(len(nums)-1,-1,-1):
+            take = nums[i] + i_plus_2
+            skip = i_plus_1
+            dp_i = max(take, skip)
+            i_plus_1, i_plus_2 = dp_i, i_plus_1
+
+        return dp_i
+# Time: O(N)
+# Space: O(1)
     
         
