4fd19b797a8de5f60db3e3f9ff9b106bcf7be737
Quai Wentt <quai.wentt@gmail.com>
2025-01-21 16:49:38 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (8.03%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,24 +1,31 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # @cache
-        # def dp(i):
-        #     if i >= len(nums):
-        #         return 0
 
-        #     take = nums[i] + dp(i+2)
-        #     skip = dp(i+1)
+        # Top Down Memoization
+        cache = {}
+        def dp(i):
+            if i >= len(nums):
+                return 0
+            if i in cache:
+                return cache[i]
 
-        #     return max(take,skip)
+            take = nums[i] + dp(i+2)
+            skip = dp(i+1)
+            result =  max(take,skip)
+            cache[i] = result
+            return result
 
-        # return dp(0)
+        return dp(0)
 
-        dp = defaultdict(int)
-        for i in range(len(nums)-1,-1,-1):
-            take = nums[i] + dp[i+2]
-            skip = dp[i+1]
-            dp[i] = max(take, skip)
+        # Bottom Up - Tabulation
 
-        return dp[0]
+        # dp = defaultdict(int)
+        # for i in range(len(nums)-1,-1,-1):
+        #     take = nums[i] + dp[i+2]
+        #     skip = dp[i+1]
+        #     dp[i] = max(take, skip)
+
+        # return dp[0]
     
     
         
