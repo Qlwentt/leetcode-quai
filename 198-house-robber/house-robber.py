267bbe8366ee371e7251d6bb9e0346d0702318e2
Quai Wentt <quai.wentt@gmail.com>
2025-01-21 16:42:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.78%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i >= len(nums):
+                return 0
+
+            take = nums[i] + dp(i+2)
+            skip = dp(i+1)
+
+            return max(take,skip)
+
+        return dp(0)
+
+
+        # dp = collections.defaultdict(int)
+    
+        # for i in range(len(nums)-1,-1,-1):
+        #     take = nums[i] + dp[i+2]
+        #     skip = dp[i+1]
+        #     dp[i] = max(take, skip)
+            
+        # return dp[0]
+        
+    
+    
+        
