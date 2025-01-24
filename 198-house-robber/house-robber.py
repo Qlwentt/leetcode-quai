0795f10c0f87ee5c306821dd747143e6c22a6a85
Quai Wentt <quai.wentt@gmail.com>
2025-01-24 09:22:47 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.51%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,33 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # Top Down Memoization
-        # cache = {}
-        # def dp(i):
-        #     if i >= len(nums):
-        #         return 0
-        #     if i in cache:
-        #         return cache[i]
+#         cache = {}
+#         def dp(i):
+#             if i >= len(nums):
+#                 return 0
+#             if i in cache:
+#                 return cache[i]
 
-        #     take = nums[i] + dp(i+2)
-        #     skip = dp(i+1)
-        #     result =  max(take,skip)
-        #     cache[i] = result
-        #     return result
+#             take = nums[i] + dp(i+2)
+#             skip = dp(i+1)
+#             result =  max(take,skip)
+#             cache[i] = result
+#             return result
 
-        # return dp(0)
+#         return dp(0)
 
-# Time: O(N)
-# Space: O(N)
+# # Time: O(N)
+# # Space: O(N)
 
-        # Bottom Up - Tabulation
+#         # Bottom Up - Tabulation
 
-        # dp = defaultdict(int)
-        # for i in range(len(nums)-1,-1,-1):
-        #     take = nums[i] + dp[i+2]
-        #     skip = dp[i+1]
-        #     dp[i] = max(take, skip)
+#         #[1,3,1,3,100] => 103
 
-        # return dp[0]
+#         dp = defaultdict(int)
+#         for i in range(len(nums)-1,-1,-1):
+#             take = nums[i] + dp[i+2]
+#             skip = dp[i+1]
+#             dp[i] = max(take, skip)
+
+#         return dp[0]
 # Time: O(N)
 # Space: O(N)
 
