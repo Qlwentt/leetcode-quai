b970a387d3dde518e9ab4c1f11f8ac893c7f5e1d
Quai Wentt <quai.wentt@gmail.com>
2025-02-04 15:24:53 -0500

    Time: 8 ms (49.39%) | Memory: 21.7 MB (24.16%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key= lambda x: x[0]) # sort by start
+        answer = [intervals[0]]
+        for start, end in intervals[1:]:
+            if answer[-1][1] >= start: # overlap
+                answer[-1] = [min(start, answer[-1][0]), max(end, answer[-1][1])]
+            else:
+                answer.append([start,end])
+            
+        
+        return answer
+    
+# Time: O(N*log(N)) because you had to sort
+# Space: O(N) for answer
