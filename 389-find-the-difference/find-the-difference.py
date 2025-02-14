4ece016307f8e928e200af9a614f9591afd4e080
Quai Wentt <quai.wentt@gmail.com>
2025-02-13 21:22:48 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.09%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,8 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         answer = 0
-        both = s + t
-        for char in both:
+        
+        for char in s:
+            answer ^= ord(char)
+
+        for char in t:
             answer ^= ord(char)
         
         return chr(answer)
