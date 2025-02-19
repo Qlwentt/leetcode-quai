280251b44682e975974dd78c550a439742b46cfa
Quai Wentt <quai.wentt@gmail.com>
2025-02-19 10:31:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.96%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            if n & 1:
+                count += 1
+            n >>= 1
+        return count
+                
