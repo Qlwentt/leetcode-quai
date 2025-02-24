621ef3d4dc66d40d9d621e42ef4c955c8ba18bd3
Quai Wentt <quai.wentt@gmail.com>
2025-02-24 13:10:36 -0500

    Time: 41 ms (10.59%) | Memory: 26.6 MB (97.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,23 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # return len(set(nums)) != len(nums)
+        # # set loop solution
+        # nums_set = set()
+        # for num in nums:
+        #     if num in nums_set:
+        #         return True
+        #     nums_set.add(num)
+        # return False
+        # # Time: O(N)
+        # # Space: O(N)
 
-        nums_set = set()
-        for num in nums:
-            if num in nums_set:
+        # # one liner
+        # return len(set(nums)) != len(nums)
+        # # Time: O(N)
+        # # Space: O(N)
+
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return True
-            nums_set.add(num)
         return False
-        
+
