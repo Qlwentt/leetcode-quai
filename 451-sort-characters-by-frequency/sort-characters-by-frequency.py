4c23c337b91f0958aa19855259616d85a6f05f7a
Quai Wentt <quai.wentt@gmail.com>
2025-02-28 00:06:00 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (35.91%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,20 +1,35 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        char_to_count = defaultdict(int)
-        for char in s:
-            char_to_count[char] += 1
+        # dictionary and bucket sort
+        # char_to_count = defaultdict(int)
+        # for char in s:
+        #     char_to_count[char] += 1
 
-        counts_to_char = defaultdict(list)
-        for char, count in char_to_count.items():
-            counts_to_char[count].append(char)
+        # counts_to_char = defaultdict(list)
+        # for char, count in char_to_count.items():
+        #     counts_to_char[count].append(char)
         
+        # answer = []
+        # for count in range(len(s),0,-1):
+        #     for char in counts_to_char[count]:
+        #         answer.append(char*count)
+        
+        # return "".join(answer)
+    # Time: O(N)
+    # Space: O(N)  
+
+    # using Counter
+
+        counts = collections.Counter(s)
         answer = []
-        for count in range(len(s),0,-1):
-            for char in counts_to_char[count]:
-                answer.append(char*count)
-        
+        for char, count in counts.most_common():
+            answer.append(char*count)
         return "".join(answer)
-        
+    # Time: O(NlogN)
+    # Space: O(N)
+
+
+
         
 
         
