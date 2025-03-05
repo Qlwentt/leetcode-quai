941bedc9dd04069ee4f6d1f2452142261ee99c79
Quai Wentt <quai.wentt@gmail.com>
2025-03-04 19:40:47 -0500

    Time: 11 ms (83.54%) | Memory: 21 MB (53.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,15 +3,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         # # count of characters as key in a dictionary, list of words as the value
-        words = defaultdict(list)
-        for word in strs:
-            hashed_word = {char: 0 for char in "abcdefghijklmnopqrstuvwxyz"}
-            for char in word:
-                hashed_word[char] += 1
+        # words = defaultdict(list)
+        # for word in strs:
+        #     hashed_word = {char: 0 for char in "abcdefghijklmnopqrstuvwxyz"}
+        #     for char in word:
+        #         hashed_word[char] += 1
             
-            words[(tuple(hashed_word.items()))].append(word)
+        #     words[(tuple(hashed_word.items()))].append(word)
             
-        return list(words.values())
+        # return list(words.values())
 
         # Time: O(NK) where N is the length of strs and K is the max len of a string in strs
         # Space: O(NK)
