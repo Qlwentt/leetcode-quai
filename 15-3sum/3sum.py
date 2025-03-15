33cf6e9319b38958039cc6aacaa175e050c6ecd0
Quai Wentt <quai.wentt@gmail.com>
2025-03-15 10:28:43 -0400

    Time: 1955 ms (5.82%) | Memory: 20.5 MB (88.99%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # answer = set()
-        # nums.sort()
-        # target = 0
-        # for i in range(len(nums)):
-        #     lo = i + 1
-        #     hi = len(nums) -1
-        #     a = nums[i]
-        #     while lo < hi:
-        #         if a + nums[lo] + nums[hi] < target:
-        #             # too small, increase sum
-        #             lo += 1
-        #         elif a + nums[lo] + nums[hi] > target:
-        #             # too large, decrase sum
-        #             hi -= 1
-        #         else:
-        #             # equals target
-        #             answer.add(tuple([a, nums[lo], nums[hi]]))
-        #             lo += 1
-        #             hi -= 1
-        # return list(answer)
+        answer = set()
+        nums.sort()
+        target = 0
+        for i in range(len(nums)):
+            lo = i + 1
+            hi = len(nums) -1
+            a = nums[i]
+            while lo < hi:
+                if a + nums[lo] + nums[hi] < target:
+                    # too small, increase sum
+                    lo += 1
+                elif a + nums[lo] + nums[hi] > target:
+                    # too large, decrase sum
+                    hi -= 1
+                else:
+                    # equals target
+                    answer.add(tuple([a, nums[lo], nums[hi]]))
+                    lo += 1
+                    hi -= 1
+        return list(answer)
 
         answer = []
         nums.sort()
         target = 0
-
+        
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
