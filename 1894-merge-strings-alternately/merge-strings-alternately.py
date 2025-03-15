a9b0859991e8edd2a1575adccb9258c743090e85
Quai Wentt <quai.wentt@gmail.com>
2025-03-15 09:47:31 -0400

    Time: 35 ms (76.72%) | Memory: 18 MB (8.21%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        answer = []
+        pointer1 = 0
+        pointer2 = 0
+
+        while pointer1 < len(word1) and pointer2 < len(word2):
+            answer.append(word1[pointer1])
+            answer.append(word2[pointer2])
+            pointer1 += 1
+            pointer2 += 1
+        
+        while pointer1 < len(word1):
+            answer.append(word1[pointer1])
+            pointer1 += 1
+
+        while pointer2 < len(word2):
+            answer.append(word2[pointer2])
+            pointer2 += 1
+        
+        return "".join(answer)
+        
